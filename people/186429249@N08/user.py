e316ae7ceca966a03fa4c1d535f383d24cbfe4f8
The Map Group <admin@the-map-group.top>
2024-11-04 20:14:58 -0300

    Removed member '186429249@N08'

--- people/186429249@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '186429249@N08',
-  'alias': '186429249@N08',
-  'name': 'Yuri Pain',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/186429249@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/186429249@N08/',
-  'markers': 0,
-  'photos': 0
-}
