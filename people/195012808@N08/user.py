ef1f788f95fd786647ea38f53c82c8b8e36c0bd6
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:00 -0300

    Removed member '195012808@N08'

--- people/195012808@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '195012808@N08',
-  'alias': '195012808@N08',
-  'name': 'Erin Hodge',
-  'avatar': '../../icons/photographer.svg',
-  'url': 'https://www.flickr.com/photos/195012808@N08/',
-  'markers': 0,
-  'photos': 0
-}
