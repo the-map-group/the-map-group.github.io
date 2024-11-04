53c26c54e7a87b46bba472a4e86b728208f03511
The Map Group <admin@the-map-group.top>
2024-11-04 17:27:37 -0300

    [auto] Updated map for member 'jeanpaulneveu'

--- people/didifiosi/countries.py
+++ people/didifiosi/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'FR': ['France', 41, 1459]
+  'FR': ['France', 41, 1461]
 }
--- people/didifiosi/user.py
+++ people/didifiosi/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/131237713@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/didifiosi/',
   'markers': 41,
-  'photos': 1459
+  'photos': 1461
 }
