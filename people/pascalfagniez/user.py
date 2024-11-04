a3a703821ec76462d6acca57403b74375a8084d3
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:07 -0300

    Removed member 'pascalfagniez'

--- people/pascalfagniez/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '127946907@N07',
-  'alias': 'pascalfagniez',
-  'name': 'Pascal FAGNIEZ',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/127946907@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/pascalfagniez/',
-  'markers': 0,
-  'photos': 0
-}
