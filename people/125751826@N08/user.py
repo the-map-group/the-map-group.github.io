f55e2c497dfc2fdc8529697c8d9a082e257a47bf
The Map Group <admin@the-map-group.top>
2024-11-04 20:14:56 -0300

    Removed member '125751826@N08'

--- people/125751826@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '125751826@N08',
-  'alias': '125751826@N08',
-  'name': 'christian rault',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/125751826@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/125751826@N08/',
-  'markers': 0,
-  'photos': 0
-}
