81bd76285f8763258ec9fb8e090ca2cd809273d5
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:07 -0300

    Removed member 'russellcardwell'

--- people/russellcardwell/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '55341805@N02',
-  'alias': 'russellcardwell',
-  'name': 'Russell Cardwell',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/55341805@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/russellcardwell/',
-  'markers': 0,
-  'photos': 0
-}
