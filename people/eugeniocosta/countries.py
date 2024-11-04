e4f830436b3ccab45abea5c5a0ead60383df9a59
The Map Group <admin@the-map-group.top>
2024-11-04 10:40:22 -0300

    [auto] Updated map for member 'Eugenio GV Costa'

--- people/eugeniocosta/countries.py
+++ people/eugeniocosta/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'IT': ['Italy', 651, 670],
+  'IT': ['Italy', 652, 671],
   'FR': ['France', 1, 1]
 }
--- people/eugeniocosta/user.py
+++ people/eugeniocosta/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Eugenio Costa',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/169904704@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/eugeniocosta/',
-  'markers': 652,
-  'photos': 671
+  'markers': 653,
+  'photos': 672
 }
