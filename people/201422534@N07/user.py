d0aeca64172c5ad452539f69d29c3d1b34b9a661
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:04 -0300

    Removed member '201422534@N07'

--- people/201422534@N07/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '201422534@N07',
-  'alias': '201422534@N07',
-  'name': 'Pop Loli',
-  'avatar': '../../icons/photographer.svg',
-  'url': 'https://www.flickr.com/photos/201422534@N07/',
-  'markers': 0,
-  'photos': 0
-}
