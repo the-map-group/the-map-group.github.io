9c317a4b8d3a36e97b2dccaf7d3ef9d68a50a447
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:02 -0300

    Removed member '196976091@N02'

--- people/196976091@N02/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '196976091@N02',
-  'alias': '196976091@N02',
-  'name': 'bernard notebom',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/196976091@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/196976091@N02/',
-  'markers': 0,
-  'photos': 0
-}
