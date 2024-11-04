f0f854418c77fa652d6ba2910d70b50d55ead0b5
The Map Group <admin@the-map-group.top>
2024-11-04 20:14:59 -0300

    Removed member '191609180@N06'

--- people/191609180@N06/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '191609180@N06',
-  'alias': '191609180@N06',
-  'name': 'Abdou Nabile',
-  'avatar': '../../icons/photographer.svg',
-  'url': 'https://www.flickr.com/photos/191609180@N06/',
-  'markers': 0,
-  'photos': 0
-}
