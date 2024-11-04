e8310551d9c55e82da39b2ab54dbd16f75b22adb
The Map Group <admin@the-map-group.top>
2024-11-04 20:14:57 -0300

    Removed member '160084334@N02'

--- people/160084334@N02/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '160084334@N02',
-  'alias': '160084334@N02',
-  'name': 'Marco Delvecchio',
-  'avatar': '../../icons/photographer.svg',
-  'url': 'https://www.flickr.com/photos/160084334@N02/',
-  'markers': 0,
-  'photos': 0
-}
