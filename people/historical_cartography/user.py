68a436dfdadff9df21dad803e39778b767b82a23
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:06 -0300

    Removed member 'historical_cartography'

--- people/historical_cartography/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '195627800@N03',
-  'alias': 'historical_cartography',
-  'name': 'Historic  Cartography',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/195627800@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/historical_cartography/',
-  'markers': 0,
-  'photos': 0
-}
