8beebc63570947f99a2ceb9e0fbe93571f1f3714
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:01 -0300

    Removed member '195078854@N04'

--- people/195078854@N04/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '195078854@N04',
-  'alias': '195078854@N04',
-  'name': 'Mohammad Ruhul Amin',
-  'avatar': '../../icons/photographer.svg',
-  'url': 'https://www.flickr.com/photos/195078854@N04/',
-  'markers': 0,
-  'photos': 0
-}
