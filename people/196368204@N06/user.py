ee8a3dc6c1c180993bab83cfeeeab5588e7b70f0
The Map Group <admin@the-map-group.top>
2024-11-04 20:15:02 -0300

    Removed member '196368204@N06'

--- people/196368204@N06/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '196368204@N06',
-  'alias': '196368204@N06',
-  'name': 'Richard Forensky',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/196368204@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/196368204@N06/',
-  'markers': 0,
-  'photos': 0
-}
