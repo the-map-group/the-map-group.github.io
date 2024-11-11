c5193cdaa7de5f135244c86a3e714b5b9c9d69ee
The Map Group <admin@the-map-group.top>
2024-11-11 03:35:12 -0300

    [auto] Updated map for member 'tquist24'

--- people/123895834@N08/countries.py
+++ people/123895834@N08/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'US': ['United States of America', 2978, 3986],
-  'CA': ['Canada', 10, 12],
-  'MX': ['Mexico', 12, 13],
-  'CU': ['Cuba', 8, 9],
-  'BZ': ['Belize', 6, 6],
-  'DO': ['Dominican Republic', 4, 33],
-  'NL': ['The Netherlands', 19, 22],
-  'IT': ['Italy', 167, 185],
-  'KY': ['Cayman Islands', 5, 5],
   'BS': ['Bahamas', 12, 12],
-  'AI': ['Anguilla', 7, 9],
-  'DE': ['Germany', 14, 15],
+  'KY': ['Cayman Islands', 5, 5],
+  'NL': ['The Netherlands', 19, 22],
+  'CU': ['Cuba', 8, 9],
   'PR': ['Puerto Rico', 4, 4],
-  'GB': ['United Kingdom', 99, 118]
+  'IT': ['Italy', 168, 186],
+  'AI': ['Anguilla', 7, 9],
+  'US': ['United States of America', 2978, 3986],
+  'BZ': ['Belize', 6, 6],
+  'DE': ['Germany', 14, 15],
+  'MX': ['Mexico', 12, 13],
+  'GB': ['United Kingdom', 99, 118],
+  'CA': ['Canada', 10, 12],
+  'DO': ['Dominican Republic', 4, 33]
 }
--- people/123895834@N08/user.py
+++ people/123895834@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'tquist24',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123895834@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/123895834@N08/',
-  'markers': 3346,
-  'photos': 4432
+  'markers': 3347,
+  'photos': 4433
 }
