071cba4f0977a2222be91460f954340b0d09eb8a
The Map Group <admin@the-map-group.top>
2024-11-12 03:28:34 -0300

    [auto] Removed member 'grundi1'

--- people/grundi1/countries.py
+++
@@ -1,8 +0,0 @@
-countries_dict = {
-  'IE': ['Ireland', 38, 84],
-  'GR': ['Greece', 6, 8],
-  'AT': ['Austria', 189, 672],
-  'IT': ['Italy', 34, 57],
-  'SI': ['Slovenia', 1, 7],
-  'HR': ['Croatia', 13, 36]
-}
--- people/grundi1/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '7182681@N07',
-  'alias': 'grundi1',
-  'name': 'Christian Grundner',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/7182681@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/grundi1/',
-  'markers': 281,
-  'photos': 864
-}
