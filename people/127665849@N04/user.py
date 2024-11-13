5c3fdad7abf6a1c31e96c7a0d9b50913df40d36e
The Map Group <admin@the-map-group.top>
2024-11-13 03:34:59 -0300

    [auto] Updated map for member 'dwhannon.photog'

---
+++ people/127665849@N04/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '127665849@N04',
+  'alias': '127665849@N04',
+  'name': 'David Hannon',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/127665849@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/127665849@N04/',
+  'markers': 0,
+  'photos': 0
+}
