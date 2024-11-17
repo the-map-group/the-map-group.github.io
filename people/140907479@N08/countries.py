f350b8b8a3682d1f609dc373cb6ea32cfaa5a6b4
The Map Group <admin@the-map-group.top>
2024-11-17 03:22:20 -0300

    [auto] Updated map for member 'Crystal.Rain'

--- people/140907479@N08/countries.py
+++ people/140907479@N08/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'FR': ['France', 1, 1],
-  'DE': ['Germany', 8, 8],
-  'QA': ['Qatar', 4, 7],
   'NL': ['Netherlands', 3, 3],
+  'FR': ['France', 1, 1],
+  'US': ['United States of America', 2, 2],
   'RU': ['Russia', 859, 1056],
-  'TR': ['Turkey', 35, 69],
-  'UZ': ['Uzbekistan', 1, 10],
   'AQ': ['Antarctica', 1, 3],
-  'IN': ['India', 8, 8],
-  'US': ['United States of America', 2, 2]
+  'UZ': ['Uzbekistan', 1, 10],
+  'DE': ['Germany', 8, 8],
+  'TR': ['Turkey', 35, 69],
+  'QA': ['Qatar', 4, 7],
+  'IN': ['India', 10, 11]
 }
--- people/140907479@N08/user.py
+++ people/140907479@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dmitry Ivanov',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/140907479@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/140907479@N08/',
-  'markers': 922,
-  'photos': 1167
+  'markers': 924,
+  'photos': 1170
 }
