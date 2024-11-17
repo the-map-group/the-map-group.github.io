2159688f3c2b7fea5852720e5522358bb691ddc4
The Map Group <admin@the-map-group.top>
2024-11-17 03:25:41 -0300

    [auto] Updated map for member 'Gabriel Paladino Ibáñez'

--- people/gabouruguay-collection/countries.py
+++ people/gabouruguay-collection/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
   'PE': ['Peru', 42, 85],
-  'JP': ['Japan', 28, 56],
   'MX': ['Mexico', 26, 295],
-  'ES': ['Spain', 35, 81],
-  'AR': ['Argentina', 44, 130],
   'DO': ['Dominican Republic', 11, 16],
-  'UY': ['Uruguay', 587, 2060],
+  'UY': ['Uruguay', 588, 2061],
+  'AR': ['Argentina', 44, 130],
   'CL': ['Chile', 7, 15],
+  'ES': ['Spain', 35, 81],
+  'JP': ['Japan', 28, 56],
   'ZA': ['South Africa', 57, 130]
 }
--- people/gabouruguay-collection/user.py
+++ people/gabouruguay-collection/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gabriel Paladino Ibáñez',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/93293461@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/gabouruguay-collection/',
-  'markers': 837,
-  'photos': 2868
+  'markers': 838,
+  'photos': 2869
 }
