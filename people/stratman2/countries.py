0de1a863a7200a099b9066b614975efb9578ab66
The Map Group <admin@the-map-group.top>
2024-11-19 06:51:37 -0300

    [auto] Updated map for member 'Stratman2'

--- people/stratman2/countries.py
+++ people/stratman2/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'MY': ['Malaysia', 477, 4381],
   'US': ['United States of America', 1, 1],
-  'SG': ['Singapore', 27, 200],
+  'MW': ['Malawi', 1, 1],
   'ID': ['Indonesia', 14, 46],
-  'MW': ['Malawi', 1, 1]
+  'MY': ['Malaysia', 476, 4380],
+  'SG': ['Singapore', 27, 200]
 }
--- people/stratman2/user.py
+++ people/stratman2/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Stratman2 (taking care of Joey',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8278193@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/stratman2/',
-  'markers': 523,
-  'photos': 4646
+  'markers': 522,
+  'photos': 4645
 }
