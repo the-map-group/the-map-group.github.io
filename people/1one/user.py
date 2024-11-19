b7c2c59209f14bd67238118b073af01301ac0c2c
The Map Group <admin@the-map-group.top>
2024-11-19 06:41:10 -0300

    [auto] Updated map for member '七福神'

--- people/1one/user.py
+++ people/1one/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Shichifuku Jin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/52009851@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/1one/',
-  'markers': 3170,
-  'photos': 37173
+  'markers': 3171,
+  'photos': 37174
 }
