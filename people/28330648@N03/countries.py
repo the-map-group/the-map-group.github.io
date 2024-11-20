a001b65ad44d0fa6075e10826a7e43cce2b880db
The Map Group <admin@the-map-group.top>
2024-11-20 03:33:44 -0300

    [auto] Updated map for member 'Florian106'

--- people/28330648@N03/countries.py
+++ people/28330648@N03/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'IT': ['Italy', 231, 289],
-  'AT': ['Austria', 101, 111],
-  'DE': ['Germany', 4319, 4819],
-  'SI': ['Slovenia', 1, 2],
+  'DE': ['Germany', 4322, 4822],
   'FR': ['France', 300, 403],
   'HR': ['Croatia', 140, 151],
-  'CH': ['Switzerland', 5, 5]
+  'CH': ['Switzerland', 5, 5],
+  'AT': ['Austria', 101, 111],
+  'SI': ['Slovenia', 1, 2]
 }
--- people/28330648@N03/user.py
+++ people/28330648@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Florian106',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28330648@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/28330648@N03/',
-  'markers': 5097,
-  'photos': 5780
+  'markers': 5100,
+  'photos': 5783
 }
