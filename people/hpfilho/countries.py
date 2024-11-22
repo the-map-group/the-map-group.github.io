915ef2d0d5f7ef8982a297003c4030526297375d
The Map Group <admin@the-map-group.top>
2024-11-22 10:51:16 -0300

    Reset map for member 'hpfilho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'VA': ['Vatican City', 22, 31],
-  'CL': ['Chile', 78, 198],
-  'HR': ['Croatia', 184, 243],
-  'IT': ['Italy', 77, 126],
-  'PY': ['Paraguay', 1, 1],
+  'AR': ['Argentina', 89, 160],
   'UY': ['Uruguay', 36, 67],
+  'PY': ['Paraguay', 1, 1],
+  'HR': ['Croatia', 184, 243],
   'US': ['United States of America', 50, 132],
+  'IT': ['Italy', 77, 126],
+  'CL': ['Chile', 78, 198],
   'BR': ['Brazil', 176, 394],
-  'AR': ['Argentina', 88, 160]
+  'VA': ['Vatican City', 22, 31]
 }
--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Haraldo Albergaria',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
-  'markers': 712,
+  'markers': 713,
   'photos': 1352
 }
