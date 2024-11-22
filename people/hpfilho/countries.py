f59f0cc502068be57463f2e5c1565adbd4626416
The Map Group <admin@the-map-group.top>
2024-11-22 03:00:27 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'US': ['United States of America', 50, 132],
   'IT': ['Italy', 77, 126],
+  'UY': ['Uruguay', 36, 67],
   'PY': ['Paraguay', 1, 1],
   'BR': ['Brazil', 176, 394],
-  'CL': ['Chile', 78, 198],
-  'AR': ['Argentina', 81, 152],
-  'UY': ['Uruguay', 36, 67],
+  'VA': ['Vatican City', 22, 31],
   'HR': ['Croatia', 184, 243],
-  'VA': ['Vatican City', 22, 31]
+  'AR': ['Argentina', 84, 155],
+  'US': ['United States of America', 50, 132],
+  'CL': ['Chile', 78, 198]
 }
--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Haraldo Albergaria',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
-  'markers': 705,
-  'photos': 1344
+  'markers': 708,
+  'photos': 1347
 }
