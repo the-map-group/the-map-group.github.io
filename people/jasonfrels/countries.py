c572a7e21dfe35546220bcbb9c02a8c73ffd76f6
The Map Group <admin@the-map-group.top>
2024-11-27 03:03:57 -0300

    [auto] Updated map for member 'Jason Frels'

--- people/jasonfrels/countries.py
+++ people/jasonfrels/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
+  'US': ['United States', 115, 136],
   'NO': ['Norway', 19, 19],
   'SG': ['Singapore', 1, 1],
-  'AR': ['Argentina', 1, 1],
-  'CL': ['Chile', 9, 10],
   'CA': ['Canada', 4, 5],
-  'US': ['United States', 115, 135]
+  'AR': ['Argentina', 1, 1],
+  'CL': ['Chile', 9, 10]
 }
--- people/jasonfrels/user.py
+++ people/jasonfrels/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/76199006@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/jasonfrels/',
   'markers': 150,
-  'photos': 172
+  'photos': 173
 }
