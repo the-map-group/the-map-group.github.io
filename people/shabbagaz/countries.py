75739f168cc800e14870cecbddc6328eae0984b6
The Map Group <admin@the-map-group.top>
2024-12-01 03:19:57 -0300

    [auto] Updated map for member 'Gaz 1984'

--- people/shabbagaz/countries.py
+++ people/shabbagaz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'GB': ['United Kingdom', 12620, 18505],
   'IE': ['Ireland', 54, 58],
   'DE': ['Germany', 25, 230],
-  'ES': ['Spain', 21, 103],
-  'GB': ['United Kingdom', 12620, 18504]
+  'ES': ['Spain', 21, 103]
 }
--- people/shabbagaz/user.py
+++ people/shabbagaz/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33352310@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/shabbagaz/',
   'markers': 12720,
-  'photos': 18895
+  'photos': 18896
 }
