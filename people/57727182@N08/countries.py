910b6929e5cc526b0d4d4e9cd50bebdde50c10e1
The Map Group <admin@the-map-group.top>
2024-12-02 04:12:20 -0300

    [auto] Updated map for member 'bobbyblack51'

--- people/57727182@N08/countries.py
+++ people/57727182@N08/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'GB': ['United Kingdom', 232, 12269],
-  'AT': ['Austria', 10, 65],
-  'CZ': ['Czechia', 45, 360],
   'DE': ['Germany', 76, 501],
   'CH': ['Switzerland', 13, 78],
-  'IT': ['Italy', 1, 1],
   'BE': ['Belgium', 11, 76],
-  'LU': ['Luxembourg', 1, 8],
+  'GB': ['United Kingdom', 232, 12219],
+  'FR': ['France', 31, 99],
   'NL': ['Netherlands', 12, 62],
-  'PL': ['Poland', 1, 5],
   'IE': ['Ireland', 24, 553],
-  'FR': ['France', 31, 99]
+  'PL': ['Poland', 1, 5],
+  'AT': ['Austria', 10, 65],
+  'IT': ['Italy', 1, 1],
+  'LU': ['Luxembourg', 1, 8],
+  'CZ': ['Czechia', 45, 360]
 }
--- people/57727182@N08/user.py
+++ people/57727182@N08/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/57727182@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/57727182@N08/',
   'markers': 457,
-  'photos': 14077
+  'photos': 14027
 }
