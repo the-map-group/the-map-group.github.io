191a3e7d1891829324ecd6de251405e6b2e18237
The Map Group <admin@the-map-group.top>
2024-12-02 14:12:57 -0300

    [auto] Updated map for member 'HereIsTom'

--- people/hereistom/countries.py
+++ people/hereistom/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
+  'BE': ['Belgium', 3, 3],
   'SI': ['Slovenia', 1, 1],
-  'NL': ['The Netherlands', 1212, 1316],
+  'TR': ['Turkey', 63, 63],
   'BR': ['Brazil', 2, 2],
-  'IT': ['Italy', 1, 1],
   'GR': ['Greece', 1, 1],
-  'DE': ['Germany', 2, 2],
-  'TH': ['Thailand', 48, 56],
-  'TR': ['Turkey', 63, 65],
-  'HR': ['Croatia', 1, 1],
-  'ES': ['Spain', 49, 49],
+  'IT': ['Italy', 1, 1],
   'PT': ['Portugal', 95, 96],
-  'BE': ['Belgium', 3, 3]
+  'ES': ['Spain', 49, 49],
+  'TH': ['Thailand', 48, 56],
+  'HR': ['Croatia', 1, 1],
+  'DE': ['Germany', 2, 2],
+  'NL': ['Netherlands', 1212, 1312]
 }
--- people/hereistom/user.py
+++ people/hereistom/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/47035764@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/hereistom/',
   'markers': 1478,
-  'photos': 1593
+  'photos': 1587
 }
