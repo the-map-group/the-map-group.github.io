ea0d0187ff8edb8bdb25d281077c02acbab815c3
The Map Group <admin@the-map-group.top>
2024-12-02 13:49:44 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/user.py
+++ people/diwan/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/81504125@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/diwan/',
   'markers': 8370,
-  'photos': 8895
+  'photos': 8886
 }
