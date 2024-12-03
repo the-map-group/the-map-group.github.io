2315eafcb0087b56a92e8c1b8b077bb74039c5f4
The Map Group <admin@the-map-group.top>
2024-12-03 03:19:39 -0300

    [auto] Updated map for member 'jeanpaulneveu'

--- people/didifiosi/user.py
+++ people/didifiosi/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'JEAN PAUL NEVEU',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/131237713@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/didifiosi/',
-  'markers': 41,
-  'photos': 1508
+  'markers': 37,
+  'photos': 1372
 }
