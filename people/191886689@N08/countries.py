a60878e887ff08fd9c7ad56d64d612ad727e649b
The Map Group <admin@the-map-group.top>
2024-12-03 03:17:24 -0300

    [auto] Updated map for member 'MesphotosAmateur.'

--- people/191886689@N08/countries.py
+++ people/191886689@N08/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'PT': ['Portugal', 6, 7],
   'FR': ['France', 120, 135],
-  'IT': ['Italy', 9, 11]
+  'IT': ['Italy', 9, 11],
+  'PT': ['Portugal', 5, 6]
 }
--- people/191886689@N08/user.py
+++ people/191886689@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'MesphotosAmateur.',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/191886689@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/191886689@N08/',
-  'markers': 135,
-  'photos': 153
+  'markers': 134,
+  'photos': 152
 }
