dcdd3b07df3fcf940a1a6dee0f4aa97f68a4d0a7
The Map Group <admin@the-map-group.top>
2024-12-03 00:05:03 -0300

    [auto] Updated map for member 'mikaelsoderberg'

--- people/mikaelsoederberg/countries.py
+++ people/mikaelsoederberg/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
+  'DE': ['Germany', 6, 6],
   'CH': ['Switzerland', 3, 4],
   'DK': ['Denmark', 4, 4],
-  'SE': ['Sweden', 228, 289],
-  'DE': ['Germany', 6, 6],
-  'IT': ['Italy', 26, 29]
+  'IT': ['Italy', 26, 29],
+  'SE': ['Sweden', 229, 290]
 }
--- people/mikaelsoederberg/user.py
+++ people/mikaelsoederberg/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'mikaelsoderberg',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/35130206@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/mikaelsoederberg/',
-  'markers': 267,
-  'photos': 332
+  'markers': 268,
+  'photos': 333
 }
