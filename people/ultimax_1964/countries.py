0287af2c2a4716c53bfd0a32f11db49df8c98589
The Map Group <admin@the-map-group.top>
2024-12-03 00:46:37 -0300

    [auto] Updated map for member 'Steve W3'

--- people/ultimax_1964/countries.py
+++ people/ultimax_1964/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'GB': ['United Kingdom', 30, 35],
+  'AU': ['Australia', 149, 428],
   'ES': ['Spain', 3, 3],
-  'PT': ['Portugal', 4, 6],
-  'FR': ['France', 12, 17],
   'GL': ['Greenland', 10, 23],
-  'AU': ['Australia', 144, 423],
-  'NZ': ['New Zealand', 8, 23],
+  'GB': ['United Kingdom', 30, 35],
+  'FR': ['France', 12, 17],
+  'CA': ['Canada', 4, 13],
   'US': ['United States of America', 11, 29],
   'JP': ['Japan', 6, 26],
   'IS': ['Iceland', 12, 46],
+  'SG': ['Singapore', 2, 3],
+  'PT': ['Portugal', 4, 6],
   'NO': ['Norway', 6, 13],
-  'CA': ['Canada', 4, 13],
-  'SG': ['Singapore', 2, 3]
+  'NZ': ['New Zealand', 8, 23]
 }
--- people/ultimax_1964/user.py
+++ people/ultimax_1964/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Steve Weston',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/58435673@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/ultimax_1964/',
-  'markers': 252,
-  'photos': 660
+  'markers': 257,
+  'photos': 665
 }
