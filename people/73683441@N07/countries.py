f06d1d293af0ea4d9a9f4de1d2faa3f8d77e3e53
The Map Group <admin@the-map-group.top>
2024-12-03 02:37:01 -0300

    [auto] Updated map for member 'Gerald (Wayne) Prout'

--- people/73683441@N07/countries.py
+++ people/73683441@N07/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'PT': ['Portugal', 110, 174],
-  'AU': ['Australia', 69, 100],
-  'FJ': ['Fiji', 9, 20],
-  'NZ': ['New Zealand', 54, 174],
+  'CA': ['Canada', 2190, 5057],
+  'US': ['United States of America', 1685, 3377],
+  'FR': ['France', 26, 31],
   'ES': ['Spain', 29, 31],
-  'FR': ['France', 27, 32],
-  'CA': ['Canada', 2193, 5078],
-  'GI': ['Gibraltar', 2, 4],
-  'US': ['United States of America', 1686, 3377]
+  'NZ': ['New Zealand', 54, 174],
+  'PT': ['Portugal', 110, 174],
+  'FJ': ['Fiji', 9, 20],
+  'AU': ['Australia', 69, 100],
+  'GI': ['Gibraltar', 2, 4]
 }
