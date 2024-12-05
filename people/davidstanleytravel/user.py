4911c9e309d5e56116f010b88988ce3980107276
The Map Group <admin@the-map-group.top>
2024-12-04 23:05:17 -0300

    Reset map for member 'davidstanleytravel'

--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'David Stanley',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79721788@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
-  'markers': 7439,
-  'photos': 8637
+  'markers': 7441,
+  'photos': 8639
 }
