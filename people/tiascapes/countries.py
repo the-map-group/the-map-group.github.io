b27d609e38a6163b17a10a58a07f615ab8b99f43
The Map Group <admin@the-map-group.top>
2024-12-06 03:30:24 -0300

    [auto] Updated map for member 'TIA International Photography'

--- people/tiascapes/countries.py
+++ people/tiascapes/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GH': ['Ghana', 2, 2],
   'GB': ['United Kingdom', 21, 21],
-  'US': ['United States of America', 620, 964],
-  'FR': ['France', 23, 23],
-  'AU': ['Australia', 11, 12],
-  'CN': ['China', 51, 51],
-  'MY': ['Malaysia', 7, 8],
   'CA': ['Canada', 84, 100],
-  'SG': ['Singapore', 7, 7],
+  'FR': ['France', 23, 23],
+  'CN': ['China', 51, 51],
+  'US': ['United States of America', 620, 964],
   'MX': ['Mexico', 4, 4],
-  'AR': ['Argentina', 41, 43]
+  'SG': ['Singapore', 7, 7],
+  'AR': ['Argentina', 41, 43],
+  'AU': ['Australia', 11, 12],
+  'MY': ['Malaysia', 7, 8],
+  'GH': ['Ghana', 2, 2]
 }
