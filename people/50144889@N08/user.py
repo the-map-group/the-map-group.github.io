69ef31c2effa4efc4cad345703069644bcbe66a5
The Map Group <admin@the-map-group.top>
2024-12-06 06:13:11 -0300

    [auto] Updated map for member 'Robert.Pittman'

--- people/50144889@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '50144889@N08',
-  'alias': '50144889@N08',
-  'name': 'Robert Pittman',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/50144889@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/50144889@N08/',
-  'markers': 2404,
-  'photos': 2602
-}
