86601b190c22824bc12c764449a319412ba2c50a
The Map Group <admin@the-map-group.top>
2024-12-05 21:01:37 -0300

    [auto] Updated map for member 'karen_ds1101'

--- people/karen_ds1101/countries.py
+++ people/karen_ds1101/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'US': ['United States of America', 11, 19],
-  'SG': ['Singapore', 66, 78],
-  'NZ': ['New Zealand', 10, 10],
-  'JP': ['Japan', 10, 10],
-  'GB': ['United Kingdom', 15, 16],
-  'IT': ['Italy', 5, 6],
-  'BN': ['Brunei', 2, 3],
-  'DE': ['Germany', 1, 1],
-  'IE': ['Ireland', 3, 3],
-  'AU': ['Australia', 911, 1036],
   'NO': ['Norway', 4, 7],
   'CN': ['China', 79, 125],
   'CL': ['Chile', 5, 9],
   'MY': ['Malaysia', 8, 11],
-  'PE': ['Peru', 11, 16]
+  'GB': ['United Kingdom', 15, 16],
+  'IE': ['Ireland', 3, 3],
+  'DE': ['Germany', 1, 1],
+  'US': ['United States of America', 11, 19],
+  'IT': ['Italy', 5, 6],
+  'NZ': ['New Zealand', 10, 10],
+  'BN': ['Brunei', 2, 3],
+  'AU': ['Australia', 911, 1036],
+  'SG': ['Singapore', 66, 78],
+  'PE': ['Peru', 11, 16],
+  'JP': ['Japan', 10, 10]
 }
