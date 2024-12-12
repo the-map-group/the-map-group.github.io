bd5911354f571afabe71527965234146c12349e9
The Map Group <admin@the-map-group.top>
2024-12-12 20:04:42 -0300

    [auto] Updated map for member 'Quel.R.R'

--- people/quelrr/countries.py
+++ people/quelrr/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'AR': ['Argentina', 3, 3],
-  'US': ['United States of America', 4, 4],
   'HR': ['Croatia', 2, 2],
+  'BR': ['Brazil', 22, 35],
   'IT': ['Italy', 3, 3],
-  'VA': ['Vatican City', 1, 1],
-  'BR': ['Brazil', 22, 35]
+  'US': ['United States', 4, 4],
+  'AR': ['Argentina', 3, 3],
+  'VA': ['Vatican City', 1, 1]
 }
--- people/quelrr/user.py
+++ people/quelrr/user.py
@@ -5,5 +5,6 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/132838898@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/quelrr/',
   'markers': 35,
-  'photos': 48
+  'photos': 48,
+  'countries': 6
 }
