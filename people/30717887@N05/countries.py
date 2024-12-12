c56c9ade10e1809ab8a903f4f3456e76292dfd15
The Map Group <admin@the-map-group.top>
2024-12-12 15:57:10 -0300

    [auto] Updated map for member 'Tor Einar Andersen'

--- people/30717887@N05/countries.py
+++ people/30717887@N05/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'PL': ['Poland', 4, 5],
-  'DK': ['Denmark', 19, 25],
-  'ES': ['Spain', 29, 37],
-  'FR': ['France', 7, 7],
-  'DE': ['Germany', 2, 2],
-  'IE': ['Ireland', 1, 1],
-  'IT': ['Italy', 39, 49],
-  'VA': ['Vatican City', 4, 4],
-  'GB': ['United Kingdom', 21, 32],
   'NL': ['Netherlands', 1, 1],
+  'FR': ['France', 7, 7],
   'SE': ['Sweden', 3, 4],
-  'NO': ['Norway', 221, 266]
+  'IT': ['Italy', 42, 54],
+  'IE': ['Ireland', 1, 1],
+  'ES': ['Spain', 29, 37],
+  'GB': ['United Kingdom', 21, 32],
+  'VA': ['Vatican City', 4, 4],
+  'NO': ['Norway', 221, 266],
+  'DK': ['Denmark', 15, 21],
+  'DE': ['Germany', 2, 2],
+  'PL': ['Poland', 4, 5]
 }
--- people/30717887@N05/user.py
+++ people/30717887@N05/user.py
@@ -4,6 +4,7 @@ user_info = {
   'name': 'Tor Einar Andersen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30717887@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/30717887@N05/',
-  'markers': 351,
-  'photos': 433
+  'markers': 354,
+  'photos': 438,
+  'countries': 12
 }
