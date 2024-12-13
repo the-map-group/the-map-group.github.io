5960f8faee51e2df775b6844b3d1ee137c00ab68
The Map Group <admin@the-map-group.top>
2024-12-13 10:55:22 -0300

    [auto] Updated map for member 'Gray Moon Gallery'

--- people/26915283@N07/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '26915283@N07',
-  'alias': '26915283@N07',
-  'name': 'Gray Moon Gallery',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/26915283@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/26915283@N07/',
-  'markers': 180,
-  'photos': 200
-}
