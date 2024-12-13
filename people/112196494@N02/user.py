d1a672c8cdc68158fadae69d34b52eecb15248ee
The Map Group <admin@the-map-group.top>
2024-12-13 10:55:37 -0300

    [auto] Updated map for member 'thunderbird-72'

--- people/112196494@N02/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '112196494@N02',
-  'alias': '112196494@N02',
-  'name': 'thunderbird-72',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/112196494@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/112196494@N02/',
-  'markers': 1426,
-  'photos': 2743
-}
