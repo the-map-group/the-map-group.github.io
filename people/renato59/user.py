fda653c87ce74e4f831ac7efbce38abfab1805e2
The Map Group <admin@the-map-group.top>
2024-12-13 10:58:51 -0300

    [auto] Updated map for member 'ReNato '59'

--- people/renato59/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '87940223@N08',
-  'alias': 'renato59',
-  'name': 'ReNato \'59',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/87940223@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/renato59/',
-  'markers': 479,
-  'photos': 1635
-}
