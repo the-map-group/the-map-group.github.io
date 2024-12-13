b255454a3691eb5698782c4e760bcdc0972d2872
The Map Group <admin@the-map-group.top>
2024-12-13 10:59:52 -0300

    [auto] Updated map for member 'mouzhik'

--- people/zemzem/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '67878469@N00',
-  'alias': 'zemzem',
-  'name': 'mouzhik',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/67878469@N00_r.jpg',
-  'url': 'https://www.flickr.com/photos/zemzem/',
-  'markers': 288,
-  'photos': 422
-}
