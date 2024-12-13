fab981225530db70adcd2c42d6364db1d13502c0
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:56 -0300

    [auto] Updated map for member 'marionkaminski'

--- people/147422800@N02/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '147422800@N02',
-  'alias': '147422800@N02',
-  'name': 'Marion Kaminski',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/147422800@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/147422800@N02/',
-  'markers': 176,
-  'photos': 302
-}
