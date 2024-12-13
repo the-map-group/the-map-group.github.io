776a5a231a593f4973c0c6ca5c1ce1dde742c349
The Map Group <admin@the-map-group.top>
2024-12-13 10:58:28 -0300

    [auto] Updated map for member 'enrico377'

--- people/enricoveneruso/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '193758259@N04',
-  'alias': 'enricoveneruso',
-  'name': 'Enrico Veneruso',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/193758259@N04_r.jpg',
-  'url': 'https://www.flickr.com/photos/enricoveneruso/',
-  'markers': 316,
-  'photos': 319
-}
