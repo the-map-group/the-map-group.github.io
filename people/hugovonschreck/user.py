b8a2232ad27b6b1bad20364f992737379ad7b991
The Map Group <admin@the-map-group.top>
2024-12-13 10:52:50 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '74758246@N08',
-  'alias': 'hugovonschreck',
-  'name': 'Heinz Dieter  Heil',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/74758246@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/hugovonschreck/',
-  'markers': 1567,
-  'photos': 6282
-}
