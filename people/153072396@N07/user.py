4e3697446778e545623e72aec4b9752bf4124d1a
The Map Group <admin@the-map-group.top>
2024-12-13 10:54:29 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '153072396@N07',
-  'alias': '153072396@N07',
-  'name': 'dominique FILLION',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/153072396@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/153072396@N07/',
-  'markers': 282,
-  'photos': 636
-}
