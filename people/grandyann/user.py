5638a811a5526b7b8896a6d2a9dc71a152e074e0
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:52 -0300

    [auto] Updated map for member 'grand Yann'

--- people/grandyann/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '24482362@N05',
-  'alias': 'grandyann',
-  'name': 'Yann Pinczon du Sel',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/24482362@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/grandyann/',
-  'markers': 877,
-  'photos': 1161
-}
