feda1c3c5225d778a612adaf11d06e5ced29a7b1
The Map Group <admin@the-map-group.top>
2024-12-13 10:56:34 -0300

    [auto] Updated map for member 'acase1968'

--- people/60035031@N06/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '60035031@N06',
-  'alias': '60035031@N06',
-  'name': 'Al Case',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/60035031@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/60035031@N06/',
-  'markers': 1450,
-  'photos': 1645
-}
