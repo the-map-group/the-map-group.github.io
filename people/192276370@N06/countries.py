5894085045a8d157f96b80eb170743936c798626
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:21 -0300

    [auto] Updated map for member 'Patrice Dardoize'

--- people/192276370@N06/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'FR': ['France', 609, 764],
-  'IS': ['Iceland', 23, 26]
-}
--- people/192276370@N06/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '192276370@N06',
-  'alias': '192276370@N06',
-  'name': 'Patrice Dardoize',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/192276370@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/192276370@N06/',
-  'markers': 632,
-  'photos': 790
-}
