452fc72c55fb91a2a7035f4331b2568170bdc95e
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:13 -0300

    [auto] Updated map for member 'karsaiag'

--- people/120304125@N06/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '120304125@N06',
-  'alias': '120304125@N06',
-  'name': 'András Karsai',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/120304125@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/120304125@N06/',
-  'markers': 356,
-  'photos': 358
-}
