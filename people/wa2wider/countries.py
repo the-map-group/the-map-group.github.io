30fd943af5cf2137a99a2e021aa765ac32e8807f
The Map Group <admin@the-map-group.top>
2024-12-13 10:59:02 -0300

    [auto] Updated map for member 'wa2wider'

--- people/wa2wider/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'CA': ['Canada', 203, 226],
-  'CH': ['Switzerland', 13, 13]
-}
--- people/wa2wider/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '15360731@N07',
-  'alias': 'wa2wider',
-  'name': 'wa2wider',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/15360731@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/wa2wider/',
-  'markers': 216,
-  'photos': 239
-}
