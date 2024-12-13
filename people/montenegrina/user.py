716db722359452f28878d73a6c20107a2b5e3ca1
The Map Group <admin@the-map-group.top>
2024-12-13 10:55:49 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '92496593@N08',
-  'alias': 'montenegrina',
-  'name': 'ForceMajeureMontenegro',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/92496593@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/montenegrina/',
-  'markers': 1349,
-  'photos': 1532
-}
