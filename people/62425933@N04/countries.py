32e939b78516b871ef681e9e8f0606f247e23cc7
The Map Group <admin@the-map-group.top>
2024-12-13 10:59:55 -0300

    [auto] Updated map for member 'Wes Iversen'

--- people/62425933@N04/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'US': ['United States', 2231, 3410],
-  'CA': ['Canada', 6, 8]
-}
--- people/62425933@N04/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '62425933@N04',
-  'alias': '62425933@N04',
-  'name': 'Wes Iversen',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/62425933@N04_r.jpg',
-  'url': 'https://www.flickr.com/photos/62425933@N04/',
-  'markers': 2239,
-  'photos': 3420
-}
