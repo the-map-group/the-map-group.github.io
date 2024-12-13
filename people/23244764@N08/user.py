004233a966f7717eb7cc81855b81e7fd007daf28
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:06 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '23244764@N08',
-  'alias': '23244764@N08',
-  'name': 'Jim Smith',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/23244764@N08/',
-  'markers': 281,
-  'photos': 532
-}
