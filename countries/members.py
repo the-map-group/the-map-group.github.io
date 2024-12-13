021391659c24646b68611dcbdc760f95a05652a4
The Map Group <admin@the-map-group.top>
2024-12-13 11:22:21 -0300

    [auto] Updated map for member 'haroldmoses'

--- countries/members.py
+++ countries/members.py
@@ -1,4 +1,5 @@
 members_dict = {
+  'MA': [['57729348@N02', 'mosesharold', 'Harold  Moses', 'https://live.staticflickr.com/5674/buddyicons/57729348@N02_r.jpg', 5, 5]],
   'DK': [['8920963@N03', '8920963@N03', 'Steen Jeppesen', 'https://live.staticflickr.com/5674/buddyicons/8920963@N03_r.jpg', 1, 1]],
   'PT': [['51839301@N00', '51839301@N00', 'Hans Pohl', 'https://live.staticflickr.com/5674/buddyicons/51839301@N00_r.jpg', 1, 1]]
 }
--- people/mosesharold/user.py
+++ people/mosesharold/user.py
@@ -4,7 +4,7 @@ user_info = {
   'name': 'Harold  Moses',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/57729348@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/mosesharold/',
-  'markers': 3351,
-  'photos': 4667,
-  'countries': 70
+  'markers': 5,
+  'photos': 5,
+  'countries': 1
 }
