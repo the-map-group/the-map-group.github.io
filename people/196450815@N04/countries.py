21b218a3f273014cb8669ddd097b273038a01214
The Map Group <admin@the-map-group.top>
2024-12-13 10:59:26 -0300

    [auto] Updated map for member 'Yabba Link'

--- people/196450815@N04/countries.py
+++
@@ -1,5 +0,0 @@
-countries_dict = {
-  'NZ': ['New Zealand', 2, 2],
-  'AU': ['Australia', 38, 38],
-  'BI': ['Burundi', 3, 5]
-}
--- people/196450815@N04/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '196450815@N04',
-  'alias': '196450815@N04',
-  'name': 'Imbella  Ecos',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/196450815@N04_r.jpg',
-  'url': 'https://www.flickr.com/photos/196450815@N04/',
-  'markers': 43,
-  'photos': 45
-}
