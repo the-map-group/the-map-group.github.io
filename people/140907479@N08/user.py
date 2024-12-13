f0bd332b6e5d2cc0af6acdb59bf4fc36869da4f2
The Map Group <admin@the-map-group.top>
2024-12-13 10:56:26 -0300

    [auto] Updated map for member 'Crystal.Rain'

--- people/140907479@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '140907479@N08',
-  'alias': '140907479@N08',
-  'name': 'Dmitry Ivanov',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/140907479@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/140907479@N08/',
-  'markers': 919,
-  'photos': 1171
-}
