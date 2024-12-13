139bcdd613d0a783aa66060f489e5c56e5a8864c
The Map Group <admin@the-map-group.top>
2024-12-13 11:33:24 -0300

    [auto] Updated map for member 'Elena Lebedeva (IG: ellebedeva_)'

---
+++ people/ellebedeva/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'HU': ['Hungary', 1, 1],
+  'GB': ['United Kingdom', 3, 3],
+  'RU': ['Russia', 109, 115],
+  'FI': ['Finland', 12, 13],
+  'KZ': ['Kazakhstan', 16, 16],
+  'ES': ['Spain', 34, 36],
+  'MT': ['Malta', 2, 3],
+  'CZ': ['Czech Republic', 5, 5],
+  'IT': ['Italy', 4, 4],
+  'MA': ['Morocco', 1, 1],
+  'BY': ['Belarus', 1, 1],
+  'EG': ['Egypt', 1, 1],
+  'AT': ['Austria', 2, 2],
+  'GR': ['Greece', 3, 3],
+  'LI': ['Liechtenstein', 1, 1],
+  'CH': ['Switzerland', 12, 12],
+  'MN': ['Mongolia', 1, 1]
+}
