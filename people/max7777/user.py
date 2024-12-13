868b1ccfa2fdd71b64e502563b4f5cff9fb3e1ca
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:33 -0300

    [auto] Updated map for member 'OMNIBUS DUBITANDUM'

--- people/max7777/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '30866147@N05',
-  'alias': 'max7777',
-  'name': 'OMNIBUS DUBITANDUM',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/30866147@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/max7777/',
-  'markers': 68,
-  'photos': 94
-}
