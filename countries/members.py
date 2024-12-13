1ef7cc2b33a6673bbc1d43d719efd01b60adbc68
The Map Group <admin@the-map-group.top>
2024-12-13 14:49:00 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- countries/members.py
+++ countries/members.py
@@ -1,11 +1,18 @@
 members_dict = {
-  'BR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 176, 394]],
-  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126]],
-  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132]],
-  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 90, 162]],
-  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31]],
   'HR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 184, 243]],
+  'NZ': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 9, 9]],
   'UY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 36, 67]],
-  'PY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 1, 1]],
-  'CL': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 78, 198]]
+  'CL': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 78, 198]],
+  'BR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 176, 394]],
+  'GB': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 26, 28]],
+  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 706, 981]],
+  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 25, 32]],
+  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 90, 162]],
+  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 186, 245]],
+  'AU': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 11, 11]],
+  'PA': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 1, 2]],
+  'CA': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 90, 158]],
+  'RU': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 1, 1]],
+  'CH': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 6, 9]],
+  'PY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 1, 1]]
 }
