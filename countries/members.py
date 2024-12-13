05661a393f6cc01f81d3b3e8238fd5994d95f78f
The Map Group <admin@the-map-group.top>
2024-12-13 11:29:56 -0300

    [auto] Updated map for member 'H. P. Filho'

--- countries/members.py
+++ countries/members.py
@@ -1,5 +1,11 @@
 members_dict = {
-  'MA': [['57729348@N02', 'mosesharold', 'Harold  Moses', 'https://live.staticflickr.com/5674/buddyicons/57729348@N02_r.jpg', 5, 5]],
-  'DK': [['8920963@N03', '8920963@N03', 'Steen Jeppesen', 'https://live.staticflickr.com/5674/buddyicons/8920963@N03_r.jpg', 1, 1]],
-  'PT': [['51839301@N00', '51839301@N00', 'Hans Pohl', 'https://live.staticflickr.com/5674/buddyicons/51839301@N00_r.jpg', 1, 1]]
+  'HR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 184, 243]],
+  'BR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 176, 394]],
+  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31]],
+  'PY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 1, 1]],
+  'UY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 36, 67]],
+  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 90, 162]],
+  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132]],
+  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126]],
+  'CL': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 78, 198]]
 }
