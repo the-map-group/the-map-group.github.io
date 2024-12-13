3279ab94297894fe635c543adad5e2a919413be3
The Map Group <admin@the-map-group.top>
2024-12-13 10:53:32 -0300

    [auto] Updated map for member 'mark.paradox'

--- people/paradox_m/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'IS': ['Iceland', 1, 2],
-  'HR': ['Croatia', 1, 2]
-}
--- people/paradox_m/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '98488245@N03',
-  'alias': 'paradox_m',
-  'name': 'Mark  PARADOX',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/98488245@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/paradox_m/',
-  'markers': 2,
-  'photos': 4
-}
