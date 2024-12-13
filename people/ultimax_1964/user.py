de2ba083086165b99f2aa62a7a2167c7f1eea791
The Map Group <admin@the-map-group.top>
2024-12-13 10:52:57 -0300

    [auto] Updated map for member 'Steve W3'

--- people/ultimax_1964/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '58435673@N08',
-  'alias': 'ultimax_1964',
-  'name': 'Steve Weston',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/58435673@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/ultimax_1964/',
-  'markers': 257,
-  'photos': 665
-}
