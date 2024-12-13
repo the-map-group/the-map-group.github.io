3cfa5134ebe47b648b88ebd1e51171ef5f07f82c
The Map Group <admin@the-map-group.top>
2024-12-12 22:30:48 -0300

    [auto] Updated map for member 'karen_ds1101'

--- people/karen_ds1101/countries.py
+++ people/karen_ds1101/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'NO': ['Norway', 4, 7],
-  'CN': ['China', 79, 125],
-  'CL': ['Chile', 5, 9],
-  'MY': ['Malaysia', 8, 11],
-  'GB': ['United Kingdom', 15, 16],
-  'IE': ['Ireland', 3, 3],
-  'DE': ['Germany', 1, 1],
-  'US': ['United States of America', 11, 19],
   'IT': ['Italy', 5, 6],
+  'CN': ['China', 79, 125],
   'NZ': ['New Zealand', 10, 10],
-  'BN': ['Brunei', 2, 3],
+  'CL': ['Chile', 5, 9],
+  'GB': ['United Kingdom', 15, 16],
+  'DE': ['Germany', 1, 1],
   'AU': ['Australia', 911, 1036],
-  'SG': ['Singapore', 66, 78],
+  'BN': ['Brunei', 2, 3],
+  'MY': ['Malaysia', 8, 11],
+  'IE': ['Ireland', 3, 3],
+  'US': ['United States', 11, 19],
   'PE': ['Peru', 11, 16],
-  'JP': ['Japan', 10, 10]
+  'JP': ['Japan', 10, 10],
+  'NO': ['Norway', 4, 7],
+  'SG': ['Singapore', 66, 78]
 }
--- people/karen_ds1101/user.py
+++ people/karen_ds1101/user.py
@@ -5,5 +5,6 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51073972@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/karen_ds1101/',
   'markers': 1141,
-  'photos': 1350
+  'photos': 1350,
+  'countries': 15
 }
