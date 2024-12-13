ad173eff88fe77a977b9ff23f276cd048341e45d
The Map Group <admin@the-map-group.top>
2024-12-13 01:34:08 -0300

    [auto] Updated map for member 'Francois Le Rumeur'

--- people/100112015@N06/countries.py
+++ people/100112015@N06/countries.py
@@ -1,5 +1,4 @@
 countries_dict = {
-  'PT': ['Portugal', 1, 2],
-  'ES': ['Spain', 1, 1],
-  'FR': ['France', 173, 247]
+  'FR': ['France', 173, 247],
+  'ES': ['Spain', 1, 1]
 }
--- people/100112015@N06/user.py
+++ people/100112015@N06/user.py
@@ -5,5 +5,6 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/100112015@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/100112015@N06/',
   'markers': 175,
-  'photos': 250
+  'photos': 250,
+  'countries': 2
 }
