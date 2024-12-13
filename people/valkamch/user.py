2101a60a8c693c41f9e7efb616dac4ef45cab6a2
The Map Group <admin@the-map-group.top>
2024-12-13 10:56:38 -0300

    [auto] Updated map for member 'ValKamch'

--- people/valkamch/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '143593890@N07',
-  'alias': 'valkamch',
-  'name': 'ValKamch',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/143593890@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/valkamch/',
-  'markers': 4798,
-  'photos': 5556
-}
