26a55ca42d052e48a9f91ea027f767f928d25d43
The Map Group <admin@the-map-group.top>
2024-12-13 05:53:03 -0300

    [auto] Updated map for member 'TIA International Photography'

--- people/tiascapes/countries.py
+++ people/tiascapes/countries.py
@@ -1,13 +1,12 @@
 countries_dict = {
-  'GB': ['United Kingdom', 21, 21],
-  'CA': ['Canada', 84, 100],
-  'FR': ['France', 23, 23],
-  'CN': ['China', 51, 51],
-  'US': ['United States of America', 620, 964],
-  'MX': ['Mexico', 4, 4],
-  'SG': ['Singapore', 7, 7],
   'AR': ['Argentina', 41, 43],
+  'GB': ['United Kingdom', 21, 21],
+  'SG': ['Singapore', 7, 7],
+  'CA': ['Canada', 84, 100],
   'AU': ['Australia', 11, 12],
-  'MY': ['Malaysia', 7, 8],
-  'GH': ['Ghana', 2, 2]
+  'US': ['United States', 624, 968],
+  'CN': ['China', 51, 51],
+  'FR': ['France', 23, 23],
+  'GH': ['Ghana', 2, 2],
+  'MY': ['Malaysia', 7, 8]
 }
--- people/tiascapes/user.py
+++ people/tiascapes/user.py
@@ -5,5 +5,6 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/12744878@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/tiascapes/',
   'markers': 871,
-  'photos': 1235
+  'photos': 1235,
+  'countries': 10
 }
