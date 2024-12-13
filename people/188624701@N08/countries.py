6541ea1ba47f3e152741b6fe20a33aa475aa068b
The Map Group <admin@the-map-group.top>
2024-12-13 10:52:23 -0300

    [auto] Updated map for member 'perunique :)'

--- people/188624701@N08/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'BA': ['Bosnia and Herzegovina', 1, 1],
-  'HR': ['Croatia', 54, 82]
-}
--- people/188624701@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '188624701@N08',
-  'alias': '188624701@N08',
-  'name': 'Vedran Peruničić',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188624701@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/188624701@N08/',
-  'markers': 55,
-  'photos': 83
-}
