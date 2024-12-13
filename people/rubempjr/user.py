3a32595cfbfda9bfc46d113cbeb6da5c79d6b783
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:02 -0300

    [auto] Updated map for member 'Rubem Jr'

--- people/rubempjr/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '38795342@N06',
-  'alias': 'rubempjr',
-  'name': 'Rubem Porto Jr',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38795342@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/rubempjr/',
-  'markers': 7841,
-  'photos': 19976
-}
