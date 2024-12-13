7a5b9dda0502a8d13523de4c3f2fa77edb914a04
The Map Group <admin@the-map-group.top>
2024-12-13 10:57:48 -0300

    [auto] Updated map for member 'Ivor Jones'

--- people/photos-by-ivor/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '74974888@N04',
-  'alias': 'photos-by-ivor',
-  'name': 'Ivor Jones',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/74974888@N04_r.jpg',
-  'url': 'https://www.flickr.com/photos/photos-by-ivor/',
-  'markers': 1641,
-  'photos': 30006
-}
