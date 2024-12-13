fc69df648edb19e5393afa3183b2dc641a426126
The Map Group <admin@the-map-group.top>
2024-12-13 10:56:42 -0300

    [auto] Updated map for member 'SeaLord 50+'

--- people/186479257@N03/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '186479257@N03',
-  'alias': '186479257@N03',
-  'name': 'SeaLord 50+',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/186479257@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/186479257@N03/',
-  'markers': 88,
-  'photos': 99
-}
