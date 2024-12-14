a3cc55fad99a203eba65c02c9ef3123c2d5fb752
The Map Group <admin@the-map-group.top>
2024-12-14 09:24:16 -0300

    [auto] Updated map for member 'Mani_H.'

---
+++ people/manfred_h/countries.py
@@ -0,0 +1,10 @@
+countries_dict = {
+  'GB': ['United Kingdom', 1, 2],
+  'FR': ['France', 22, 31],
+  'SE': ['Sweden', 2, 3],
+  'IT': ['Italy', 1, 1],
+  'DE': ['Germany', 625, 1232],
+  'NL': ['Netherlands', 35, 50],
+  'DK': ['Denmark', 33, 39],
+  'BE': ['Belgium', 5, 5]
+}
---
+++ people/manfred_h/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '165713115@N07',
+  'alias': 'manfred_h',
+  'name': 'Manfred Hermann',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/165713115@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/manfred_h/',
+  'markers': 724,
+  'photos': 1363,
+  'countries': 8
+}
