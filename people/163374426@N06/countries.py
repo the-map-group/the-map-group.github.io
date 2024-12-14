f21a07f3a5f5ac1f7d8cf63b9b6d9ccd524e3476
The Map Group <admin@the-map-group.top>
2024-12-14 06:34:10 -0300

    [auto] Updated map for member 'xray_jets'

---
+++ people/163374426@N06/countries.py
@@ -0,0 +1,11 @@
+countries_dict = {
+  'CL': ['Chile', 5, 7],
+  'NZ': ['New Zealand', 1, 1],
+  'IT': ['Italy', 3, 3],
+  'IS': ['Iceland', 3, 3],
+  'AR': ['Argentina', 2, 2],
+  'KR': ['South Korea', 8, 8],
+  'PE': ['Peru', 2, 2],
+  'JP': ['Japan', 16, 20],
+  'AU': ['Australia', 110, 125]
+}
---
+++ people/163374426@N06/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '163374426@N06',
+  'alias': '163374426@N06',
+  'name': 'Peter K',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/163374426@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/163374426@N06/',
+  'markers': 151,
+  'photos': 172,
+  'countries': 9
+}
