7abf3ee3817385aac991a678d8790847d65f587a
The Map Group <admin@the-map-group.top>
2024-12-14 09:10:38 -0300

    [auto] Updated map for member 'Bernard Spragg'

---
+++ people/volvob12b/countries.py
@@ -0,0 +1,13 @@
+countries_dict = {
+  'CA': ['Canada', 213, 290],
+  'NZ': ['New Zealand', 2618, 3428],
+  'TH': ['Thailand', 22, 25],
+  'PH': ['Philippines', 39, 67],
+  'MY': ['Malaysia', 38, 52],
+  'BN': ['Brunei', 38, 51],
+  'CN': ['China', 147, 179],
+  'JP': ['Japan', 17, 29],
+  'US': ['United States', 275, 421],
+  'AU': ['Australia', 237, 337],
+  'SG': ['Singapore', 10, 11]
+}
---
+++ people/volvob12b/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '88123769@N02',
+  'alias': 'volvob12b',
+  'name': 'Bernard Spragg. NZ',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/volvob12b/',
+  'markers': 3654,
+  'photos': 4890,
+  'countries': 11
+}
