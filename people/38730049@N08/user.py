928b3aca084cab23f3cd1512b5c7bade9214677f
The Map Group <admin@the-map-group.top>
2024-12-14 11:48:19 -0300

    [auto] Updated map for member 'Gmlskis'

---
+++ people/38730049@N08/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '38730049@N08',
+  'alias': '38730049@N08',
+  'name': 'George Landis',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38730049@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/38730049@N08/',
+  'markers': 973,
+  'photos': 58152,
+  'countries': 1
+}
