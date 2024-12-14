76ff7d77f3c0e7123ba66999a6c350b44412c4ff
The Map Group <admin@the-map-group.top>
2024-12-14 10:51:20 -0300

    [auto] Updated map for member 'xd_travel'

---
+++ people/111314495@N05/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '111314495@N05',
+  'alias': '111314495@N05',
+  'name': 'Mike X-d',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/111314495@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/111314495@N05/',
+  'markers': 5443,
+  'photos': 9805,
+  'countries': 57
+}
