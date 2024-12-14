9d5089336a448145824636dd21cf4f07ccb33e9a
The Map Group <admin@the-map-group.top>
2024-12-14 05:30:06 -0300

    [auto] Updated map for member 'Liam Cheasty'

---
+++ people/liamcheasty/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '43413480@N07',
+  'alias': 'liamcheasty',
+  'name': 'Liam Cheasty',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/43413480@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/liamcheasty/',
+  'markers': 3381,
+  'photos': 17574,
+  'countries': 30
+}
