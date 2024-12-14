0bdecc5f29f977231fef2508aeda0fcc22f752b3
The Map Group <admin@the-map-group.top>
2024-12-14 10:53:01 -0300

    [auto] Updated map for member 'Nicolas Hoizey'

---
+++ people/nicolas-hoizey/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '38608514@N00',
+  'alias': 'nicolas-hoizey',
+  'name': 'Nicolas Hoizey',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38608514@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/nicolas-hoizey/',
+  'markers': 359,
+  'photos': 403,
+  'countries': 19
+}
