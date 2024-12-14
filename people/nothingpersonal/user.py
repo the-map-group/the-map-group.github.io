7a52f081463aaf28c54f99f5374de39633d5f870
The Map Group <admin@the-map-group.top>
2024-12-13 23:20:51 -0300

    [auto] Updated map for member 'Dmitry Djouce'

---
+++ people/nothingpersonal/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '46721940@N00',
+  'alias': 'nothingpersonal',
+  'name': 'Dmitry Djouce',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/46721940@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/nothingpersonal/',
+  'markers': 2660,
+  'photos': 2995,
+  'countries': 16
+}
