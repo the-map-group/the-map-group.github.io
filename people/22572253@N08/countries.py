3e5b42668539ca300a004403a6620969a9e1c45b
The Map Group <admin@the-map-group.top>
2024-12-13 22:41:53 -0300

    [auto] Updated map for member 'Northern Tony'

--- people/22572253@N08/countries.py
+++ people/22572253@N08/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'IT': ['Italy', 10, 10],
   'CA': ['Canada', 2, 2],
+  'IT': ['Italy', 10, 10],
   'GB': ['United Kingdom', 69, 69],
   'US': ['United States', 18, 18],
   'VA': ['Vatican City', 1, 1]
