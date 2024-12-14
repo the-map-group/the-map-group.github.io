223f9b9e67906c840f637e5e68805a6159d625b2
The Map Group <admin@the-map-group.top>
2024-12-14 04:49:31 -0300

    [auto] Updated map for member 'Howard Somerville (10 million views)'

---
+++ people/byhowardsomerville/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '14463685@N07',
+  'alias': 'byhowardsomerville',
+  'name': 'Howard Somerville',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/byhowardsomerville/',
+  'markers': 794,
+  'photos': 943,
+  'countries': 36
+}
