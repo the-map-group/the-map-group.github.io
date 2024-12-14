21207725ec1a7f3419fda69314c2d7f256297d80
The Map Group <admin@the-map-group.top>
2024-12-14 04:44:23 -0300

    [auto] Updated map for member 'jamesgreig@dr.com'

---
+++ people/jimgreig/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'GB': ['United Kingdom', 747, 915],
+  'NL': ['Netherlands', 6, 6]
+}
---
+++ people/jimgreig/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '38286807@N03',
+  'alias': 'jimgreig',
+  'name': 'James Greig',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38286807@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/jimgreig/',
+  'markers': 754,
+  'photos': 922,
+  'countries': 2
+}
