ec4695f2ff326acaabd3356486443917b96d88a4
The Map Group <admin@the-map-group.top>
2024-12-14 09:04:54 -0300

    [auto] Updated map for member 'peter.rath'

---
+++ people/peterrath/countries.py
@@ -0,0 +1,15 @@
+countries_dict = {
+  'NO': ['Norway', 6, 9],
+  'NL': ['Netherlands', 2, 10],
+  'GB': ['United Kingdom', 1, 3],
+  'BS': ['Bahamas', 1, 2],
+  'DK': ['Denmark', 1, 1],
+  'PT': ['Portugal', 2, 8],
+  'AT': ['Austria', 6, 7],
+  'PL': ['Poland', 1, 4],
+  'GR': ['Greece', 16, 60],
+  'DE': ['Germany', 223, 314],
+  'ES': ['Spain', 79, 103],
+  'TR': ['Turkey', 29, 107],
+  'EG': ['Egypt', 24, 83]
+}
