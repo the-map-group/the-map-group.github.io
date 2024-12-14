0de91f7a596f3966ac493cdda956f62bb44366e2
The Map Group <admin@the-map-group.top>
2024-12-14 02:28:15 -0300

    [auto] Updated map for member 'javiernoval'

---
+++ people/jnoval/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '66031636@N00',
+  'alias': 'jnoval',
+  'name': 'Javier Noval',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/66031636@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/jnoval/',
+  'markers': 383,
+  'photos': 617,
+  'countries': 13
+}
