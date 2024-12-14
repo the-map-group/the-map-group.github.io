26fc4dd4adf7524d64733fb7848587c51ed499c0
The Map Group <admin@the-map-group.top>
2024-12-14 00:52:21 -0300

    [auto] Updated map for member 'ManxieB- sorry, struggling!'

---
+++ people/barbaraelaw/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'GB': ['United Kingdom', 2393, 2951],
+  'IE': ['Ireland', 35, 53],
+  'TZ': ['Tanzania', 59, 318]
+}
---
+++ people/barbaraelaw/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '25404639@N08',
+  'alias': 'barbaraelaw',
+  'name': 'Barbara Lawrence',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/barbaraelaw/',
+  'markers': 2487,
+  'photos': 3322,
+  'countries': 3
+}
