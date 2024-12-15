6f5ebbef06d4787f427b7cddc5ee2d6a1e12a9bb
The Map Group <admin@the-map-group.top>
2024-12-15 14:09:21 -0300

    [auto] Updated map for member 'enrico sprea'

---
+++ people/enricosprea/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '129342869@N08',
+  'alias': 'enricosprea',
+  'name': 'Enrico Sprea',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/129342869@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/enricosprea/',
+  'markers': 968,
+  'photos': 979,
+  'countries': 24
+}
