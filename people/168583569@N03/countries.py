508de52a86c0de47f534a8ec7175ddc3fd0b5f5d
The Map Group <admin@the-map-group.top>
2024-12-15 08:07:29 -0300

    [auto] Updated map for member 'amalthus'

---
+++ people/168583569@N03/countries.py
@@ -0,0 +1,17 @@
+countries_dict = {
+  'CA': ['Canada', 1, 1],
+  'GR': ['Greece', 18, 38],
+  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States', 15, 111],
+  'CY': ['Cyprus', 2, 2],
+  'LB': ['Lebanon', 1, 1],
+  'PT': ['Portugal', 1, 1],
+  'BG': ['Bulgaria', 1, 1],
+  'FR': ['France', 4, 31],
+  'DE': ['Germany', 13, 205],
+  'AT': ['Austria', 1, 1],
+  'ES': ['Spain', 1, 1],
+  'IT': ['Italy', 6, 6],
+  'BR': ['Brazil', 1, 1],
+  'UG': ['Uganda', 1, 1]
+}
---
+++ people/168583569@N03/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '168583569@N03',
+  'alias': '168583569@N03',
+  'name': 'amalthus',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/168583569@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/168583569@N03/',
+  'markers': 84,
+  'photos': 916,
+  'countries': 15
+}
