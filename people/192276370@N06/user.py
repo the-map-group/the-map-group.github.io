f62f6aca69167dde1dda7881608c2a969d4282c8
The Map Group <admin@the-map-group.top>
2024-12-15 14:16:42 -0300

    [auto] Updated map for member 'Patrice Dardoize'

---
+++ people/192276370@N06/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '192276370@N06',
+  'alias': '192276370@N06',
+  'name': 'Patrice Dardoize',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/192276370@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/192276370@N06/',
+  'markers': 633,
+  'photos': 791,
+  'countries': 2
+}
