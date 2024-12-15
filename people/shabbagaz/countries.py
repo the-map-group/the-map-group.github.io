49d350687c1483daca8abb5940a9e48cb2608e44
The Map Group <admin@the-map-group.top>
2024-12-15 08:49:08 -0300

    [auto] Updated map for member 'Gaz 1984'

---
+++ people/shabbagaz/countries.py
@@ -0,0 +1,6 @@
+countries_dict = {
+  'ES': ['Spain', 21, 103],
+  'IE': ['Ireland', 54, 58],
+  'DE': ['Germany', 25, 230],
+  'GB': ['United Kingdom', 12619, 18504]
+}
---
+++ people/shabbagaz/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '33352310@N00',
+  'alias': 'shabbagaz',
+  'name': 'Gary Bond',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/33352310@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/shabbagaz/',
+  'markers': 12719,
+  'photos': 18895,
+  'countries': 4
+}
