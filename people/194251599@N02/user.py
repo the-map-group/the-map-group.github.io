cfb1747038dfc9cc2690ec03de20bd668123db23
The Map Group <admin@the-map-group.top>
2024-12-15 09:00:29 -0300

    [auto] Updated map for member 'regardphoto04'

---
+++ people/194251599@N02/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '194251599@N02',
+  'alias': '194251599@N02',
+  'name': 'Regard  Photo',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/194251599@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/194251599@N02/',
+  'markers': 6,
+  'photos': 6,
+  'countries': 2
+}
