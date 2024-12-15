523e7d0f88be3f5d1f807616f476d84cf75db811
The Map Group <admin@the-map-group.top>
2024-12-15 07:52:30 -0300

    [auto] Updated map for member 'Massimo Francesconi'

---
+++ people/massimo_francesconi/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'MA': ['Morocco', 1, 1],
+  'IT': ['Italy', 63, 68]
+}
---
+++ people/massimo_francesconi/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '63558969@N08',
+  'alias': 'massimo_francesconi',
+  'name': 'Massimo Francesconi',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/63558969@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/massimo_francesconi/',
+  'markers': 64,
+  'photos': 69,
+  'countries': 2
+}
