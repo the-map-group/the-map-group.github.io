fef5b5bc50ecda5712cd2dbb1a96b0a49651f4a3
The Map Group <admin@the-map-group.top>
2024-12-15 19:32:20 -0300

    [auto] Updated map for member 'cnmark'

---
+++ people/cnmark/countries.py
@@ -0,0 +1,15 @@
+countries_dict = {
+  'US': ['United States', 6, 6],
+  'SE': ['Sweden', 1, 1],
+  'DE': ['Germany', 1667, 2592],
+  'TH': ['Thailand', 2, 2],
+  'CA': ['Canada', 24, 25],
+  'IT': ['Italy', 36, 53],
+  'FR': ['France', 34, 39],
+  'GB': ['United Kingdom', 3, 3],
+  'AT': ['Austria', 17, 21],
+  'AE': ['United Arab Emirates', 7, 11],
+  'HU': ['Hungary', 23, 24],
+  'JP': ['Japan', 11, 12],
+  'CN': ['China', 1675, 2060]
+}
---
+++ people/cnmark/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '48646584@N00',
+  'alias': 'cnmark',
+  'name': 'Markus Bahlmann',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/48646584@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/cnmark/',
+  'markers': 3507,
+  'photos': 4850,
+  'countries': 13
+}
