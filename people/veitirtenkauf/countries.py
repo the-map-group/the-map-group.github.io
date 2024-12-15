2d054ee1f765463660c8e2df9e07bf09b20e05f7
The Map Group <admin@the-map-group.top>
2024-12-15 02:45:25 -0300

    [auto] Updated map for member '_Veit_'

---
+++ people/veitirtenkauf/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'US': ['United States', 65, 957],
+  'CZ': ['Czech Republic', 1, 1]
+}
---
+++ people/veitirtenkauf/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '27539916@N05',
+  'alias': 'veitirtenkauf',
+  'name': 'Veit',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/27539916@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/veitirtenkauf/',
+  'markers': 66,
+  'photos': 958,
+  'countries': 2
+}
