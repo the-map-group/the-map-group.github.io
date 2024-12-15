a4c094519d30384f8c1a3a6cc172dfe4b620cec4
The Map Group <admin@the-map-group.top>
2024-12-15 03:20:45 -0300

    [auto] Updated map for member 'cyclingshepherd'

---
+++ people/cyclingshepherd/countries.py
@@ -0,0 +1,18 @@
+countries_dict = {
+  'VN': ['Vietnam', 4, 4],
+  'HU': ['Hungary', 1, 1],
+  'ES': ['Spain', 3, 3],
+  'BR': ['Brazil', 5, 5],
+  'CN': ['China', 1, 1],
+  'GB': ['United Kingdom', 117, 185],
+  'GR': ['Greece', 3, 3],
+  'FR': ['France', 1, 1],
+  'ZA': ['South Africa', 4, 4],
+  'NA': ['Namibia', 10, 10],
+  'TH': ['Thailand', 1, 1],
+  'MA': ['Morocco', 1, 1],
+  'PT': ['Portugal', 626, 743],
+  'IT': ['Italy', 11, 12],
+  'ER': ['Eritrea', 3, 3],
+  'IN': ['India', 6, 10]
+}
---
+++ people/cyclingshepherd/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '59692385@N00',
+  'alias': 'cyclingshepherd',
+  'name': 'cyclingshepherd',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59692385@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/cyclingshepherd/',
+  'markers': 801,
+  'photos': 991,
+  'countries': 16
+}
