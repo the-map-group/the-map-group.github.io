0499f808b71435eacbb9ad08bcb14269411bd8e0
The Map Group <admin@the-map-group.top>
2024-12-15 05:32:42 -0300

    [auto] Updated map for member '七福神'

---
+++ people/1one/countries.py
@@ -0,0 +1,9 @@
+countries_dict = {
+  'BS': ['Bahamas', 6, 8],
+  'FR': ['France', 51, 52],
+  'BA': ['Bosnia and Herzegovina', 14, 14],
+  'JP': ['Japan', 3110, 37107],
+  'US': ['United States', 18, 21],
+  'GB': ['United Kingdom', 1, 1],
+  'KP': ['North Korea', 1, 1]
+}
---
+++ people/1one/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '52009851@N03',
+  'alias': '1one',
+  'name': 'Shichifuku Jin',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/52009851@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/1one/',
+  'markers': 3203,
+  'photos': 37206,
+  'countries': 7
+}
