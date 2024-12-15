20106eff382d74af88682e37c4fd7830824d2734
The Map Group <admin@the-map-group.top>
2024-12-15 14:03:57 -0300

    [auto] Updated map for member 'suntrana3'

---
+++ people/23244764@N08/countries.py
@@ -0,0 +1,13 @@
+countries_dict = {
+  'AQ': ['Antarctic Peninsula', 12, 12],
+  'US': ['United States', 163, 387],
+  'BZ': ['Belize', 5, 9],
+  'PT': ['Portugal', 1, 1],
+  'AW': ['Aruba', 13, 14],
+  'ES': ['Spain', 46, 69],
+  'AS': ['American Samoa', 1, 1],
+  'CA': ['Canada', 1, 1],
+  'MA': ['Morocco', 14, 14],
+  'GI': ['Gibraltar', 20, 20],
+  'BS': ['Bahamas', 2, 2]
+}
---
+++ people/23244764@N08/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '23244764@N08',
+  'alias': '23244764@N08',
+  'name': 'Jim Smith',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/23244764@N08/',
+  'markers': 281,
+  'photos': 533,
+  'countries': 11
+}
