abb6cdfcadda20837797cb242e53605f2a2ddd7c
The Map Group <admin@the-map-group.top>
2024-12-15 19:46:50 -0300

    [auto] Updated map for member 'PeterThoeny'

---
+++ people/peterthoeny/countries.py
@@ -0,0 +1,17 @@
+countries_dict = {
+  'NO': ['Norway', 1, 1],
+  'CN': ['China', 71, 95],
+  'IS': ['Iceland', 64, 97],
+  'LI': ['Liechtenstein', 4, 5],
+  'GL': ['Greenland', 7, 8],
+  'CH': ['Switzerland', 62, 108],
+  'EE': ['Estonia', 1, 1],
+  'IT': ['Italy', 26, 38],
+  'JP': ['Japan', 123, 144],
+  'UA': ['Ukraine', 1, 1],
+  'DE': ['Germany', 2, 3],
+  'US': ['United States', 1685, 3268],
+  'AT': ['Austria', 6, 10],
+  'CA': ['Canada', 6, 6],
+  'GB': ['United Kingdom', 17, 34]
+}
---
+++ people/peterthoeny/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '98786299@N00',
+  'alias': 'peterthoeny',
+  'name': 'Peter Thoeny - Quality HDR Pho',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/peterthoeny/',
+  'markers': 2249,
+  'photos': 4112,
+  'countries': 15
+}
