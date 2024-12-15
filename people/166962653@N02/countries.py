60cc649fda8da87979ab03af397e3224a124a9de
The Map Group <admin@the-map-group.top>
2024-12-15 19:47:34 -0300

    [auto] Updated map for member 'CravingEscape'

---
+++ people/166962653@N02/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'IT': ['Italy', 11, 11],
+  'AU': ['Australia', 46, 50]
+}
---
+++ people/166962653@N02/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '166962653@N02',
+  'alias': '166962653@N02',
+  'name': 'CravingEscape',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/166962653@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/166962653@N02/',
+  'markers': 57,
+  'photos': 61,
+  'countries': 2
+}
