7d3d2cfce863d7140a452536f0704311562ba81b
The Map Group <admin@the-map-group.top>
2024-12-15 02:44:52 -0300

    [auto] Updated map for member 'Robert C. Abraham'

---
+++ people/79080/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '31693521@N08',
+  'alias': '79080',
+  'name': 'Robert C. Abraham',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/31693521@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/79080/',
+  'markers': 244,
+  'photos': 270,
+  'countries': 15
+}
