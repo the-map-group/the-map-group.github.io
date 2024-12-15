6961e50f3b3b3fe251398f99e520828287014058
The Map Group <admin@the-map-group.top>
2024-12-15 20:17:53 -0300

    [auto] Updated map for member 'Atif Hussain Photography'

---
+++ people/atifhussainphotography/countries.py
@@ -0,0 +1,18 @@
+countries_dict = {
+  'BE': ['Belgium', 14, 14],
+  'DE': ['Germany', 46, 46],
+  'IT': ['Italy', 20, 20],
+  'BH': ['Bahrain', 1, 1],
+  'AE': ['United Arab Emirates', 8, 9],
+  'HU': ['Hungary', 27, 29],
+  'SA': ['Saudi Arabia', 10, 10],
+  'CZ': ['Czech Republic', 26, 26],
+  'FR': ['France', 16, 16],
+  'QA': ['Qatar', 121, 124],
+  'AT': ['Austria', 50, 50],
+  'TR': ['Turkey', 98, 98],
+  'NL': ['Netherlands', 22, 22],
+  'CH': ['Switzerland', 37, 37],
+  'SK': ['Slovakia', 8, 8],
+  'PK': ['Pakistan', 2, 2]
+}
---
+++ people/atifhussainphotography/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '42102968@N00',
+  'alias': 'atifhussainphotography',
+  'name': 'Atif Hussain Photography',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/42102968@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/atifhussainphotography/',
+  'markers': 569,
+  'photos': 575,
+  'countries': 16
+}
