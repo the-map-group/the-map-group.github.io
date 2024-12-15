c3628296eccbd7f3aa3ffd7b79d8263323723caa
The Map Group <admin@the-map-group.top>
2024-12-15 15:26:49 -0300

    [auto] Updated map for member 'marionkaminski'

---
+++ people/147422800@N02/countries.py
@@ -0,0 +1,16 @@
+countries_dict = {
+  'BO': ['Bolivia', 1, 1],
+  'IT': ['Italy', 12, 17],
+  'CL': ['Chile', 31, 70],
+  'PE': ['Peru', 10, 16],
+  'ES': ['Spain', 9, 13],
+  'IS': ['Iceland', 16, 28],
+  'PT': ['Portugal', 2, 3],
+  'EC': ['Ecuador', 1, 1],
+  'NO': ['Norway', 9, 11],
+  'DE': ['Germany', 49, 76],
+  'MA': ['Morocco', 6, 18],
+  'JO': ['Jordan', 3, 8],
+  'NA': ['Namibia', 6, 8],
+  'FR': ['France', 2, 4]
+}
