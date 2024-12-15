0a0329e56789e6a83f32d5bfe43f9f76f491f0ad
The Map Group <admin@the-map-group.top>
2024-12-15 14:15:39 -0300

    [auto] Updated map for member 'alcides OTA'

---
+++ people/cabodevassoura/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '41032277@N07',
+  'alias': 'cabodevassoura',
+  'name': 'alcidesota@yahoo.com-OFF-For S',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/41032277@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/cabodevassoura/',
+  'markers': 185,
+  'photos': 196,
+  'countries': 2
+}
