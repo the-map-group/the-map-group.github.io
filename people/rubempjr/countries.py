a7f545d9f3b4af9837c7a239821352d6baadec96
The Map Group <admin@the-map-group.top>
2024-12-15 14:02:03 -0300

    [auto] Updated map for member 'Rubem Jr'

---
+++ people/rubempjr/countries.py
@@ -0,0 +1,13 @@
+countries_dict = {
+  'GB': ['United Kingdom', 887, 2465],
+  'PT': ['Portugal', 2675, 6261],
+  'UY': ['Uruguay', 25, 185],
+  'AR': ['Argentina', 61, 123],
+  'CL': ['Chile', 1, 1],
+  'ES': ['Spain', 1369, 3432],
+  'BR': ['Brazil', 2577, 7153],
+  'US': ['United States', 46, 156],
+  'IT': ['Italy', 179, 409],
+  'RU': ['Russia', 1, 1],
+  'FR': ['France', 6, 24]
+}
---
+++ people/rubempjr/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '38795342@N06',
+  'alias': 'rubempjr',
+  'name': 'Rubem Porto Jr',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38795342@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/rubempjr/',
+  'markers': 7980,
+  'photos': 20428,
+  'countries': 11
+}
