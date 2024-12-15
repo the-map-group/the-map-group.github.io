af9f2fdea107ce7615c06c0af80896c6ddf836c0
The Map Group <admin@the-map-group.top>
2024-12-15 08:04:48 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

---
+++ people/montenegrina/countries.py
@@ -0,0 +1,14 @@
+countries_dict = {
+  'BA': ['Bosnia and Herzegovina', 19, 20],
+  'MA': ['Morocco', 18, 20],
+  'PT': ['Portugal', 14, 22],
+  'GR': ['Greece', 76, 87],
+  'ES': ['Spain', 111, 130],
+  'IT': ['Italy', 86, 98],
+  'AL': ['Albania', 12, 12],
+  'CZ': ['Czech Republic', 52, 61],
+  'HR': ['Croatia', 53, 60],
+  'ME': ['Montenegro', 783, 878],
+  'SI': ['Slovenia', 5, 6],
+  'FR': ['France', 73, 85]
+}
---
+++ people/montenegrina/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '92496593@N08',
+  'alias': 'montenegrina',
+  'name': 'ForceMajeureMontenegro',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/92496593@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/montenegrina/',
+  'markers': 1349,
+  'photos': 1532,
+  'countries': 12
+}
