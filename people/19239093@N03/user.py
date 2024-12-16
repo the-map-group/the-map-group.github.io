e1450babac6fe7e9fc76b8b97a06636ecf66bb6d
The Map Group <admin@the-map-group.top>
2024-12-15 23:33:37 -0300

    Reset map for member '19239093@N03'

---
+++ people/19239093@N03/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '19239093@N03',
+  'alias': '19239093@N03',
+  'name': 'Edouard',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/19239093@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/19239093@N03/',
+  'markers': 7990,
+  'photos': 10308,
+  'countries': 3
+}
