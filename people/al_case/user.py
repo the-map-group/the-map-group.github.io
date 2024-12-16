560d1ffe5181783fe4821df5587c9fc687b82894
The Map Group <admin@the-map-group.top>
2024-12-15 22:18:44 -0300

    [auto] Updated map for member 'Al Case'

---
+++ people/al_case/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '167430854@N07',
+  'alias': 'al_case',
+  'name': 'Al Case',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/167430854@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/al_case/',
+  'markers': 735,
+  'photos': 850,
+  'countries': 20
+}
