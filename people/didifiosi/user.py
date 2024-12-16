76dc678032c5803ce58bcc91dd85d7c835f7853b
The Map Group <admin@the-map-group.top>
2024-12-16 01:17:07 -0300

    [auto] Updated map for member 'jeanpaulneveu'

--- people/didifiosi/user.py
+++ people/didifiosi/user.py
@@ -5,6 +5,6 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/131237713@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/didifiosi/',
   'markers': 37,
-  'photos': 1385,
+  'photos': 1387,
   'countries': 1
 }
