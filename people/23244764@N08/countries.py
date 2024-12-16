1aa8c8e87fd5759d81596d8c259cb226b88b6076
The Map Group <admin@the-map-group.top>
2024-12-16 01:23:58 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/countries.py
+++ people/23244764@N08/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'AQ': ['Antarctic Peninsula', 12, 12],
-  'US': ['United States', 163, 387],
+  'GI': ['Gibraltar', 20, 20],
+  'AS': ['American Samoa', 1, 1],
   'BZ': ['Belize', 5, 9],
-  'PT': ['Portugal', 1, 1],
+  'BS': ['Bahamas', 2, 2],
   'AW': ['Aruba', 13, 14],
   'ES': ['Spain', 46, 69],
-  'AS': ['American Samoa', 1, 1],
+  'US': ['United States', 164, 388],
+  'PT': ['Portugal', 1, 1],
+  'AQ': ['Antarctic Peninsula', 12, 12],
   'CA': ['Canada', 1, 1],
-  'MA': ['Morocco', 14, 14],
-  'GI': ['Gibraltar', 20, 20],
-  'BS': ['Bahamas', 2, 2]
+  'MA': ['Morocco', 14, 14]
 }
--- people/23244764@N08/user.py
+++ people/23244764@N08/user.py
@@ -4,7 +4,7 @@ user_info = {
   'name': 'Jim Smith',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/23244764@N08/',
-  'markers': 281,
-  'photos': 533,
+  'markers': 282,
+  'photos': 534,
   'countries': 11
 }
