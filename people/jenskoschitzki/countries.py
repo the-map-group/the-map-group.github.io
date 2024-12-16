c8665980b3b8833bc546b9b7af3949c02bf2e6e5
The Map Group <admin@the-map-group.top>
2024-12-16 01:19:48 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'AM': ['Armenia', 159, 170],
-  'DE': ['Germany', 1783, 28214],
-  'RU': ['Russia', 427, 509],
-  'IT': ['Italy', 1691, 2076],
-  'EE': ['Estonia', 170, 170],
-  'AZ': ['Azerbaijan', 2, 2],
-  'GE': ['Georgia', 155, 176],
-  'LT': ['Lithuania', 125, 136],
-  'AT': ['Austria', 97, 125],
   'GB': ['United Kingdom', 147, 150],
+  'LV': ['Latvia', 149, 149],
+  'IT': ['Italy', 1692, 2087],
+  'RU': ['Russia', 427, 509],
+  'AT': ['Austria', 97, 125],
+  'LT': ['Lithuania', 125, 136],
+  'GE': ['Georgia', 155, 176],
+  'DE': ['Germany', 1783, 28214],
+  'AZ': ['Azerbaijan', 2, 2],
   'FI': ['Finland', 126, 127],
-  'LV': ['Latvia', 149, 149]
+  'AM': ['Armenia', 159, 170],
+  'EE': ['Estonia', 170, 170]
 }
