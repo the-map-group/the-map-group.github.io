0967f8e69d235a6246827581bd3e0aa65d2e0fa6
The Map Group <admin@the-map-group.top>
2024-12-15 22:51:48 -0300

    [auto] Updated map for member 'sarahstierch'

---
+++ people/sarahvain/countries.py
@@ -0,0 +1,17 @@
+countries_dict = {
+  'DE': ['Germany', 25, 49],
+  'FI': ['Finland', 38, 83],
+  'JP': ['Japan', 28, 46],
+  'CL': ['Chile', 37, 119],
+  'CZ': ['Czech Republic', 12, 17],
+  'US': ['United States', 12181, 16570],
+  'HU': ['Hungary', 18, 39],
+  'NL': ['Netherlands', 8, 8],
+  'BE': ['Belgium', 14, 93],
+  'GB': ['United Kingdom', 165, 439],
+  'IL': ['Israel', 9, 31],
+  'CN': ['China', 17, 77],
+  'FR': ['France', 50, 103],
+  'CA': ['Canada', 2, 2],
+  'AT': ['Austria', 18, 28]
+}
---
+++ people/sarahvain/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '7633518@N08',
+  'alias': 'sarahvain',
+  'name': 'Sarah Stierch',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/7633518@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/sarahvain/',
+  'markers': 12623,
+  'photos': 17711,
+  'countries': 15
+}
