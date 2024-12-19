aeb3e24cb0d829adf8de2e8ac2cddc2d09336e9e
The Map Group <admin@the-map-group.top>
2024-12-18 22:59:19 -0300

    [auto] Updated map for member 'Kart5a'

--- people/tinokaartovuori/countries.py
+++ people/tinokaartovuori/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'SC': ['Seychelles', 4, 4],
+  'FI': ['Finland', 45, 47],
   'MU': ['Mauritius', 1, 1],
   'US': ['United States', 3, 3],
-  'FI': ['Finland', 45, 47]
+  'SC': ['Seychelles', 4, 4]
 }
