c53bc6a33fa93b97db329f00e7c92160e1008e71
The Map Group <admin@the-map-group.top>
2024-12-19 11:46:38 -0300

    [auto] Updated map for member 'Miguelanxo'

--- people/miguelanxo57/countries.py
+++ people/miguelanxo57/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'ES': ['Spain', 295, 330],
-  'PT': ['Portugal', 52, 58],
-  'GB': ['United Kingdom', 29, 36],
+  'ES': ['Spain', 296, 331],
   'CZ': ['Czech Republic', 4, 4],
   'SK': ['Slovakia', 1, 1],
-  'DE': ['Germany', 4, 4],
+  'GB': ['United Kingdom', 29, 36],
   'AT': ['Austria', 1, 1],
   'FR': ['France', 5, 5],
-  'HU': ['Hungary', 3, 3]
+  'HU': ['Hungary', 3, 3],
+  'PT': ['Portugal', 52, 58],
+  'DE': ['Germany', 4, 4]
 }
--- people/miguelanxo57/user.py
+++ people/miguelanxo57/user.py
@@ -4,7 +4,7 @@ user_info = {
   'name': 'Miguel RC',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59150926@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/miguelanxo57/',
-  'markers': 394,
-  'photos': 442,
+  'markers': 395,
+  'photos': 443,
   'countries': 9
 }
