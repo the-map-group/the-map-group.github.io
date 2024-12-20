874487dca7ae165f1623e1ade3d6496b702edc43
The Map Group <admin@the-map-group.top>
2024-12-20 12:26:20 -0300

    Reset map for member '8920963@N03'

---
+++ people/8920963@N03/user.py
@@ -0,0 +1,10 @@
+user_info = {
+  'id': '8920963@N03',
+  'alias': '8920963@N03',
+  'name': 'Steen Jeppesen',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8920963@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/8920963@N03/',
+  'markers': 12058,
+  'photos': 17352,
+  'countries': 31
+}
