b5f28fa039414648803706a86d72c02d52d1be57
The Map Group <admin@the-map-group.top>
2024-12-21 16:21:04 -0300

    [auto] Updated map for member 'Rosie Chilton'

--- people/31248131@N05/user.py
+++ people/31248131@N05/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Rosie Chilton',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/31248131@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/31248131@N05/',
+  'location': '',
+  'countries': 1,
   'markers': 364,
-  'photos': 725,
-  'countries': 1
+  'photos': 725
 }
