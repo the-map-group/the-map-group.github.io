cb84c53f3c447ba3cc6d968c85c405078e78e3a4
The Map Group <admin@the-map-group.top>
2024-12-21 07:01:30 -0300

    [auto] Updated map for member 'Quel.R.R'

--- people/quelrr/countries.py
+++ people/quelrr/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'HR': ['Croatia', 2, 2],
-  'VA': ['Vatican City', 1, 1],
-  'BR': ['Brazil', 22, 35],
   'US': ['United States', 4, 4],
+  'VA': ['Vatican City', 1, 1],
+  'HR': ['Croatia', 2, 2],
+  'IT': ['Italy', 3, 3],
   'AR': ['Argentina', 3, 3],
-  'IT': ['Italy', 3, 3]
+  'BR': ['Brazil', 22, 35]
 }
--- people/quelrr/user.py
+++ people/quelrr/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Raquel Rampasio',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/132838898@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/quelrr/',
+  'location': '',
+  'countries': 6,
   'markers': 35,
-  'photos': 48,
-  'countries': 6
+  'photos': 48
 }
