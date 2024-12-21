292e04a645ad32f363b4baedec2d17ee29eaa29f
The Map Group <admin@the-map-group.top>
2024-12-21 14:30:51 -0300

    [auto] Updated map for member 'xray_jets'

--- people/163374426@N06/countries.py
+++ people/163374426@N06/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AR': ['Argentina', 2, 2],
-  'IT': ['Italy', 3, 3],
-  'KR': ['South Korea', 8, 8],
-  'JP': ['Japan', 16, 20],
   'AU': ['Australia', 110, 125],
-  'CL': ['Chile', 5, 7],
   'IS': ['Iceland', 3, 3],
-  'FR': ['France', 1, 1],
   'PE': ['Peru', 2, 2],
-  'NZ': ['New Zealand', 1, 1]
+  'FR': ['France', 1, 1],
+  'AR': ['Argentina', 2, 2],
+  'JP': ['Japan', 16, 20],
+  'NZ': ['New Zealand', 1, 1],
+  'IT': ['Italy', 3, 3],
+  'CL': ['Chile', 5, 7],
+  'KR': ['South Korea', 8, 8]
 }
--- people/163374426@N06/user.py
+++ people/163374426@N06/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Peter K',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/163374426@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/163374426@N06/',
+  'location': '',
+  'countries': 10,
   'markers': 151,
-  'photos': 172,
-  'countries': 10
+  'photos': 172
 }
