a35cd64d96010dc2c199a44b594db7266fb54a38
The Map Group <admin@the-map-group.top>
2024-12-21 11:39:00 -0300

    [auto] Updated map for member 'Miguel - AF'

--- people/189316105@N08/user.py
+++ people/189316105@N08/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Miguel Angel Flores',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/189316105@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/189316105@N08/',
+  'location': '',
+  'countries': 1,
   'markers': 39,
-  'photos': 166,
-  'countries': 1
+  'photos': 166
 }
