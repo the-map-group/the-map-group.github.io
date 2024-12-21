7f9f920db4c10b2d9dcd4642b7fade07a5b029d4
The Map Group <admin@the-map-group.top>
2024-12-21 20:14:48 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/user.py
+++ people/diwan/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Dirk Wandel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/81504125@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/diwan/',
-  'markers': 8392,
-  'photos': 8909,
-  'countries': 24
+  'location': 'Magdeburg, Germany',
+  'countries': 24,
+  'markers': 8398,
+  'photos': 8915
 }
