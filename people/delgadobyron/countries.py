d5448328b6ad4529509a9b93d516b9302836b72e
The Map Group <admin@the-map-group.top>
2024-12-21 19:28:02 -0300

    [auto] Updated map for member 'ByronDelgado'

--- people/delgadobyron/countries.py
+++ people/delgadobyron/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'HU': ['Hungary', 4, 5],
   'GR': ['Greece', 1, 1],
-  'IT': ['Italy', 6, 60],
-  'DE': ['Germany', 1, 1],
   'ES': ['Spain', 2, 4],
+  'HU': ['Hungary', 4, 5],
+  'IT': ['Italy', 6, 60],
   'GB': ['United Kingdom', 86, 238],
-  'PL': ['Poland', 6, 6],
-  'AL': ['Albania', 4, 5]
+  'DE': ['Germany', 1, 1],
+  'AL': ['Albania', 4, 5],
+  'PL': ['Poland', 6, 6]
 }
--- people/delgadobyron/user.py
+++ people/delgadobyron/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Byron Delgado',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28214158@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/delgadobyron/',
+  'location': '',
+  'countries': 8,
   'markers': 110,
-  'photos': 320,
-  'countries': 8
+  'photos': 320
 }
