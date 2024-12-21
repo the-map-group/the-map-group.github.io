7ab34f1452611ac518940f7dcc80053ab8a1a354
The Map Group <admin@the-map-group.top>
2024-12-21 09:10:57 -0300

    [auto] Updated map for member 'jklovelacephotography'

--- people/189479352@N07/user.py
+++ people/189479352@N07/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Jeffrey Lovelace',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/189479352@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/189479352@N07/',
+  'location': 'Santa Barbara, United States',
+  'countries': 2,
   'markers': 31,
-  'photos': 36,
-  'countries': 2
+  'photos': 36
 }
