e4a7cd66d3e87b2172020cb5c58180f61853c1d6
The Map Group <admin@the-map-group.top>
2024-12-21 20:57:54 -0300

    [auto] Updated map for member 'Michael Thornquist'

--- people/seasidesigns/user.py
+++ people/seasidesigns/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'SeaSide Signs ~ Vancouver, Can',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/55069387@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/seasidesigns/',
+  'location': 'Vancouver, Canada',
+  'countries': 1,
   'markers': 141,
-  'photos': 188,
-  'countries': 1
+  'photos': 188
 }
