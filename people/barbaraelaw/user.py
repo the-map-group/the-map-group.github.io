2ea6a63ee97c8dbd0fb735dbdbda4ffa0ce7d5f8
The Map Group <admin@the-map-group.top>
2024-12-21 03:58:34 -0300

    [auto] Updated map for member 'ManxieB- sorry, struggling!'

---
+++ people/barbaraelaw/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '25404639@N08',
+  'alias': 'barbaraelaw',
+  'name': 'Barbara Lawrence',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/barbaraelaw/',
+  'location': 'Kirk Michael, Isle of Man',
+  'countries': 3,
+  'markers': 2487,
+  'photos': 3322
+}
