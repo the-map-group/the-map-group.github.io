a7db181c44cf61fc3c97669ba14785a3148271ce
The Map Group <admin@the-map-group.top>
2024-12-21 00:12:44 -0300

    [auto] Updated map for member 'Andy.Gocher'

--- people/andygocher/user.py
+++ people/andygocher/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Andy Gocher',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/110839836@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/andygocher/',
+  'location': 'South Wales, UK',
+  'countries': 53,
   'markers': 632,
-  'photos': 1544,
-  'countries': 53
+  'photos': 1544
 }
