34ce284e2aee63d4c36047c50865ce77ae088549
The Map Group <admin@the-map-group.top>
2024-12-21 12:33:05 -0300

    [auto] Updated map for member 'karen_ds1101'

--- people/karen_ds1101/user.py
+++ people/karen_ds1101/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Karen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51073972@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/karen_ds1101/',
+  'location': 'On the way to Cairns, Australia',
+  'countries': 15,
   'markers': 1141,
-  'photos': 1350,
-  'countries': 15
+  'photos': 1350
 }
