b05a3f3668f0536f1f35c2b50db06189a93e287a
The Map Group <admin@the-map-group.top>
2024-12-21 06:59:58 -0300

    [auto] Updated map for member 'Susie_D52'

--- people/susannemichelle/user.py
+++ people/susannemichelle/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Susie',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/21593175@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/susannemichelle/',
+  'location': 'Pueblo, Colorado',
+  'countries': 1,
   'markers': 200,
-  'photos': 3259,
-  'countries': 1
+  'photos': 3259
 }
