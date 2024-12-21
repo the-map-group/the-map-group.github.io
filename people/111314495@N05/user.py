ef827aff64ec6549b4611c101238bdd4619f06d5
The Map Group <admin@the-map-group.top>
2024-12-21 19:22:13 -0300

    [auto] Updated map for member 'xd_travel'

--- people/111314495@N05/user.py
+++ people/111314495@N05/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Mike X-d',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/111314495@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/111314495@N05/',
+  'location': 'Beachwood, US',
+  'countries': 57,
   'markers': 5444,
-  'photos': 9806,
-  'countries': 57
+  'photos': 9806
 }
