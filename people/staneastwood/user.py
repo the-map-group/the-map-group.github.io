b434ad9b76334dcd70c35375489f6f13c26f56ba
The Map Group <admin@the-map-group.top>
2024-12-21 08:29:45 -0300

    [auto] Updated map for member '7 mil views - Thank you all.'

--- people/staneastwood/user.py
+++ people/staneastwood/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Stanley Eastwood',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/63713558@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/staneastwood/',
+  'location': '',
+  'countries': 18,
   'markers': 7135,
-  'photos': 38584,
-  'countries': 18
+  'photos': 38584
 }
