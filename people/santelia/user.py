66a01c4fd1600d27376790552ebcef777310c455
The Map Group <admin@the-map-group.top>
2024-12-22 19:09:50 -0300

    [auto] Updated map for member 'alfsan'

---
+++ people/santelia/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '32557441@N02',
+  'alias': 'santelia',
+  'name': 'Alfonso Santelia',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/32557441@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/santelia/',
+  'location': 'Milano, Italia',
+  'countries': 34,
+  'markers': 718,
+  'photos': 1046
+}
