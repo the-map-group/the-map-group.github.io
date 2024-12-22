38b617a607666e0ba45497f0cda47639021bef38
The Map Group <admin@the-map-group.top>
2024-12-22 04:16:06 -0300

    [auto] Updated map for member 'faun070'

--- people/faun070/user.py
+++ people/faun070/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'faun070',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33277678@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/faun070/',
+  'location': 'Amsterdam, The Netherlands',
+  'countries': 59,
   'markers': 17165,
-  'photos': 41949,
-  'countries': 59
+  'photos': 41949
 }
