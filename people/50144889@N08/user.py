391233ca44591aeed66fb6980906519427cc3c74
The Map Group <admin@the-map-group.top>
2024-12-22 04:54:07 -0300

    [auto] Updated map for member 'Robert.Pittman'

--- people/50144889@N08/user.py
+++ people/50144889@N08/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Robert Pittman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/50144889@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/50144889@N08/',
-  'markers': 2405,
-  'photos': 2603,
-  'countries': 33
+  'location': 'Shropham, Norfolk., England',
+  'countries': 33,
+  'markers': 2406,
+  'photos': 2605
 }
