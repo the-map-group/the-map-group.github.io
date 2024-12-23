78febe17933746db20937d2cb9aa152dd362dcec
The Map Group <admin@the-map-group.top>
2024-12-23 06:08:24 -0300

    [auto] Updated map for member 'Howard Somerville (10 million views)'

--- people/byhowardsomerville/user.py
+++ people/byhowardsomerville/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/byhowardsomerville/',
   'location': 'London, UK',
   'countries': 36,
-  'markers': 794,
-  'photos': 943
+  'markers': 795,
+  'photos': 944
 }
