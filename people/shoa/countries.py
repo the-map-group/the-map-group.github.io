e67590a666217b876ebdb0c910c058510f5bfbf5
The Map Group <admin@the-map-group.top>
2024-12-23 15:42:51 -0300

    [auto] Updated map for member 'Joe Dyndale'

--- people/shoa/countries.py
+++ people/shoa/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'DK': ['Denmark', 8, 8],
-  'NO': ['Norway', 4, 4],
   'US': ['United States', 2, 2],
   'IS': ['Iceland', 7, 7],
-  'CH': ['Switzerland', 1, 1]
+  'CH': ['Switzerland', 1, 1],
+  'NO': ['Norway', 4, 4]
 }
