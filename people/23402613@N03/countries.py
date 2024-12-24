55c22db5e2f769728660d42db22e4d449c19e5c7
The Map Group <admin@the-map-group.top>
2024-12-24 09:36:46 -0300

    [auto] Updated map for member 'Joao de Mio'

--- people/23402613@N03/countries.py
+++ people/23402613@N03/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'FR': ['France', 1, 1],
-  'BR': ['Brazil', 69, 111],
   'CL': ['Chile', 11, 13],
-  'IT': ['Italy', 50, 59]
+  'FR': ['France', 1, 1],
+  'IT': ['Italy', 50, 59],
+  'BR': ['Brazil', 69, 111]
 }
--- people/23402613@N03/user.py
+++ people/23402613@N03/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Joao de Mio',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23402613@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/23402613@N03/',
+  'location': 'Pesaro, Itália',
+  'countries': 4,
   'markers': 131,
-  'photos': 184,
-  'countries': 4
+  'photos': 184
 }
