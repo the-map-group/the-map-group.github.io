7179b3ff00570d396368229f91213bfcba18a0ce
The Map Group <admin@the-map-group.top>
2024-12-24 17:41:23 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'TZ': ['Tanzania', 313, 932],
-  'TR': ['Turkey', 2, 2],
+  'CA': ['Canada', 10, 13],
+  'GB': ['United Kingdom', 52, 95],
+  'MX': ['Mexico', 8, 10],
   'US': ['United States', 3653, 6397],
-  'KE': ['Kenya', 168, 277],
   'IT': ['Italy', 351, 491],
   'VA': ['Vatican City', 16, 20],
-  'NL': ['Netherlands', 1, 1],
-  'CA': ['Canada', 10, 13],
+  'KE': ['Kenya', 168, 277],
   'QA': ['Qatar', 3, 3],
-  'MX': ['Mexico', 8, 10],
-  'GB': ['United Kingdom', 52, 95]
+  'NL': ['Netherlands', 1, 1],
+  'TZ': ['Tanzania', 313, 933],
+  'TR': ['Turkey', 2, 2]
 }
--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'John McNeely Hudson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/82487858@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/botetourt/',
+  'location': 'Near Fincastle, Botetourt County, Va., USA',
+  'countries': 11,
   'markers': 4577,
-  'photos': 8241,
-  'countries': 11
+  'photos': 8242
 }
