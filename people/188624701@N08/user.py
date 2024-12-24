ed53b77158befee31a0221e2cd111559162e4bb7
The Map Group <admin@the-map-group.top>
2024-12-24 09:37:58 -0300

    [auto] Updated map for member 'perunique :)'

--- people/188624701@N08/user.py
+++ people/188624701@N08/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Vedran Peruničić',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/188624701@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/188624701@N08/',
+  'location': 'Zagreb',
+  'countries': 2,
   'markers': 55,
-  'photos': 83,
-  'countries': 2
+  'photos': 83
 }
