77cc100cd4b6ef9bd4cf4a531d7b24052c6198e0
The Map Group <admin@the-map-group.top>
2024-12-24 15:05:59 -0300

    [auto] Updated map for member 'Sacred_Spiral_Photography'

--- people/cyber-giraffe/user.py
+++ people/cyber-giraffe/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Sacred_Spiral_Photography',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/32142243@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/cyber-giraffe/',
+  'location': 'Wiltshire, England',
+  'countries': 2,
   'markers': 35,
-  'photos': 195,
-  'countries': 2
+  'photos': 195
 }
