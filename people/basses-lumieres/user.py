7a884fb35860fd5a0966e1dc08d62c7c31aafafe
The Map Group <admin@the-map-group.top>
2024-12-25 16:31:47 -0300

    [auto] Updated map for member 'Jo 0405'

--- people/basses-lumieres/user.py
+++ people/basses-lumieres/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Jo 0405',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/108339617@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/basses-lumieres/',
+  'location': 'Terre, France',
+  'countries': 3,
   'markers': 178,
-  'photos': 759,
-  'countries': 3
+  'photos': 759
 }
