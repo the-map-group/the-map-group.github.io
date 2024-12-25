d91ae2a4cb8420095a6e6e628c6212842cb396df
The Map Group <admin@the-map-group.top>
2024-12-25 12:56:32 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/user.py
+++ people/train-pix/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Ron Fisher',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/40052043@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/train-pix/',
-  'markers': 11538,
-  'photos': 23784,
-  'countries': 17
+  'location': 'Farnborough, England',
+  'countries': 17,
+  'markers': 11540,
+  'photos': 23797
 }
