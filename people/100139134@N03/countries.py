a3521ceb04ca151f0568c06401c18b31bf624385
The Map Group <admin@the-map-group.top>
2024-12-25 14:56:18 -0300

    [auto] Updated map for member 'Jesús 56'

--- people/100139134@N03/countries.py
+++ people/100139134@N03/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'IT': ['Italy', 4, 4],
-  'AT': ['Austria', 1, 1],
-  'FR': ['France', 9, 17],
   'DE': ['Germany', 2, 2],
-  'NL': ['Netherlands', 1, 1],
-  'NO': ['Norway', 3, 3],
+  'IT': ['Italy', 4, 4],
   'ES': ['Spain', 125, 456],
-  'JP': ['Japan', 10, 10]
+  'FR': ['France', 9, 17],
+  'NL': ['Netherlands', 1, 1],
+  'AT': ['Austria', 1, 1],
+  'JP': ['Japan', 10, 10],
+  'NO': ['Norway', 3, 3]
 }
--- people/100139134@N03/user.py
+++ people/100139134@N03/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Jesus Villarreal',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/100139134@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/100139134@N03/',
+  'location': 'Valencia, Spain',
+  'countries': 8,
   'markers': 155,
-  'photos': 494,
-  'countries': 8
+  'photos': 494
 }
