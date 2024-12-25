b0ec44f007e2754e1191aa73a5e71fac58c422db
The Map Group <admin@the-map-group.top>
2024-12-25 18:08:07 -0300

    [auto] Updated map for member 'regardphoto04'

--- people/194251599@N02/locations.py
+++ people/194251599@N02/locations.py
@@ -1,7 +1,7 @@
 locations_dict = {
   'IT': [
-    [[12.372665, 45.415418], [['51721102078', 'https://live.staticflickr.com/65535/51721102078_cacb593104_s.jpg']]],
     [[12.328462, 45.436911], [['51663203256', 'https://live.staticflickr.com/65535/51663203256_acffa7d9ef_s.jpg']]],
+    [[12.372665, 45.415418], [['51721102078', 'https://live.staticflickr.com/65535/51721102078_cacb593104_s.jpg']]],
     [[6.81427, 44.946138], [['51628861397', 'https://live.staticflickr.com/65535/51628861397_aae2a80010_s.jpg']]]
   ],
   'FR': [
--- people/194251599@N02/user.py
+++ people/194251599@N02/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Regard  Photo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/194251599@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/194251599@N02/',
+  'location': '',
+  'countries': 2,
   'markers': 6,
-  'photos': 6,
-  'countries': 2
+  'photos': 6
 }
