7d8b47f8a9a29a32e508b53c3696298f585f465b
The Map Group <admin@the-map-group.top>
2024-12-26 00:34:49 -0300

    [auto] Updated map for member 'SeaLord 50+'

--- people/186479257@N03/countries.py
+++ people/186479257@N03/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'ES': ['Spain', 1, 1],
+  'EG': ['Egypt', 1, 1],
+  'TR': ['Turkey', 1, 1],
+  'VA': ['Vatican City', 1, 1],
   'US': ['United States', 2, 2],
-  'GB': ['United Kingdom', 1, 1],
+  'DE': ['Germany', 68, 73],
+  'ES': ['Spain', 1, 1],
+  'PT': ['Portugal', 2, 2],
   'IT': ['Italy', 2, 2],
   'HR': ['Croatia', 8, 13],
-  'VA': ['Vatican City', 1, 1],
-  'PT': ['Portugal', 2, 2],
-  'TR': ['Turkey', 1, 1],
-  'DE': ['Germany', 68, 73],
   'DK': ['Denmark', 1, 2],
-  'EG': ['Egypt', 1, 1]
+  'GB': ['United Kingdom', 1, 1]
 }
--- people/186479257@N03/user.py
+++ people/186479257@N03/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'SeaLord 50+',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/186479257@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/186479257@N03/',
+  'location': '',
+  'countries': 11,
   'markers': 88,
-  'photos': 99,
-  'countries': 11
+  'photos': 99
 }
