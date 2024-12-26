f68e785b473b259bba10845e4815cdeb2f3874ca
The Map Group <admin@the-map-group.top>
2024-12-26 02:55:41 -0300

    [auto] Updated map for member 'The Gidinski'

--- people/ggphoto36/countries.py
+++ people/ggphoto36/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'CA': ['Canada', 6, 6],
   'IT': ['Italy', 4, 4],
-  'BS': ['Bahamas', 1, 1],
+  'US': ['United States', 914, 959],
   'CR': ['Costa Rica', 1, 1],
-  'US': ['United States', 913, 958]
+  'CA': ['Canada', 6, 6],
+  'BS': ['Bahamas', 1, 1]
 }
--- people/ggphoto36/user.py
+++ people/ggphoto36/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Gary Giddens',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28018306@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/ggphoto36/',
-  'markers': 925,
-  'photos': 970,
-  'countries': 5
+  'location': 'Monmouth, Oregon, United States',
+  'countries': 5,
+  'markers': 926,
+  'photos': 971
 }
