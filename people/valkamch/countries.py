e586d0e8e1fc5be242d06639044a38f4f5fc2b30
The Map Group <admin@the-map-group.top>
2024-12-26 00:33:05 -0300

    [auto] Updated map for member 'ValKamch'

--- people/valkamch/countries.py
+++ people/valkamch/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'UZ': ['Uzbekistan', 169, 187],
-  'FR': ['France', 1, 1],
-  'SG': ['Singapore', 2, 16],
-  'MC': ['Monaco', 5, 12],
   'IT': ['Italy', 1370, 1578],
-  'RU': ['Russia', 1486, 1723],
-  'MT': ['Malta', 1, 1],
-  'NO': ['Norway', 1, 1],
+  'MC': ['Monaco', 5, 12],
+  'FR': ['France', 1, 1],
   'IR': ['Iran', 1, 1],
+  'LK': ['Sri Lanka', 119, 175],
+  'MT': ['Malta', 1, 1],
+  'SG': ['Singapore', 2, 16],
+  'RU': ['Russia', 1486, 1723],
   'KH': ['Cambodia', 316, 378],
-  'UA': ['Ukraine', 1327, 1483],
-  'LK': ['Sri Lanka', 119, 175]
+  'NO': ['Norway', 1, 1],
+  'UZ': ['Uzbekistan', 169, 187],
+  'UA': ['Ukraine', 1327, 1483]
 }
--- people/valkamch/user.py
+++ people/valkamch/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'ValKamch',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/143593890@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/valkamch/',
+  'location': 'Moscow region, Russia',
+  'countries': 12,
   'markers': 4798,
-  'photos': 5556,
-  'countries': 12
+  'photos': 5556
 }
