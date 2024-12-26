96ff5ced824cca4eb0e230166865ebe5532ec894
The Map Group <admin@the-map-group.top>
2024-12-26 15:35:26 -0300

    [auto] Updated map for member 'CravingEscape'

--- people/166962653@N02/user.py
+++ people/166962653@N02/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'CravingEscape',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/166962653@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/166962653@N02/',
+  'location': 'Brisbane, Australia',
+  'countries': 2,
   'markers': 57,
-  'photos': 61,
-  'countries': 2
+  'photos': 61
 }
