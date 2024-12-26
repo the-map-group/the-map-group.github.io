8da019939a773342d437b68eefa900cc0a89e875
The Map Group <admin@the-map-group.top>
2024-12-26 20:06:23 -0300

    [auto] Updated map for member 'Thad Roan - Bridgepix'

--- people/80651083@N00/user.py
+++ people/80651083@N00/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Thaddeus Roan',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/80651083@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/80651083@N00/',
+  'location': 'USA',
+  'countries': 35,
   'markers': 2882,
-  'photos': 4919,
-  'countries': 35
+  'photos': 4919
 }
