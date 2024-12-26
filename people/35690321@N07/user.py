ed90fd7c6cc8daecf2e5620619fa73b4bc81654b
The Map Group <admin@the-map-group.top>
2024-12-26 16:25:22 -0300

    [auto] Updated map for member 'Julian Munilla Rio'

--- people/35690321@N07/user.py
+++ people/35690321@N07/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Julian Munilla Rio',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/35690321@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/35690321@N07/',
+  'location': 'Fuenlabrada, España',
+  'countries': 6,
   'markers': 1018,
-  'photos': 1027,
-  'countries': 6
+  'photos': 1027
 }
