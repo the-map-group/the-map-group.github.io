a014f8a3e7cd3f7ec5843836fcfd3854d5c29820
The Map Group <admin@the-map-group.top>
2024-12-26 04:57:01 -0300

    [auto] Updated map for member 'Stratman2'

--- people/stratman2/user.py
+++ people/stratman2/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Stratman2 (taking care of Joey',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8278193@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/stratman2/',
+  'location': 'Kuala Lumpur, Malaysia',
+  'countries': 5,
   'markers': 522,
-  'photos': 4645,
-  'countries': 5
+  'photos': 4645
 }
