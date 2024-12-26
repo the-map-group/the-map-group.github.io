8198b849a0c51b3f1e2de9567a9711629b7cbf44
The Map Group <admin@the-map-group.top>
2024-12-26 20:42:01 -0300

    [auto] Updated map for member 'mouzhik'

--- people/zemzem/countries.py
+++ people/zemzem/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AU': ['Australia', 7, 8],
+  'MX': ['Mexico', 2, 2],
   'FR': ['France', 231, 317],
   'DE': ['Germany', 3, 3],
   'JO': ['Jordan', 4, 5],
   'MY': ['Malaysia', 2, 43],
   'ES': ['Spain', 32, 36],
-  'TR': ['Turkey', 1, 1],
   'MA': ['Morocco', 7, 8],
-  'GB': ['United Kingdom', 1, 1],
-  'MX': ['Mexico', 2, 2]
+  'AU': ['Australia', 7, 8],
+  'TR': ['Turkey', 1, 1],
+  'GB': ['United Kingdom', 1, 1]
 }
--- people/zemzem/user.py
+++ people/zemzem/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'mouzhik',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/67878469@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/zemzem/',
+  'location': 'France',
+  'countries': 10,
   'markers': 290,
-  'photos': 424,
-  'countries': 10
+  'photos': 424
 }
