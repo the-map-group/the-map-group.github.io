bee9e99a24a193983808332b02cef2db5310532a
The Map Group <admin@the-map-group.top>
2024-12-26 15:23:44 -0300

    [auto] Updated map for member 'cnmark'

--- people/cnmark/countries.py
+++ people/cnmark/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'IT': ['Italy', 36, 53],
-  'GB': ['United Kingdom', 3, 3],
-  'AT': ['Austria', 17, 21],
   'CH': ['Switzerland', 1, 1],
-  'SE': ['Sweden', 1, 1],
-  'US': ['United States', 6, 6],
-  'AE': ['United Arab Emirates', 7, 11],
-  'DE': ['Germany', 1667, 2592],
-  'TH': ['Thailand', 2, 2],
-  'CN': ['China', 1675, 2060],
   'CA': ['Canada', 24, 25],
   'JP': ['Japan', 11, 12],
+  'HU': ['Hungary', 23, 24],
+  'TH': ['Thailand', 2, 2],
   'FR': ['France', 34, 39],
-  'HU': ['Hungary', 23, 24]
+  'GB': ['United Kingdom', 3, 3],
+  'AE': ['United Arab Emirates', 7, 11],
+  'SE': ['Sweden', 1, 1],
+  'AT': ['Austria', 17, 21],
+  'IT': ['Italy', 36, 53],
+  'DE': ['Germany', 1667, 2592],
+  'US': ['United States', 6, 6],
+  'CN': ['China', 1675, 2060]
 }
--- people/cnmark/user.py
+++ people/cnmark/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Markus Bahlmann',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/48646584@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/cnmark/',
+  'location': 'München | Munich | 慕尼黑',
+  'countries': 14,
   'markers': 3507,
-  'photos': 4850,
-  'countries': 14
+  'photos': 4850
 }
