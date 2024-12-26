9a19c6c5bee38a6079f2e3f48de446dd9a1c0f34
The Map Group <admin@the-map-group.top>
2024-12-25 23:39:35 -0300

    [auto] Updated map for member 'acase1968'

--- people/60035031@N06/countries.py
+++ people/60035031@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'IS': ['Iceland', 21, 21],
-  'KR': ['South Korea', 23, 23],
+  'IT': ['Italy', 6, 6],
   'HU': ['Hungary', 6, 6],
-  'DE': ['Germany', 11, 11],
-  'CZ': ['Czech Republic', 32, 33],
+  'IS': ['Iceland', 21, 21],
   'MX': ['Mexico', 27, 27],
   'DK': ['Denmark', 159, 211],
-  'US': ['United States', 949, 1070],
-  'JP': ['Japan', 141, 153],
-  'NO': ['Norway', 2, 2],
-  'SE': ['Sweden', 35, 42],
   'NL': ['Netherlands', 3, 3],
   'GB': ['United Kingdom', 20, 21],
+  'DE': ['Germany', 11, 11],
   'CH': ['Switzerland', 15, 16],
-  'IT': ['Italy', 6, 6]
+  'SE': ['Sweden', 35, 42],
+  'US': ['United States', 949, 1070],
+  'CZ': ['Czech Republic', 32, 33],
+  'NO': ['Norway', 2, 2],
+  'JP': ['Japan', 141, 153],
+  'KR': ['South Korea', 23, 23]
 }
--- people/60035031@N06/user.py
+++ people/60035031@N06/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'Al Case',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/60035031@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/60035031@N06/',
+  'location': 'Ashland, United States',
+  'countries': 15,
   'markers': 1450,
-  'photos': 1645,
-  'countries': 15
+  'photos': 1645
 }
