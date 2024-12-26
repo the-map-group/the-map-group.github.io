d9ab5bd8d31d2bbf1f1ce2f40b1594f889905357
The Map Group <admin@the-map-group.top>
2024-12-26 04:44:25 -0300

    [auto] Updated map for member 'alcides OTA'

--- people/cabodevassoura/user.py
+++ people/cabodevassoura/user.py
@@ -4,7 +4,8 @@ user_info = {
   'name': 'alcidesota@yahoo.com-OFF-For S',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/41032277@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/cabodevassoura/',
+  'location': 'Komaki-shi Aichi-ken, Japan',
+  'countries': 2,
   'markers': 185,
-  'photos': 196,
-  'countries': 2
+  'photos': 196
 }
