1205cce56a32087ebe214059f662b0a05966cf96
The Map Group <admin@the-map-group.top>
2024-12-27 18:47:31 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'IT': ['Italy', 1879, 2401],
-  'RU': ['Russia', 427, 509],
-  'GE': ['Georgia', 155, 176],
-  'DE': ['Germany', 1783, 28214],
-  'AM': ['Armenia', 159, 170],
-  'LT': ['Lithuania', 125, 136],
   'LV': ['Latvia', 149, 149],
-  'GB': ['United Kingdom', 147, 150],
   'AZ': ['Azerbaijan', 2, 2],
-  'FI': ['Finland', 126, 127],
-  'HR': ['Croatia', 214, 244],
+  'AT': ['Austria', 97, 125],
+  'GB': ['United Kingdom', 147, 150],
+  'GE': ['Georgia', 155, 176],
   'EE': ['Estonia', 170, 170],
-  'AT': ['Austria', 97, 125]
+  'HR': ['Croatia', 214, 244],
+  'LT': ['Lithuania', 125, 136],
+  'RU': ['Russia', 427, 509],
+  'FI': ['Finland', 126, 127],
+  'IT': ['Italy', 1887, 2409],
+  'AM': ['Armenia', 159, 170],
+  'DE': ['Germany', 1783, 28214]
 }
--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
   'location': 'Munich, Germany',
   'countries': 13,
-  'markers': 5433,
-  'photos': 32573
+  'markers': 5441,
+  'photos': 32581
 }
