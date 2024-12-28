7f8455b0e7dac7e8f00082c365f68f0a30544a29
The Map Group <admin@the-map-group.top>
2024-12-28 13:24:19 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/countries.py
+++ people/fotosavulso/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'IT': ['Italy', 1, 1],
+  'ES': ['Spain', 25, 27],
+  'PT': ['Portugal', 338, 364],
   'FR': ['France', 63, 65],
   'AD': ['Andorra', 2, 2],
-  'US': ['United States', 3, 3],
-  'PT': ['Portugal', 336, 362],
-  'ST': ['Sao Tome and Principe', 102, 109],
+  'IT': ['Italy', 1, 1],
   'CV': ['Cape Verde', 32, 37],
-  'AT': ['Austria', 1, 1],
-  'ES': ['Spain', 25, 27]
+  'US': ['United States', 3, 3],
+  'ST': ['Sao Tome and Principe', 102, 109],
+  'AT': ['Austria', 1, 1]
 }
--- people/fotosavulso/user.py
+++ people/fotosavulso/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/fotosavulso/',
   'location': '',
   'countries': 9,
-  'markers': 565,
-  'photos': 607
+  'markers': 567,
+  'photos': 609
 }
