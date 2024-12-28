97e430fe4a6a8464be0e88ab5d7be90b68db1097
The Map Group <admin@the-map-group.top>
2024-12-27 23:51:30 -0300

    Reset map for member 'dandiffendale'

---
+++ people/dandiffendale/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '7945858@N08',
+  'alias': 'dandiffendale',
+  'name': 'Dan Diffendale',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/7945858@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/dandiffendale/',
+  'location': 'Naples, Italy',
+  'countries': 17,
+  'markers': 2090,
+  'photos': 15148
+}
