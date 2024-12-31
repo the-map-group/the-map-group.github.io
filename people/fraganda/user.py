b43f3f1a1d1ef3843665b8e0f5d1f9f3773885a5
The Map Group <admin@the-map-group.top>
2024-12-31 03:18:33 -0300

    [auto] Updated map for member 'fraganda.photo'

--- people/fraganda/user.py
+++ people/fraganda/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/fraganda/',
   'location': 'Ghent, Belgium',
   'countries': 16,
-  'markers': 303,
-  'photos': 303
+  'markers': 317,
+  'photos': 317
 }
