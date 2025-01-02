f85b0148290706f0c7343d7b29ff0018675641ba
The Map Group <admin@the-map-group.top>
2025-01-02 17:44:46 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- countries/members.py
+++ countries/members.py
@@ -1,11 +1,18 @@
 members_dict = {
-  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31]],
-  'UY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 36, 67]],
-  'HR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 184, 243]],
-  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132]],
-  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126]],
+  'NZ': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 9, 9]],
+  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 707, 982]],
+  'AU': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 11, 11]],
+  'CA': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 90, 158]],
+  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 93, 172]],
   'CL': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 78, 198]],
+  'CH': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 6, 9]],
+  'RU': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 1, 1]],
+  'GB': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 26, 28]],
+  'PA': [['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 1, 2]],
+  'UY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 36, 67]],
+  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 25, 32]],
+  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126], ['8121206@N07', 'chrispricephotography', 'Chris Price', 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg', 186, 245]],
   'BR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 176, 394]],
   'PY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 1, 1]],
-  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 93, 172]]
+  'HR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 184, 243]]
 }
--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'NZ': ['New Zealand', 9, 9],
-  'PA': ['Panama', 1, 2],
   'US': ['United States', 707, 982],
-  'IT': ['Italy', 186, 245],
   'RU': ['Russia', 1, 1],
-  'CH': ['Switzerland', 6, 9],
-  'VA': ['Vatican City', 25, 32],
-  'CA': ['Canada', 90, 158],
   'AU': ['Australia', 11, 11],
-  'GB': ['United Kingdom', 26, 28]
+  'GB': ['United Kingdom', 26, 28],
+  'VA': ['Vatican City', 25, 32],
+  'NZ': ['New Zealand', 9, 9],
+  'CA': ['Canada', 90, 158],
+  'PA': ['Panama', 1, 2],
+  'IT': ['Italy', 186, 245],
+  'CH': ['Switzerland', 6, 9]
 }
