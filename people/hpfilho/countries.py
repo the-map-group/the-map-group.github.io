e8c5fb7a30dd06a24ab5543405e6c1620ca63574
The Map Group <admin@the-map-group.top>
2025-01-02 10:28:28 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'CL': ['Chile', 78, 198],
-  'PY': ['Paraguay', 1, 1],
-  'HR': ['Croatia', 184, 243],
   'VA': ['Vatican City', 22, 31],
+  'CL': ['Chile', 78, 198],
   'UY': ['Uruguay', 36, 67],
+  'PY': ['Paraguay', 1, 1],
+  'IT': ['Italy', 77, 126],
   'AR': ['Argentina', 93, 172],
   'BR': ['Brazil', 176, 394],
-  'US': ['United States', 50, 132],
-  'IT': ['Italy', 77, 126]
+  'HR': ['Croatia', 184, 243],
+  'US': ['United States', 50, 132]
 }
