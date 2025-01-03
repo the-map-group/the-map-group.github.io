051c118b9f05d38d0ed655255c9204a08de373bc
The Map Group <admin@the-map-group.top>
2025-01-03 18:11:06 -0300

    [auto] Updated map for member 'pepemartin2006'

--- people/mariomartins/countries.py
+++ people/mariomartins/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'PT': ['Portugal', 27, 50],
   'AD': ['Andorra', 1, 1],
   'FR': ['France', 7, 7],
-  'ES': ['Spain', 67, 102],
-  'PT': ['Portugal', 27, 50]
+  'ES': ['Spain', 67, 102]
 }
