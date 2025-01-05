508892c1b6586ede04f6836eb1d16400d7d7fa24
The Map Group <admin@the-map-group.top>
2025-01-05 04:04:45 -0300

    [auto] Updated map for member 'Oleg S .'

--- people/olegsokol/user.py
+++ people/olegsokol/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/olegsokol/',
   'location': 'Philadelphia, USA',
   'countries': 28,
-  'markers': 728,
-  'photos': 889
+  'markers': 730,
+  'photos': 891
 }
