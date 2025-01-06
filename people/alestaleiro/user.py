9a504700e8070dae84f7801e37b82a8619acf707
The Map Group <admin@the-map-group.top>
2025-01-06 13:22:01 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'location': 'Praia do Estaleiro - B. Camboriú, BRASIL',
   'countries': 26,
-  'markers': 3754,
-  'photos': 5063
+  'markers': 3756,
+  'photos': 5087
 }
