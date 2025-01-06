c6f0e927f9a1bd55e83954d050c8ca887a86767d
The Map Group <admin@the-map-group.top>
2025-01-06 06:34:17 -0300

    [auto] Updated map for member 'Matjaž Skrinar'

--- people/130486903@N04/user.py
+++ people/130486903@N04/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/130486903@N04/',
   'location': 'Maribor, Slovenia',
   'countries': 4,
-  'markers': 346,
-  'photos': 364
+  'markers': 349,
+  'photos': 368
 }
