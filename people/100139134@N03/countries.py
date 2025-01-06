232bdf71e6d624a4d9f26d4b10d78f0dc7b6f4e2
The Map Group <admin@the-map-group.top>
2025-01-06 06:26:58 -0300

    [auto] Updated map for member 'Jesús 56'

--- people/100139134@N03/countries.py
+++ people/100139134@N03/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'DE': ['Germany', 2, 2],
-  'IT': ['Italy', 4, 4],
-  'ES': ['Spain', 125, 456],
   'FR': ['France', 9, 17],
-  'NL': ['Netherlands', 1, 1],
+  'ES': ['Spain', 125, 456],
   'AT': ['Austria', 1, 1],
+  'NL': ['Netherlands', 1, 1],
+  'NO': ['Norway', 3, 3],
+  'DE': ['Germany', 2, 2],
   'JP': ['Japan', 10, 10],
-  'NO': ['Norway', 3, 3]
+  'IT': ['Italy', 4, 4]
 }
