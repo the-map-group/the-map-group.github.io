3208adc15e57237f3e076c023184c50763fad8b7
The Map Group <admin@the-map-group.top>
2025-01-06 10:51:20 -0300

    [auto] Updated map for member 'Alexander Sand Halvorsen'

--- people/airscan/user.py
+++ people/airscan/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/airscan/',
   'location': 'HØNEFOSS, Norway',
   'countries': 12,
-  'markers': 357,
-  'photos': 482
+  'markers': 358,
+  'photos': 483
 }
