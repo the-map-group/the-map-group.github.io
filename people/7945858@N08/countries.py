80d61574332118aed60288e977e2ce09a7978357
The Map Group <admin@the-map-group.top>
2025-01-07 17:43:10 -0300

    [auto] Updated map for member 'diffendale'

---
+++ people/7945858@N08/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'IE': ['Ireland', 4, 19],
+  'US': ['United States', 245, 1344],
+  'GR': ['Greece', 640, 4146],
+  'MA': ['Morocco', 119, 592],
+  'AT': ['Austria', 7, 130],
+  'CA': ['Canada', 3, 73],
+  'JO': ['Jordan', 5, 29],
+  'TR': ['Turkey', 37, 546],
+  'IT': ['Italy', 871, 7163],
+  'RO': ['Romania', 4, 48],
+  'GB': ['United Kingdom', 52, 334],
+  'EG': ['Egypt', 34, 397],
+  'PT': ['Portugal', 50, 184],
+  'DE': ['Germany', 20, 168],
+  'BG': ['Bulgaria', 12, 106],
+  'VA': ['Vatican City', 11, 173],
+  'FR': ['France', 2, 2]
+}
