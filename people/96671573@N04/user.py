6847a4636b34a573f75ff5aa3d063ca600168363
The Map Group <admin@the-map-group.top>
2025-01-07 17:54:54 -0300

    [auto] Updated map for member 'Gérard Cachon'

---
+++ people/96671573@N04/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '96671573@N04',
+  'alias': '96671573@N04',
+  'name': 'Gérard Cachon',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/96671573@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/bennett-cachon/',
+  'location': '',
+  'countries': 5,
+  'markers': 100,
+  'photos': 144
+}
