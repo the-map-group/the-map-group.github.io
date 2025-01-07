9c4e77f5b681174f87fd340dd3386bffcd5c4c55
The Map Group <admin@the-map-group.top>
2025-01-06 23:16:44 -0300

    [auto] Updated map for member 'Rubem Jr'

--- people/rubempjr/countries.py
+++ people/rubempjr/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'AR': ['Argentina', 61, 123],
   'RU': ['Russia', 1, 1],
-  'CL': ['Chile', 1, 1],
-  'IT': ['Italy', 320, 612],
-  'US': ['United States', 46, 156],
-  'FR': ['France', 6, 24],
-  'ES': ['Spain', 1522, 3993],
+  'PT': ['Portugal', 2675, 6263],
   'UY': ['Uruguay', 25, 185],
-  'PT': ['Portugal', 2675, 6262],
-  'GB': ['United Kingdom', 887, 2465],
+  'AR': ['Argentina', 61, 123],
+  'FR': ['France', 6, 24],
+  'US': ['United States', 46, 158],
+  'GB': ['United Kingdom', 887, 2466],
+  'IT': ['Italy', 320, 612],
+  'ES': ['Spain', 1522, 3994],
   'VA': ['Vatican City', 12, 15],
-  'BR': ['Brazil', 2577, 7153]
+  'BR': ['Brazil', 2577, 7154],
+  'CL': ['Chile', 1, 1]
 }
--- people/rubempjr/user.py
+++ people/rubempjr/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Rio de Janeiro, Brasil',
   'countries': 12,
   'markers': 8133,
-  'photos': 20990
+  'photos': 20996
 }
