a030479e3ce9933993856c075edccad47d906ed0
The Map Group <admin@the-map-group.top>
2025-01-07 23:54:53 -0300

    [auto] Removed member 'joeinpenticton'

--- people/joeinpenticton/countries.py
+++
@@ -1,9 +0,0 @@
-countries_dict = {
-  'CU': ['Cuba', 1, 1],
-  'FR': ['France', 1, 1],
-  'CA': ['Canada', 532, 541],
-  'ES': ['Spain', 7, 7],
-  'DE': ['Germany', 2, 2],
-  'PT': ['Portugal', 36, 37],
-  'US': ['United States', 138, 139]
-}
