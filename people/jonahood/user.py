cc89f2577048d0ad30f970c0f4eb3ed384663c33
The Map Group <admin@the-map-group.top>
2025-01-08 14:14:29 -0300

    [auto] Updated map for member 'glancingshots.uk'

--- people/jonahood/user.py
+++ people/jonahood/user.py
@@ -1,6 +1,6 @@
 user_info = {
   'id': '10363199@N06',
-  'alias': '10363199@N06',
+  'alias': 'jonahood',
   'name': 'jonathan hood',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/10363199@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/jonahood/',
