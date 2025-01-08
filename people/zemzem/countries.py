1aa408970b5d89f53560b2f24302ea6019d9d3c4
The Map Group <admin@the-map-group.top>
2025-01-08 14:15:10 -0300

    [auto] Updated map for member 'mouzhik'

--- people/zemzem/countries.py
+++ people/zemzem/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'DE': ['Germany', 3, 3],
-  'JO': ['Jordan', 4, 5],
-  'ES': ['Spain', 32, 36],
   'MY': ['Malaysia', 2, 43],
-  'MA': ['Morocco', 7, 8],
-  'FR': ['France', 231, 317],
-  'GB': ['United Kingdom', 1, 1],
   'AU': ['Australia', 7, 8],
+  'MA': ['Morocco', 7, 8],
+  'MX': ['Mexico', 2, 2],
+  'GB': ['United Kingdom', 1, 1],
+  'DE': ['Germany', 3, 3],
   'TR': ['Turkey', 1, 1],
-  'MX': ['Mexico', 2, 2]
+  'JO': ['Jordan', 4, 5],
+  'FR': ['France', 232, 318],
+  'ES': ['Spain', 32, 36]
 }
