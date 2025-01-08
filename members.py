233d54609ea68c95b3deccdfa7c7cbd5aebde5e9
The Map Group <admin@the-map-group.top>
2025-01-08 14:15:52 -0300

    [auto] Updated group map

--- members.py
+++ members.py
@@ -1,2 +1,16 @@
 members_list = [
+  ['23349126@N08', 'joeinpenticton', 'Joe (José)  Garcia', "https://live.staticflickr.com/5674/buddyicons/23349126@N08_r.jpg", 719, 730, 7],
+  ['193758259@N04', 'enricoveneruso', 'Enrico Veneruso', "https://live.staticflickr.com/5674/buddyicons/193758259@N04_r.jpg", 317, 320, 11],
+  ['7945858@N08', 'dandiffendale', 'Dan Diffendale', "https://live.staticflickr.com/5674/buddyicons/7945858@N08_r.jpg", 2116, 15454, 17],
+  ['98786299@N00', 'peterthoeny', 'Peter Thoeny - Quality HDR Pho', "https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg", 2259, 4136, 15],
+  ['96671573@N04', 'bennett-cachon', 'Gérard Cachon', "https://live.staticflickr.com/5674/buddyicons/96671573@N04_r.jpg", 100, 144, 5],
+  ['87940223@N08', 'renato59', 'ReNato \'59', "https://live.staticflickr.com/5674/buddyicons/87940223@N08_r.jpg", 479, 1636, 5],
+  ['86411192@N06', 'lucchesefabrizio', 'Fabrizio LUCCHESE', "https://live.staticflickr.com/5674/buddyicons/86411192@N06_r.jpg", 1368, 1372, 23],
+  ['15360731@N07', 'wa2wider', 'wa2wider', "https://live.staticflickr.com/5674/buddyicons/15360731@N07_r.jpg", 220, 243, 2],
+  ['42102968@N00', 'atifhussainphotography', 'Atif Hussain Photography', "https://live.staticflickr.com/5674/buddyicons/42102968@N00_r.jpg", 569, 575, 17],
+  ['28287831@N00', 'kimbar', 'Kim', "https://live.staticflickr.com/5674/buddyicons/28287831@N00_r.jpg", 6168, 8513, 69],
+  ['126251698@N03', 'miamiboy', 'Urban Florida Photographer', "https://live.staticflickr.com/5674/buddyicons/126251698@N03_r.jpg", 7822, 12901, 26],
+  ['10363199@N06', 'jonahood', 'jonathan hood', "https://live.staticflickr.com/5674/buddyicons/10363199@N06_r.jpg", 176, 899, 19],
+  ['167430854@N07', 'al_case', 'Al Case', "https://live.staticflickr.com/5674/buddyicons/167430854@N07_r.jpg", 735, 850, 20],
+  ['67878469@N00', 'zemzem', 'mouzhik', "https://live.staticflickr.com/5674/buddyicons/67878469@N00_r.jpg", 291, 425, 10]
 ]
