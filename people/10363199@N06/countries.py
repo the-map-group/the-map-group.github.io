95afa6d79221946af4b28c136a5acbf1e93b7f75
The Map Group <admin@the-map-group.top>
2025-01-07 22:51:24 -0300

    [auto] Updated map for member 'glancingshots.uk'

---
+++ people/10363199@N06/countries.py
@@ -0,0 +1,21 @@
+countries_dict = {
+  'IT': ['Italy', 1, 1],
+  'KE': ['Kenya', 1, 17],
+  'EE': ['Estonia', 1, 2],
+  'MV': ['Maldives', 2, 9],
+  'NZ': ['New Zealand', 12, 64],
+  'SY': ['Syria', 2, 2],
+  'GB': ['United Kingdom', 135, 364],
+  'IE': ['Ireland', 1, 55],
+  'SE': ['Sweden', 2, 38],
+  'PL': ['Poland', 1, 1],
+  'IS': ['Iceland', 1, 1],
+  'HR': ['Croatia', 1, 45],
+  'ZA': ['South Africa', 3, 19],
+  'EG': ['Egypt', 1, 3],
+  'GR': ['Greece', 2, 2],
+  'AR': ['Argentina', 1, 29],
+  'SD': ['Sudan', 2, 40],
+  'TR': ['Turkey', 1, 6],
+  'ET': ['Ethiopia', 6, 201]
+}
