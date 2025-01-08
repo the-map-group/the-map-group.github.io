a775d231c4150c082823b53c984cd259d31e4cc6
The Map Group <admin@the-map-group.top>
2025-01-08 14:02:02 -0300

    [auto] Updated map for member 'wa2wider'

--- people/wa2wider/user.py
+++ people/wa2wider/user.py
@@ -1,6 +1,6 @@
 user_info = {
   'id': '15360731@N07',
-  'alias': '15360731@N07',
+  'alias': 'wa2wider',
   'name': 'wa2wider',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/15360731@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/wa2wider/',
