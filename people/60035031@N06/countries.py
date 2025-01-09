52356b6ae688bccc50a1632f4bbd8cff136d41ce
The Map Group <admin@the-map-group.top>
2025-01-09 16:56:16 -0300

    [auto] Updated map for member 'acase1968'

--- people/60035031@N06/countries.py
+++ people/60035031@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'NL': ['Netherlands', 3, 3],
-  'JP': ['Japan', 141, 153],
-  'DK': ['Denmark', 159, 211],
-  'US': ['United States', 949, 1070],
-  'CZ': ['Czech Republic', 32, 33],
-  'DE': ['Germany', 11, 11],
   'HU': ['Hungary', 6, 6],
-  'GB': ['United Kingdom', 20, 21],
-  'MX': ['Mexico', 27, 27],
-  'IT': ['Italy', 6, 6],
+  'JP': ['Japan', 141, 153],
   'SE': ['Sweden', 35, 42],
   'NO': ['Norway', 2, 2],
   'IS': ['Iceland', 21, 21],
+  'US': ['United States', 949, 1070],
+  'IT': ['Italy', 6, 6],
+  'CZ': ['Czech Republic', 32, 33],
   'KR': ['South Korea', 23, 23],
-  'CH': ['Switzerland', 15, 16]
+  'DE': ['Germany', 11, 11],
+  'NL': ['Netherlands', 3, 3],
+  'DK': ['Denmark', 159, 211],
+  'CH': ['Switzerland', 15, 16],
+  'MX': ['Mexico', 27, 27],
+  'GB': ['United Kingdom', 20, 21]
 }
