9f24635b0490ed889cfd88d8869e4c0cd4ef14e6
The Map Group <admin@the-map-group.top>
2025-01-09 04:12:53 -0300

    [auto] Updated map for member 'Miguelanxo'

--- people/miguelanxo57/countries.py
+++ people/miguelanxo57/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'HU': ['Hungary', 3, 3],
-  'CZ': ['Czech Republic', 4, 4],
-  'ES': ['Spain', 296, 331],
   'GB': ['United Kingdom', 29, 36],
+  'FR': ['France', 5, 5],
+  'ES': ['Spain', 296, 331],
   'SK': ['Slovakia', 1, 1],
-  'AT': ['Austria', 1, 1],
-  'PT': ['Portugal', 52, 58],
   'DE': ['Germany', 4, 4],
-  'FR': ['France', 5, 5]
+  'PT': ['Portugal', 52, 58],
+  'CZ': ['Czech Republic', 4, 4],
+  'HU': ['Hungary', 3, 3],
+  'AT': ['Austria', 1, 1]
 }
