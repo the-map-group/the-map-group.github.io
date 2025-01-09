2f59af12ed9e620023237fbb8f29f5ec3599308b
The Map Group <admin@the-map-group.top>
2025-01-09 15:10:49 -0300

    [auto] Updated map for member 'Joe Bob Brim'

--- people/jbrimacombe/countries.py
+++ people/jbrimacombe/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'US': ['United States', 10, 37],
   'GB': ['United Kingdom', 7, 7],
   'AU': ['Australia', 672, 1483],
-  'US': ['United States', 10, 37],
   'NZ': ['New Zealand', 3, 3]
 }
