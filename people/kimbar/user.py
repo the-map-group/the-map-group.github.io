e4b46bf2fd5a9fa5744c199e720756f0e6b1ea22
The Map Group <admin@the-map-group.top>
2025-01-09 18:27:20 -0300

    [auto] Updated map for member 'kimbar/Thanks for 6.7 million views!'

--- people/kimbar/user.py
+++ people/kimbar/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/kimbar/',
   'location': 'Berkeley, CA, U.S.',
   'countries': 69,
-  'markers': 6168,
-  'photos': 8513
+  'markers': 6174,
+  'photos': 8519
 }
