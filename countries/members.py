2a1b71ce509f6aa1fceb611f496f6a2d91e8c7e3
The Map Group <admin@the-map-group.top>
2025-01-09 10:25:38 -0300

    [auto] Updated map for member 'H. P. Filho'

--- countries/members.py
+++ countries/members.py
@@ -1,2 +1,11 @@
 members_dict = {
+  'VA': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 22, 31]],
+  'AR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 93, 176]],
+  'IT': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 77, 126]],
+  'CL': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 78, 198]],
+  'HR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 184, 243]],
+  'US': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 50, 132]],
+  'UY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 36, 67]],
+  'BR': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 176, 394]],
+  'PY': [['30622510@N02', 'hpfilho', 'Haraldo Albergaria', 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg', 1, 1]]
 }
--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'BR': ['Brazil', 176, 394],
+  'US': ['United States', 50, 132],
+  'CL': ['Chile', 78, 198],
+  'VA': ['Vatican City', 22, 31],
+  'IT': ['Italy', 77, 126],
+  'PY': ['Paraguay', 1, 1],
   'AR': ['Argentina', 93, 176],
   'UY': ['Uruguay', 36, 67],
-  'BR': ['Brazil', 176, 394],
-  'IT': ['Italy', 77, 126],
-  'HR': ['Croatia', 184, 243],
-  'PY': ['Paraguay', 1, 1],
-  'VA': ['Vatican City', 22, 31],
-  'US': ['United States', 50, 132],
-  'CL': ['Chile', 78, 198]
+  'HR': ['Croatia', 184, 243]
 }
