c3c7bdaccc7997ececa43ebf3f21b85100bd4cdf
The Map Group <admin@the-map-group.top>
2025-01-09 04:54:23 -0300

    [auto] Updated map for member 'OMNIBUS DUBITANDUM'

--- people/max7777/countries.py
+++ people/max7777/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'AF': ['Afghanistan', 20, 30],
-  'US': ['United States', 3, 4],
-  'GB': ['United Kingdom', 1, 1],
-  'IN': ['India', 2, 2],
-  'IQ': ['Iraq', 1, 1],
   'JO': ['Jordan', 1, 1],
-  'TJ': ['Tajikistan', 1, 1],
-  'IR': ['Iran', 4, 4],
-  'EG': ['Egypt', 3, 3],
+  'AF': ['Afghanistan', 20, 30],
   'CN': ['China', 1, 5],
-  'PK': ['Pakistan', 29, 40]
+  'IQ': ['Iraq', 1, 1],
+  'EG': ['Egypt', 3, 3],
+  'IR': ['Iran', 4, 4],
+  'PK': ['Pakistan', 29, 40],
+  'TJ': ['Tajikistan', 1, 1],
+  'IN': ['India', 2, 2],
+  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States', 3, 4]
 }
