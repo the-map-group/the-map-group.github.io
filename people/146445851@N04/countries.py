468b1462113425833cd8c854d9a4c3c973a21821
The Map Group <admin@the-map-group.top>
2025-01-09 16:40:38 -0300

    [auto] Updated map for member 'giosuèbolis'

--- people/146445851@N04/countries.py
+++ people/146445851@N04/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'NE': ['Niger', 6, 7],
-  'VA': ['Vatican City', 1, 1],
-  'IT': ['Italy', 175, 217],
-  'FR': ['France', 6, 7],
-  'UA': ['Ukraine', 2, 2],
-  'CH': ['Switzerland', 12, 21],
+  'DZ': ['Algeria', 6, 7],
   'LY': ['Libya', 3, 3],
+  'IT': ['Italy', 175, 217],
+  'UA': ['Ukraine', 2, 2],
   'ML': ['Mali', 10, 11],
+  'CH': ['Switzerland', 12, 21],
+  'NE': ['Niger', 6, 7],
   'TR': ['Turkey', 2, 2],
+  'FR': ['France', 6, 7],
   'HU': ['Hungary', 1, 1],
-  'DZ': ['Algeria', 6, 7]
+  'VA': ['Vatican City', 1, 1]
 }
