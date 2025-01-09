22cbeeca50c317d480ac2917402da301dfc1904c
The Map Group <admin@the-map-group.top>
2025-01-09 04:47:14 -0300

    [auto] Updated map for member 'Stratman2'

--- people/stratman2/countries.py
+++ people/stratman2/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'MY': ['Malaysia', 475, 4375],
-  'SG': ['Singapore', 27, 200],
-  'ID': ['Indonesia', 15, 55],
   'US': ['United States', 1, 1],
-  'MW': ['Malawi', 1, 1]
+  'SG': ['Singapore', 27, 200],
+  'MW': ['Malawi', 1, 1],
+  'ID': ['Indonesia', 15, 55],
+  'MY': ['Malaysia', 475, 4375]
 }
