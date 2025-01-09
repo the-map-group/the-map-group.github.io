79d14e5ce64888e978252813ab4ea5bc505c6ff1
The Map Group <admin@the-map-group.top>
2025-01-09 16:58:43 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/countries.py
+++ people/zimwiz/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'AU': ['Australia', 1, 1],
+  'GL': ['Greenland', 1, 1],
+  'BM': ['Bermuda', 5, 5],
   'CA': ['Canada', 4, 4],
   'VN': ['Vietnam', 2, 2],
-  'BM': ['Bermuda', 5, 5],
-  'GL': ['Greenland', 1, 1],
+  'AU': ['Australia', 1, 1],
   'US': ['United States', 653, 926]
 }
