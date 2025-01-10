97de8db5b117fa114499086ec6fcbf5df967d3eb
The Map Group <admin@the-map-group.top>
2025-01-10 18:59:39 -0300

    [auto] Updated map for member 'Alexander Sand Halvorsen'

--- people/airscan/countries.py
+++ people/airscan/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'NO': ['Norway', 260, 374],
-  'US': ['United States', 2, 2],
-  'NL': ['Netherlands', 5, 5],
-  'FR': ['France', 25, 26],
-  'DE': ['Germany', 1, 1],
   'TH': ['Thailand', 2, 2],
-  'CH': ['Switzerland', 3, 3],
-  'VN': ['Vietnam', 1, 1],
-  'AT': ['Austria', 2, 2],
+  'IT': ['Italy', 9, 10],
+  'US': ['United States', 2, 2],
   'GB': ['United Kingdom', 47, 56],
+  'CH': ['Switzerland', 3, 3],
+  'NL': ['Netherlands', 5, 5],
   'SE': ['Sweden', 3, 3],
-  'IT': ['Italy', 9, 10]
+  'NO': ['Norway', 266, 380],
+  'DE': ['Germany', 1, 1],
+  'AT': ['Austria', 2, 2],
+  'FR': ['France', 25, 26],
+  'VN': ['Vietnam', 1, 1]
 }
--- people/airscan/user.py
+++ people/airscan/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/airscan/',
   'location': 'HØNEFOSS, Norway',
   'countries': 12,
-  'markers': 360,
-  'photos': 485
+  'markers': 366,
+  'photos': 491
 }
