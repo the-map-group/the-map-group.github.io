c5ddabb76406738316d66bc2effe33bcb42f7809
The Map Group <admin@the-map-group.top>
2025-01-11 18:38:34 -0300

    Reset map for member '23244764@N08'

--- people/23244764@N08/countries.py
+++ people/23244764@N08/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'AS': ['American Samoa', 1, 1],
-  'AQ': ['Antarctic Peninsula', 12, 12],
-  'US': ['United States', 165, 389],
-  'HT': ['Haiti', 1, 1],
-  'GI': ['Gibraltar', 20, 20],
-  'CA': ['Canada', 1, 1],
-  'PT': ['Portugal', 1, 1],
-  'MA': ['Morocco', 14, 14],
   'BZ': ['Belize', 5, 9],
-  'ES': ['Spain', 46, 69],
+  'CA': ['Canada', 1, 1],
+  'AS': ['American Samoa', 1, 1],
+  'US': ['United States', 165, 389],
+  'MA': ['Morocco', 14, 14],
+  'AQ': ['Antarctica', 12, 12],
   'BS': ['Bahamas', 2, 2],
-  'AW': ['Aruba', 13, 14]
+  'HT': ['Haiti', 1, 1],
+  'ES': ['Spain', 46, 69],
+  'GI': ['Gibraltar', 20, 20],
+  'AW': ['Aruba', 13, 14],
+  'PT': ['Portugal', 1, 1]
 }
