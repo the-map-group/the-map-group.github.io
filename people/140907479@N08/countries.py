4ffe7eb739e3a27e3e17be4aba82d8e92c7b1880
The Map Group <admin@the-map-group.top>
2025-01-12 16:14:38 -0300

    Reset map for member '140907479@N08'

--- people/140907479@N08/countries.py
+++ people/140907479@N08/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'IN': ['India', 27, 36],
-  'UZ': ['Uzbekistan', 1, 10],
   'NL': ['Netherlands', 3, 3],
-  'TR': ['Turkey', 33, 67],
-  'DE': ['Germany', 11, 11],
-  'FR': ['France', 1, 1],
-  'AQ': ['Antarctica', 1, 3],
   'RU': ['Russia', 844, 1053],
+  'IN': ['India', 27, 36],
+  'QA': ['Qatar', 4, 7],
+  'DE': ['Germany', 11, 11],
+  'TR': ['Turkey', 33, 67],
+  'UZ': ['Uzbekistan', 1, 10],
   'US': ['United States', 2, 2],
-  'QA': ['Qatar', 4, 7]
+  'FR': ['France', 1, 1],
+  'AQ': ['Antarctica', 1, 3]
 }
