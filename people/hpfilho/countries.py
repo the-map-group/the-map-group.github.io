745b1eec408fe3d8554a415dc02580e93faf607c
The Map Group <admin@the-map-group.top>
2025-01-12 13:19:56 -0300

    Reset map for member 'hpfilho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'HR': ['Croatia', 184, 243],
-  'UY': ['Uruguay', 36, 67],
-  'VA': ['Vatican City', 22, 31],
-  'AR': ['Argentina', 94, 178],
-  'PY': ['Paraguay', 1, 1],
-  'IT': ['Italy', 77, 126],
   'CL': ['Chile', 78, 198],
+  'AR': ['Argentina', 99, 178],
+  'PY': ['Paraguay', 1, 1],
+  'VA': ['Vatican City', 22, 31],
+  'UY': ['Uruguay', 38, 69],
   'US': ['United States', 50, 132],
+  'IT': ['Italy', 77, 126],
+  'HR': ['Croatia', 184, 243],
   'BR': ['Brazil', 176, 394]
 }
--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/hpfilho/',
   'location': 'Campinas, Brazil',
   'countries': 9,
-  'markers': 718,
-  'photos': 1370
+  'markers': 725,
+  'photos': 1372
 }
