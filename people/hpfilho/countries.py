bb7523b096bb042aa3d4b2b5178afe5ea934645b
The Map Group <admin@the-map-group.top>
2025-01-13 03:00:20 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'US': ['United States', 50, 132],
+  'UY': ['Uruguay', 39, 70],
   'CL': ['Chile', 78, 198],
   'AR': ['Argentina', 99, 178],
-  'PY': ['Paraguay', 1, 1],
-  'VA': ['Vatican City', 22, 31],
-  'UY': ['Uruguay', 38, 69],
-  'US': ['United States', 50, 132],
-  'IT': ['Italy', 77, 126],
   'HR': ['Croatia', 184, 243],
-  'BR': ['Brazil', 176, 394]
+  'PY': ['Paraguay', 1, 1],
+  'BR': ['Brazil', 176, 394],
+  'VA': ['Vatican City', 22, 31],
+  'IT': ['Italy', 77, 126]
 }
--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/hpfilho/',
   'location': 'Campinas, Brazil',
   'countries': 9,
-  'markers': 725,
-  'photos': 1372
+  'markers': 726,
+  'photos': 1373
 }
