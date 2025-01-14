9e7ea07e6fc92edd8faa6959d5fb49c3e03f8e42
The Map Group <admin@the-map-group.top>
2025-01-14 03:10:40 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'KE': ['Kenya', 169, 284],
-  'NL': ['Netherlands', 1, 1],
-  'TR': ['Turkey', 2, 2],
-  'QA': ['Qatar', 3, 3],
-  'IT': ['Italy', 351, 491],
   'GB': ['United Kingdom', 52, 95],
+  'IT': ['Italy', 351, 491],
   'CA': ['Canada', 10, 13],
+  'QA': ['Qatar', 3, 3],
+  'KE': ['Kenya', 169, 284],
+  'TR': ['Turkey', 2, 2],
+  'TZ': ['Tanzania', 313, 937],
+  'US': ['United States', 3668, 6424],
   'VA': ['Vatican City', 16, 20],
-  'MX': ['Mexico', 8, 10],
-  'US': ['United States', 3667, 6423],
-  'TZ': ['Tanzania', 313, 937]
+  'NL': ['Netherlands', 1, 1],
+  'MX': ['Mexico', 8, 10]
 }
--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/botetourt/',
   'location': 'Near Fincastle, Botetourt County, Va., USA',
   'countries': 11,
-  'markers': 4592,
-  'photos': 8279
+  'markers': 4593,
+  'photos': 8280
 }
