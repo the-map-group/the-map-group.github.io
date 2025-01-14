fec62dca1519d4e675ead44597eca79f8109a230
The Map Group <admin@the-map-group.top>
2025-01-14 03:17:40 -0300

    [auto] Updated map for member 'diffendale'

--- people/dandiffendale/user.py
+++ people/dandiffendale/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/dandiffendale/',
   'location': 'Naples, Italy',
   'countries': 17,
-  'markers': 2117,
-  'photos': 15463
+  'markers': 2118,
+  'photos': 15477
 }
