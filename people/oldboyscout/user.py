974f753e7c49d5e85709b141ce71bc0d72ee40cf
The Map Group <admin@the-map-group.top>
2025-01-14 15:11:07 -0300

    [auto] Updated map for member 'gsegelken'

--- people/oldboyscout/user.py
+++ people/oldboyscout/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/oldboyscout/',
   'location': '',
   'countries': 25,
-  'markers': 2304,
-  'photos': 2555
+  'markers': 2305,
+  'photos': 2556
 }
