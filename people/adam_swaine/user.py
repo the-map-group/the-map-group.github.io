738f0cc1fe03123a722ac14f1d642e72e06b1966
The Map Group <admin@the-map-group.top>
2025-01-17 15:32:52 -0300

    [auto] Updated map for member 'Adam Swaine'

--- people/adam_swaine/user.py
+++ people/adam_swaine/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/adam_swaine/',
   'location': 'london, england',
   'countries': 5,
-  'markers': 17006,
-  'photos': 25290
+  'markers': 17009,
+  'photos': 25293
 }
