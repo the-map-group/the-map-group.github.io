5bc4948493c44d06f9757bee56bd370d26cbdbe1
The Map Group <admin@the-map-group.top>
2025-01-17 03:17:59 -0300

    [auto] Updated map for member 'Gmlskis'

--- people/38730049@N08/countries.py
+++ people/38730049@N08/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States', 976, 59038]
+  'US': ['United States', 976, 59037]
 }
--- people/38730049@N08/user.py
+++ people/38730049@N08/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Visalia, California, USA',
   'countries': 1,
   'markers': 976,
-  'photos': 59038
+  'photos': 59037
 }
