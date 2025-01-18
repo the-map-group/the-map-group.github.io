2a4386bbc0381aedfc8ac2be5305661904518f1e
The Map Group <admin@the-map-group.top>
2025-01-18 03:08:09 -0300

    [auto] Updated map for member 'mpfau64'

--- people/120177649@N03/countries.py
+++ people/120177649@N03/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'JE': ['Jersey', 14, 81],
   'AT': ['Austria', 4, 16],
   'BE': ['Belgium', 3, 29],
-  'JE': ['Jersey', 14, 81],
+  'CH': ['Switzerland', 32, 123],
   'FR': ['France', 124, 802],
-  'IT': ['Italy', 18, 72],
+  'GB': ['United Kingdom', 680, 5039],
   'IE': ['Ireland', 19, 84],
   'DE': ['Germany', 661, 3267],
-  'CH': ['Switzerland', 32, 123],
-  'GB': ['United Kingdom', 680, 5032]
+  'IT': ['Italy', 18, 72]
 }
