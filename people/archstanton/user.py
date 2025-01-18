8075a3d3b870c5f6fb2fbd353a63e71972677b12
The Map Group <admin@the-map-group.top>
2025-01-18 03:16:06 -0300

    [auto] Updated map for member '- Ozymandias -'

--- people/archstanton/user.py
+++ people/archstanton/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/archstanton/',
   'location': '',
   'countries': 29,
-  'markers': 30293,
-  'photos': 36716
+  'markers': 30308,
+  'photos': 36737
 }
