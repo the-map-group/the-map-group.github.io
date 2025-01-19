99b3b705e0b866e05666dd9ffb314e5cd9eeca72
The Map Group <admin@the-map-group.top>
2025-01-19 03:36:48 -0300

    [auto] Updated map for member 'Thanks for 10.1 Million Views'

--- people/joeinpenticton/countries.py
+++ people/joeinpenticton/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'CA': ['Canada', 535, 544],
   'FR': ['France', 1, 1],
+  'CA': ['Canada', 536, 545],
   'PT': ['Portugal', 38, 39],
   'DE': ['Germany', 2, 2],
   'CU': ['Cuba', 1, 1],
--- people/joeinpenticton/user.py
+++ people/joeinpenticton/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/joeinpenticton/',
   'location': 'Penticton, British Columbia, Canada',
   'countries': 7,
-  'markers': 724,
-  'photos': 735
+  'markers': 725,
+  'photos': 736
 }
