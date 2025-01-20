e7570c97008d6f87a48f953f375442a152eeddce
The Map Group <admin@the-map-group.top>
2025-01-20 03:34:05 -0300

    [auto] Updated map for member 'Crystal.Rain'

--- people/140907479@N08/countries.py
+++ people/140907479@N08/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'TR': ['Turkey', 33, 67],
-  'NL': ['Netherlands', 3, 3],
-  'QA': ['Qatar', 4, 7],
-  'DE': ['Germany', 11, 11],
-  'UZ': ['Uzbekistan', 1, 10],
   'US': ['United States', 2, 2],
+  'NL': ['Netherlands', 3, 3],
   'IN': ['India', 27, 36],
   'FR': ['France', 1, 1],
-  'RU': ['Russia', 844, 1054]
+  'DE': ['Germany', 11, 11],
+  'QA': ['Qatar', 4, 7],
+  'UZ': ['Uzbekistan', 1, 10],
+  'RU': ['Russia', 847, 1057],
+  'TR': ['Turkey', 33, 67]
 }
--- people/140907479@N08/user.py
+++ people/140907479@N08/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/140907479@N08/',
   'location': 'Russia',
   'countries': 9,
-  'markers': 927,
-  'photos': 1194
+  'markers': 930,
+  'photos': 1197
 }
