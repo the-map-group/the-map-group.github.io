448c970187db450cdf4c6b0a87e5d98671cda177
The Map Group <admin@the-map-group.top>
2025-01-21 15:25:03 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/peterthoeny/',
   'location': 'Cupertino, USA',
   'countries': 15,
-  'markers': 2265,
-  'photos': 4148
+  'markers': 2266,
+  'photos': 4149
 }
