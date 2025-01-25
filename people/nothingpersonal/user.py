4b45f26695c111edec03010b86e7d429326f98f5
The Map Group <admin@the-map-group.top>
2025-01-25 15:03:26 -0300

    [auto] Updated map for member 'Dmitry Djouce'

--- people/nothingpersonal/user.py
+++ people/nothingpersonal/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/nothingpersonal/',
   'location': 'London',
   'countries': 16,
-  'markers': 2638,
-  'photos': 2958
+  'markers': 2683,
+  'photos': 3003
 }
