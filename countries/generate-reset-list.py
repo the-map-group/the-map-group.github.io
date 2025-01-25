8683beedd72b57c44f917f7d4bcc31c2013365ea
The Map Group <admin@the-map-group.top>
2025-01-25 11:42:30 -0300

    [auto] Updated map for member 'H. P. Filho'

---
+++ countries/generate-reset-list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from members import members_dict
+
+countries_codes = ['BR', 'PY', 'AR', 'FK']
+
+members_list = []
+for code in countries_codes:
+    for member in members_dict[code]:
+        member_alias = member[1]
+        if member_alias not in members_list:
+            members_list.append(member_alias)
+
+reset_file = open('reset.py', 'w')
+reset_file.write("reset_list = [\n")
+for member in members_list:
+    reset_file.write("  \'{}\',\n".format(member))
+reset_file.write("]\n")
+reset_file.close()
--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'US': ['United States', 50, 132],
-  'VA': ['Vatican City', 22, 31],
-  'HR': ['Croatia', 184, 243],
   'IT': ['Italy', 77, 126],
   'UY': ['Uruguay', 47, 80],
-  'PY': ['Paraguay', 2, 4],
   'CL': ['Chile', 78, 198],
   'AR': ['Argentina', 98, 177],
-  'BR': ['Brazil', 176, 392]
+  'PY': ['Paraguay', 2, 4],
+  'VA': ['Vatican City', 22, 31],
+  'US': ['United States', 50, 132],
+  'BR': ['Brazil', 176, 392],
+  'HR': ['Croatia', 184, 243]
 }
