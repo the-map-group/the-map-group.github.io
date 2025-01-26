ff226ee2988e5a3d18911951be85703a149b6850
The Map Group <admin@the-map-group.top>
2025-01-26 02:59:20 -0300

    [auto] Updated map for member 'Joao de Mio'

--- people/23402613@N03/countries.py
+++ people/23402613@N03/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'CL': ['Chile', 11, 13],
   'IT': ['Italy', 50, 59],
-  'BR': ['Brazil', 69, 111],
   'FR': ['France', 1, 1],
-  'CL': ['Chile', 11, 13]
+  'BR': ['Brazil', 69, 111]
 }
