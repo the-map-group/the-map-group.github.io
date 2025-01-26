a2f00a437fc7ec5de28bff266291c1de0901f31f
The Map Group <admin@the-map-group.top>
2025-01-26 17:24:25 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/countries.py
+++ people/fotosavulso/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'US': ['United States', 3, 3],
   'IT': ['Italy', 1, 1],
-  'ST': ['Sao Tome and Principe', 102, 109],
-  'AT': ['Austria', 1, 1],
-  'CV': ['Cape Verde', 32, 37],
   'FR': ['France', 63, 65],
-  'PT': ['Portugal', 339, 365],
+  'US': ['United States', 3, 3],
+  'ST': ['Sao Tome and Principe', 102, 109],
   'AD': ['Andorra', 2, 2],
-  'ES': ['Spain', 25, 27]
+  'AT': ['Austria', 1, 1],
+  'ES': ['Spain', 25, 27],
+  'PT': ['Portugal', 339, 365],
+  'CV': ['Cape Verde', 32, 37]
 }
