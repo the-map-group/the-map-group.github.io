11e85bcc6e65743d768b9e44c4877d621fdaca5e
The Map Group <admin@the-map-group.top>
2025-01-26 19:35:33 -0300

    [auto] Updated map for member 'acase1968'

--- people/60035031@N06/countries.py
+++ people/60035031@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'HU': ['Hungary', 6, 6],
-  'JP': ['Japan', 141, 153],
-  'SE': ['Sweden', 35, 42],
   'NO': ['Norway', 2, 2],
-  'IS': ['Iceland', 21, 21],
-  'US': ['United States', 949, 1070],
-  'IT': ['Italy', 6, 6],
-  'CZ': ['Czech Republic', 32, 33],
-  'KR': ['South Korea', 23, 23],
-  'DE': ['Germany', 11, 11],
-  'NL': ['Netherlands', 3, 3],
   'DK': ['Denmark', 159, 211],
+  'HU': ['Hungary', 6, 6],
+  'NL': ['Netherlands', 3, 3],
   'CH': ['Switzerland', 15, 16],
+  'IS': ['Iceland', 21, 21],
+  'JP': ['Japan', 141, 153],
+  'CZ': ['Czech Republic', 32, 33],
+  'US': ['United States', 949, 1070],
+  'DE': ['Germany', 11, 11],
+  'GB': ['United Kingdom', 20, 21],
   'MX': ['Mexico', 27, 27],
-  'GB': ['United Kingdom', 20, 21]
+  'IT': ['Italy', 6, 6],
+  'SE': ['Sweden', 35, 42],
+  'KR': ['South Korea', 23, 23]
 }
