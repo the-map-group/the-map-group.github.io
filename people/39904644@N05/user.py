e25a8251f3822dd91949f21cfbfcb47d05916015
The Map Group <admin@the-map-group.top>
2025-02-04 06:08:56 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/user.py
+++ people/39904644@N05/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/39904644@N05/',
   'location': 'digitale Suppe, Deutschland',
   'countries': 19,
-  'markers': 1733,
-  'photos': 2022
+  'markers': 1735,
+  'photos': 2025
 }
