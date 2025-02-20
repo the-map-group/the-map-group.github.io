499f2caf45ccc891e78faac6e4dfe8e9812ef249
The Map Group <admin@the-map-group.top>
2025-02-20 10:31:53 -0300

    [auto] Updated map for member 'dimitri.verledens'

--- people/138182637@N08/user.py
+++ people/138182637@N08/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/138182637@N08/',
   'location': 'Gent, België',
   'countries': 27,
-  'markers': 1007,
-  'photos': 1310
+  'markers': 1008,
+  'photos': 1311
 }
