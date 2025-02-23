f2977852b5a105bf7223e2a3205f4bf321dc7781
The Map Group <admin@the-map-group.top>
2025-02-23 11:51:12 -0300

    [auto] Updated map for member 'Pieter Lommerse'

---
+++ people/charlesinshanghai/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'ID': ['Indonesia', 6, 9],
+  'US': ['United States', 2, 2],
+  'CA': ['Canada', 1, 1],
+  'FR': ['France', 2, 3],
+  'CN': ['China', 156, 221],
+  'TR': ['Turkey', 2, 2],
+  'AU': ['Australia', 2, 5],
+  'ES': ['Spain', 1, 1],
+  'IN': ['India', 2, 2],
+  'NO': ['Norway', 1, 2],
+  'IT': ['Italy', 3, 3],
+  'DE': ['Germany', 3, 5],
+  'JP': ['Japan', 16, 25],
+  'NL': ['Netherlands', 47, 76],
+  'EG': ['Egypt', 2, 2],
+  'GB': ['United Kingdom', 8, 12],
+  'VN': ['Vietnam', 1, 1]
+}
