99ab9f2f0a77ecdf72eb169afbb1752c100bfc21
The Map Group <admin@the-map-group.top>
2025-02-24 10:27:24 -0300

    [auto] Updated map for member 'pearled.42.guild'

---
+++ people/202296413@N02/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '202296413@N02',
+  'alias': '202296413@N02',
+  'name': 'Kiefer',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/202296413@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/202296413@N02/',
+  'location': '',
+  'countries': 0,
+  'markers': 0,
+  'photos': 0
+}
