5af6be60f6feee9df3c429c996c34f1011ed3fab
The Map Group <admin@the-map-group.top>
2025-03-03 15:21:15 -0300

    [auto] Updated map for member 'ManxieB- sorry, struggling!'

--- people/barbaraelaw/countries.py
+++ people/barbaraelaw/countries.py
@@ -1,5 +1,6 @@
 countries_dict = {
+  'TZ': ['Tanzania', 59, 318],
   'IE': ['Ireland', 35, 53],
-  'GB': ['United Kingdom', 2396, 2956],
-  'TZ': ['Tanzania', 59, 318]
+  'GB': ['United Kingdom', 334, 453],
+  'IM': ['Isle of Man', 2062, 2503]
 }
--- people/barbaraelaw/user.py
+++ people/barbaraelaw/user.py
@@ -5,7 +5,7 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/barbaraelaw/',
   'location': 'Kirk Michael, Isle of Man',
-  'countries': 3,
+  'countries': 4,
   'markers': 2490,
   'photos': 3327
 }
