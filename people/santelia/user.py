53bc38e62c1e91cc317eedb59c78b3b0c305f33f
The Map Group <admin@the-map-group.top>
2025-03-03 14:08:02 -0300

    [auto] Updated map for member 'alfsan'

--- people/santelia/user.py
+++ people/santelia/user.py
@@ -5,7 +5,7 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/32557441@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/santelia/',
   'location': 'Milano, Italia',
-  'countries': 34,
+  'countries': 33,
   'markers': 721,
   'photos': 1049
 }
