e94edda58046d404f74f9b2fd053c4303a727b12
The Map Group <admin@the-map-group.top>
2025-03-03 13:35:07 -0300

    [auto] Updated map for member 'Northern Tony'

--- people/22572253@N08/countries.py
+++ people/22572253@N08/countries.py
@@ -1,7 +1,6 @@
 countries_dict = {
-  'CA': ['Canada', 2, 2],
   'IT': ['Italy', 10, 10],
-  'GB': ['United Kingdom', 69, 69],
-  'US': ['United States', 18, 18],
-  'VA': ['Vatican City', 1, 1]
+  'US': ['United States', 20, 20],
+  'VA': ['Vatican City', 1, 1],
+  'GB': ['United Kingdom', 69, 69]
 }
--- people/22572253@N08/user.py
+++ people/22572253@N08/user.py
@@ -5,7 +5,7 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/22572253@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/22572253@N08/',
   'location': '',
-  'countries': 5,
+  'countries': 4,
   'markers': 100,
   'photos': 100
 }
