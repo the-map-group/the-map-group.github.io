7350b119c667d2b40bfcb4227c382651203c338a
The Map Group <admin@the-map-group.top>
2025-03-04 04:24:44 -0300

    [auto] Updated map for member 'khrawlings'

--- people/khrawlings/user.py
+++ people/khrawlings/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/khrawlings/',
   'location': '',
   'countries': 21,
-  'markers': 2957,
-  'photos': 3862
+  'markers': 2959,
+  'photos': 3868
 }
