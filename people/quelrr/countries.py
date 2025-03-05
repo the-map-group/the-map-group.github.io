04d426458280eecdc81b33c40435214b457d548c
The Map Group <admin@the-map-group.top>
2025-03-05 18:05:00 -0300

    [auto] Updated map for member 'Quel.R.R'

--- people/quelrr/countries.py
+++ people/quelrr/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
   'IT': ['Italy', 3, 3],
-  'VA': ['Vatican City', 1, 1],
+  'AR': ['Argentina', 3, 3],
   'BR': ['Brazil', 22, 35],
   'HR': ['Croatia', 2, 2],
   'US': ['United States', 4, 4],
-  'AR': ['Argentina', 3, 3]
+  'VA': ['Vatican City', 1, 1]
 }
