1fc036c380d433a1ccf839b22c15a4b630fe7a2b
The Map Group <admin@the-map-group.top>
2025-03-06 00:00:50 -0300

    [auto] Updated map for member 'SeaLord 50+'

--- people/186479257@N03/countries.py
+++ people/186479257@N03/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'IT': ['Italy', 2, 2],
-  'EG': ['Egypt', 1, 1],
   'HR': ['Croatia', 8, 13],
-  'VA': ['Vatican City', 1, 1],
   'ES': ['Spain', 1, 1],
-  'PT': ['Portugal', 1, 1],
-  'DE': ['Germany', 68, 73],
-  'GB': ['United Kingdom', 1, 1],
-  'DK': ['Denmark', 1, 2],
+  'US': ['United States', 2, 2],
   'TR': ['Turkey', 1, 1],
-  'US': ['United States', 2, 2]
+  'DE': ['Germany', 68, 73],
+  'EG': ['Egypt', 1, 1],
+  'GB': ['United Kingdom', 1, 1],
+  'IT': ['Italy', 2, 2],
+  'VA': ['Vatican City', 1, 1],
+  'DK': ['Denmark', 1, 2],
+  'PT': ['Portugal', 1, 1]
 }
