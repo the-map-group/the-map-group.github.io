2ea9d0c33d621cebe66c6bda8da3630230f1ad3d
The Map Group <admin@the-map-group.top>
2025-03-05 21:40:27 -0300

    [auto] Updated map for member 'InnAtElmwood'

--- people/12513181@N07/countries.py
+++ people/12513181@N07/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'US': ['United States', 540, 703],
-  'TC': ['Turks and Caicos Islands', 10, 19],
+  'FR': ['France', 1, 1],
   'GR': ['Greece', 1, 1],
-  'FR': ['France', 1, 1]
+  'TC': ['Turks and Caicos Islands', 10, 19]
 }
