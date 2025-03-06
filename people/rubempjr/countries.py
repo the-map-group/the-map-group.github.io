4304ec872874366165563a40783278fc07e5fcc0
The Map Group <admin@the-map-group.top>
2025-03-06 15:32:47 -0300

    [auto] Updated map for member 'Rubem Jr'

--- people/rubempjr/countries.py
+++ people/rubempjr/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'GB': ['United Kingdom', 887, 2466],
-  'FR': ['France', 6, 24],
   'IT': ['Italy', 320, 612],
+  'ES': ['Spain', 1479, 3833],
+  'MX': ['Mexico', 2, 5],
+  'BR': ['Brazil', 2580, 7167],
   'RU': ['Russia', 1, 1],
-  'ES': ['Spain', 1478, 3833],
-  'BR': ['Brazil', 2579, 7159],
+  'PT': ['Portugal', 2675, 6260],
+  'GB': ['United Kingdom', 887, 2466],
+  'US': ['United States', 46, 159],
+  'VA': ['Vatican City', 12, 15],
+  'FR': ['France', 6, 24],
   'UY': ['Uruguay', 27, 209],
   'CL': ['Chile', 1, 1],
-  'PT': ['Portugal', 2675, 6261],
-  'AR': ['Argentina', 61, 123],
-  'MX': ['Mexico', 2, 5],
-  'US': ['United States', 46, 159],
-  'VA': ['Vatican City', 12, 15]
+  'AR': ['Argentina', 61, 123]
 }
--- people/rubempjr/user.py
+++ people/rubempjr/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/rubempjr/',
   'location': 'Rio de Janeiro, Brasil',
   'countries': 13,
-  'markers': 8143,
-  'photos': 21038
+  'markers': 8145,
+  'photos': 21045
 }
