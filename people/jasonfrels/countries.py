b5b92bfade5118cb0e68898e74fe198712a97605
The Map Group <admin@the-map-group.top>
2025-03-07 18:17:37 -0300

    [auto] Updated map for member 'Jason Frels'

--- people/jasonfrels/countries.py
+++ people/jasonfrels/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'CA': ['Canada', 4, 5],
   'SG': ['Singapore', 1, 1],
-  'AR': ['Argentina', 1, 1],
-  'SJ': ['Svalbard and Jan Mayen', 19, 19],
   'CL': ['Chile', 9, 10],
+  'AR': ['Argentina', 1, 1],
+  'CA': ['Canada', 4, 5],
+  'SJ': ['Svalbard and Jan Mayen', 19, 19],
   'US': ['United States', 121, 144]
 }
