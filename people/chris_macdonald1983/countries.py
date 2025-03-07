53afd7bf15f092ad81d8c930b88ac1dd69929e7b
The Map Group <admin@the-map-group.top>
2025-03-07 12:27:25 -0300

    [auto] Updated map for member 'CJphoto83'

--- people/chris_macdonald1983/countries.py
+++ people/chris_macdonald1983/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'US': ['United States', 25, 31],
-  'CN': ['China', 31, 42],
   'CA': ['Canada', 4, 4],
-  'SG': ['Singapore', 14, 24]
+  'US': ['United States', 25, 31],
+  'SG': ['Singapore', 14, 24],
+  'CN': ['China', 31, 42]
 }
