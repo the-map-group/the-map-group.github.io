9aaf20f449eedd8ca89f34961b11a431ab389dd4
The Map Group <admin@the-map-group.top>
2025-03-07 03:08:09 -0300

    [auto] Updated map for member 'K@moeiras'

--- people/kamo_kamoeiras/countries.py
+++ people/kamo_kamoeiras/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'AD': ['Andorra', 2, 2],
-  'GI': ['Gibraltar', 4, 4],
-  'PT': ['Portugal', 33, 43],
-  'ES': ['Spain', 1914, 2520],
   'FR': ['France', 2, 2],
-  'MX': ['Mexico', 1, 2]
+  'ES': ['Spain', 1916, 2522],
+  'AD': ['Andorra', 2, 2],
+  'PT': ['Portugal', 33, 43],
+  'MX': ['Mexico', 1, 2],
+  'GI': ['Gibraltar', 4, 4]
 }
--- people/kamo_kamoeiras/user.py
+++ people/kamo_kamoeiras/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/kamo_kamoeiras/',
   'location': 'Salamanca, ESPAÑA',
   'countries': 6,
-  'markers': 1956,
-  'photos': 2573
+  'markers': 1958,
+  'photos': 2575
 }
