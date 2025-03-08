3f6f9cf15d958baa563f92e9c610bd5caef8cb16
The Map Group <admin@the-map-group.top>
2025-03-08 19:57:54 -0300

    [auto] Updated map for member 'dwightcarr_77'

--- people/197276323@N06/user.py
+++ people/197276323@N06/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '197276323@N06',
   'alias': '197276323@N06',
   'name': 'Dwight Carr',
-  'avatar': '../../icons/photographer.svg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/197276323@N06/',
   'location': '',
   'countries': 1,
