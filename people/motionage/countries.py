f0776b72876eacaa0437125949ce674b09138874
The Map Group <admin@the-map-group.top>
2025-03-08 09:09:14 -0300

    [auto] Updated map for member 'asarstudios'

--- people/motionage/countries.py
+++ people/motionage/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'NZ': ['New Zealand', 1, 2],
-  'GB': ['United Kingdom', 3, 36],
   'DE': ['Germany', 2, 2],
-  'TR': ['Turkey', 1, 1],
-  'US': ['United States', 32, 46]
+  'US': ['United States', 32, 46],
+  'GB': ['United Kingdom', 3, 36],
+  'NZ': ['New Zealand', 1, 2],
+  'TR': ['Turkey', 1, 1]
 }
