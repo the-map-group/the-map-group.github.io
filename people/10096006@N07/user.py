7c246324f90d32b88604288780d3c5e90245a452
The Map Group <admin@the-map-group.top>
2025-03-08 20:01:39 -0300

    [auto] Updated map for member 'ceri1965'

--- people/10096006@N07/user.py
+++ people/10096006@N07/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '10096006@N07',
   'alias': '10096006@N07',
   'name': 'ceri1965',
-  'avatar': '../../icons/photographer.svg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/10096006@N07/',
   'location': '',
   'countries': 1,
