16916499066cacee687f998ec0500139b1b31b13
The Map Group <admin@the-map-group.top>
2025-03-08 01:56:18 -0300

    [auto] Updated map for member 'Mister Oy'

--- people/misteroy/user.py
+++ people/misteroy/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/misteroy/',
   'location': '',
   'countries': 2,
-  'markers': 4252,
-  'photos': 6080
+  'markers': 4253,
+  'photos': 6081
 }
