ad0687519257a6b760317efd5d99e1167630a934
The Map Group <admin@the-map-group.top>
2025-03-07 21:21:05 -0300

    [auto] Updated map for member 'benbobjr'

--- people/benbobjr/countries.py
+++ people/benbobjr/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'BE': ['Belgium', 111, 253],
-  'SE': ['Sweden', 25, 144],
-  'AE': ['United Arab Emirates', 167, 175],
-  'DK': ['Denmark', 39, 135],
   'AD': ['Andorra', 3, 7],
-  'DE': ['Germany', 14, 66],
+  'CN': ['China', 12, 132],
   'FR': ['France', 40, 129],
+  'US': ['United States', 221, 585],
+  'DE': ['Germany', 14, 66],
+  'GB': ['United Kingdom', 37067, 48305],
   'AT': ['Austria', 32, 164],
   'IT': ['Italy', 327, 496],
-  'AU': ['Australia', 419, 763],
+  'AE': ['United Arab Emirates', 167, 175],
   'LU': ['Luxembourg', 10, 50],
+  'BE': ['Belgium', 111, 253],
   'ES': ['Spain', 2, 6],
+  'AU': ['Australia', 419, 763],
+  'SE': ['Sweden', 25, 144],
   'NL': ['Netherlands', 29, 33],
-  'CN': ['China', 12, 132],
-  'GB': ['United Kingdom', 37067, 48305],
-  'US': ['United States', 221, 585]
+  'DK': ['Denmark', 39, 135]
 }
