51c73759b926ab43c01c1012ba387b101b869d8d
The Map Group <admin@the-map-group.top>
2025-03-08 15:06:55 -0300

    [auto] Updated map for member 'Wayloncash'

---
+++ people/wayloncash/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '97555832@N08',
+  'alias': 'wayloncash',
+  'name': 'Wayloncash',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/97555832@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/wayloncash/',
+  'location': '',
+  'countries': 27,
+  'markers': 22279,
+  'photos': 46900
+}
