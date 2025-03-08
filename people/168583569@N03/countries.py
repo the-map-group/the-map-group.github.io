513c5433af7ab24af2afa4035bad50eff2724968
The Map Group <admin@the-map-group.top>
2025-03-07 23:39:33 -0300

    [auto] Updated map for member 'amalthus'

--- people/168583569@N03/countries.py
+++ people/168583569@N03/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'US': ['United States', 8, 8],
-  'GB': ['United Kingdom', 1, 1],
-  'IT': ['Italy', 6, 6],
   'FR': ['France', 3, 3],
-  'AT': ['Austria', 1, 1],
-  'CY': ['Cyprus', 2, 2],
-  'PT': ['Portugal', 1, 1],
-  'BR': ['Brazil', 1, 1],
+  'GB': ['United Kingdom', 1, 1],
   'HR': ['Croatia', 1, 1],
   'ES': ['Spain', 1, 1],
-  'LB': ['Lebanon', 1, 1],
-  'UG': ['Uganda', 1, 1],
+  'AT': ['Austria', 1, 1],
   'GR': ['Greece', 18, 38],
+  'UG': ['Uganda', 1, 1],
+  'CY': ['Cyprus', 2, 2],
+  'DE': ['Germany', 13, 204],
+  'US': ['United States', 8, 8],
+  'LB': ['Lebanon', 1, 1],
   'BG': ['Bulgaria', 1, 1],
-  'DE': ['Germany', 13, 204]
+  'PT': ['Portugal', 1, 1],
+  'IT': ['Italy', 6, 6],
+  'BR': ['Brazil', 1, 1]
 }
