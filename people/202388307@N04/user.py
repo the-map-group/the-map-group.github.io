65b75f362e87f13095ef4c8ecd7d77d583ef5053
The Map Group <admin@the-map-group.top>
2025-03-09 14:53:06 -0300

    [auto] Updated map for member 'aothman.ksa'

---
+++ people/202388307@N04/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '202388307@N04',
+  'alias': '202388307@N04',
+  'name': 'Abdelaziz Osman',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/202388307@N04/',
+  'location': '',
+  'countries': 0,
+  'markers': 0,
+  'photos': 0
+}
