50d94535fba75d9b3c6612d15dbae28811e376a6
The Map Group <admin@the-map-group.top>
2025-03-10 13:43:52 -0300

    [auto] Updated map for member 'Fabrizio Lucchese'

--- people/lucchesefabrizio/user.py
+++ people/lucchesefabrizio/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/lucchesefabrizio/',
   'location': 'Udine, Italy',
   'countries': 23,
-  'markers': 1389,
-  'photos': 1395
+  'markers': 1392,
+  'photos': 1398
 }
