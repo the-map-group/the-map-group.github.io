2e830f0af94cf2707b03d2f26493b316715111e7
The Map Group <admin@the-map-group.top>
2025-03-13 03:00:32 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'US': ['United States', 728, 1019],
+  'NZ': ['New Zealand', 9, 9],
+  'US': ['United States', 729, 1020],
+  'IT': ['Italy', 186, 245],
+  'GB': ['United Kingdom', 26, 28],
+  'CH': ['Switzerland', 6, 9],
   'CA': ['Canada', 90, 158],
   'RU': ['Russia', 1, 1],
   'AU': ['Australia', 11, 11],
   'PA': ['Panama', 1, 2],
-  'GB': ['United Kingdom', 26, 28],
-  'NZ': ['New Zealand', 9, 9],
-  'IT': ['Italy', 186, 245],
-  'VA': ['Vatican City', 25, 32],
-  'CH': ['Switzerland', 6, 9]
+  'VA': ['Vatican City', 25, 32]
 }
--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/chrispricephotography/',
   'location': 'Blacksburg, VA, USA',
   'countries': 10,
-  'markers': 1083,
-  'photos': 1514
+  'markers': 1084,
+  'photos': 1515
 }
