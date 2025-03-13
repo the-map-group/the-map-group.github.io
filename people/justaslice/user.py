901246bdf7fffc7ce31924115afee56ac27de4a9
The Map Group <admin@the-map-group.top>
2025-03-13 03:05:40 -0300

    [auto] Updated map for member '█ Slices of Light ✴ █▀ ▀ ▀'

--- people/justaslice/user.py
+++ people/justaslice/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/justaslice/',
   'location': '',
   'countries': 30,
-  'markers': 3137,
-  'photos': 4832
+  'markers': 3138,
+  'photos': 4833
 }
