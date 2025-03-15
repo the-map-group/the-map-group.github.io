e1779a38b7b579f1cf09a88bcedc8546a9de738f
The Map Group <admin@the-map-group.top>
2025-03-15 03:07:05 -0300

    [auto] Updated map for member 'Mani_H.'

--- people/manfred_h/countries.py
+++ people/manfred_h/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'DE': ['Germany', 636, 1259],
-  'FR': ['France', 22, 31],
   'NL': ['Netherlands', 39, 55],
-  'IT': ['Italy', 1, 1],
-  'SE': ['Sweden', 2, 3],
   'DK': ['Denmark', 33, 39],
+  'GB': ['United Kingdom', 1, 2],
+  'DE': ['Germany', 636, 1260],
+  'IT': ['Italy', 1, 1],
   'BE': ['Belgium', 5, 5],
-  'GB': ['United Kingdom', 1, 2]
+  'SE': ['Sweden', 2, 3],
+  'FR': ['France', 22, 31]
 }
--- people/manfred_h/user.py
+++ people/manfred_h/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': '',
   'countries': 8,
   'markers': 739,
-  'photos': 1395
+  'photos': 1396
 }
