77117b27546380e9b48eb86101d1b9d0a94569de
The Map Group <admin@the-map-group.top>
2025-03-20 03:08:28 -0300

    [auto] Updated map for member 'Mani_H.'

--- people/manfred_h/countries.py
+++ people/manfred_h/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'NL': ['Netherlands', 39, 55],
-  'GB': ['United Kingdom', 1, 2],
   'DK': ['Denmark', 33, 39],
-  'DE': ['Germany', 638, 1262],
-  'BE': ['Belgium', 5, 5],
-  'IT': ['Italy', 1, 1],
+  'GB': ['United Kingdom', 1, 2],
   'SE': ['Sweden', 2, 3],
-  'FR': ['France', 22, 31]
+  'IT': ['Italy', 1, 1],
+  'DE': ['Germany', 640, 1264],
+  'FR': ['France', 22, 31],
+  'BE': ['Belgium', 5, 5],
+  'NL': ['Netherlands', 39, 55]
 }
--- people/manfred_h/user.py
+++ people/manfred_h/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/manfred_h/',
   'location': '',
   'countries': 8,
-  'markers': 741,
-  'photos': 1398
+  'markers': 743,
+  'photos': 1400
 }
