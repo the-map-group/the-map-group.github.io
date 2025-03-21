5a6abc98bae5d5aa591ee96ca2ef0aefc958becf
The Map Group <admin@the-map-group.top>
2025-03-21 03:07:53 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/volvob12b/',
   'location': 'Christchurch, New Zealand',
   'countries': 11,
-  'markers': 3666,
-  'photos': 4886
+  'markers': 3663,
+  'photos': 4883
 }
