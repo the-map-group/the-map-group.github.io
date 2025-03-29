5ab1cc083c5d13628fe2c453e2b6c0758316c6f6
The Map Group <admin@the-map-group.top>
2025-03-29 03:42:57 -0300

    [auto] Updated map for member 'Ian & Marg'

--- people/16938270@N00/user.py
+++ people/16938270@N00/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/16938270@N00/',
   'location': 'Imbil, Australia',
   'countries': 35,
-  'markers': 955,
-  'photos': 1010
+  'markers': 957,
+  'photos': 1012
 }
