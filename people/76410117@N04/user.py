1c435042d00eb04b134ff66f31db5e17d6eeec88
The Map Group <admin@the-map-group.top>
2025-03-31 03:43:35 -0300

    [auto] Updated map for member 'yves62160'

--- people/76410117@N04/user.py
+++ people/76410117@N04/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/76410117@N04/',
   'location': '',
   'countries': 6,
-  'markers': 1913,
-  'photos': 3515
+  'markers': 1914,
+  'photos': 3517
 }
