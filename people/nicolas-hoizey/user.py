a5bb4e503ae0b6a1099453858c9c63ee559b8bc5
The Map Group <admin@the-map-group.top>
2025-03-31 03:27:16 -0300

    [auto] Updated map for member 'Nicolas Hoizey'

--- people/nicolas-hoizey/user.py
+++ people/nicolas-hoizey/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/nicolas-hoizey/',
   'location': 'Paris, France',
   'countries': 19,
-  'markers': 389,
-  'photos': 439
+  'markers': 390,
+  'photos': 440
 }
