2086d20e02889105986b3ce3829bfd4f837439f6
The Map Group <admin@the-map-group.top>
2025-04-01 03:29:46 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'IS': ['Iceland', 66, 101],
-  'IT': ['Italy', 34, 51],
-  'AT': ['Austria', 6, 10],
+  'CN': ['China', 72, 96],
+  'US': ['United States', 1724, 3364],
   'DE': ['Germany', 2, 3],
+  'EE': ['Estonia', 1, 1],
+  'AT': ['Austria', 6, 10],
   'GL': ['Greenland', 5, 5],
-  'US': ['United States', 1723, 3362],
-  'UA': ['Ukraine', 1, 1],
   'CA': ['Canada', 3, 3],
   'GB': ['United Kingdom', 17, 34],
-  'CN': ['China', 72, 96],
   'JP': ['Japan', 133, 155],
-  'EE': ['Estonia', 1, 1],
-  'NO': ['Norway', 1, 1],
   'CH': ['Switzerland', 226, 390],
-  'LI': ['Liechtenstein', 4, 5]
+  'UA': ['Ukraine', 1, 1],
+  'LI': ['Liechtenstein', 4, 5],
+  'IT': ['Italy', 34, 51],
+  'NO': ['Norway', 1, 1],
+  'IS': ['Iceland', 66, 101]
 }
--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/peterthoeny/',
   'location': 'Cupertino, USA',
   'countries': 15,
-  'markers': 2295,
-  'photos': 4219
+  'markers': 2296,
+  'photos': 4221
 }
