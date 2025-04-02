de86b8023931a5592ab71c8d0587d86698bd6ac1
The Map Group <admin@the-map-group.top>
2025-04-02 03:02:56 -0300

    [auto] Updated map for member 'philippeguillot21'

--- people/130828646@N02/user.py
+++ people/130828646@N02/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/130828646@N02/',
   'location': 'Entre-Deux, Réunion',
   'countries': 36,
-  'markers': 16038,
-  'photos': 41252
+  'markers': 16041,
+  'photos': 41262
 }
