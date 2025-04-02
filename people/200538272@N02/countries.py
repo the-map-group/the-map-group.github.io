6eb8863f590ebcb006e8a56bcf14b409a8e26db1
The Map Group <admin@the-map-group.top>
2025-04-02 03:48:10 -0300

    [auto] Updated map for member 'haniarodak'

---
+++ people/200538272@N02/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'PL': ['Poland', 1, 1],
+  'IT': ['Italy', 2, 2]
+}
---
+++ people/200538272@N02/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '200538272@N02',
+  'alias': '200538272@N02',
+  'name': 'Hanna Rodak',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/200538272@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/200538272@N02/',
+  'location': '',
+  'countries': 2,
+  'markers': 3,
+  'photos': 3
+}
