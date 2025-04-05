4a42d211fb263fdbd899007c32cba99884a70d73
The Map Group <admin@the-map-group.top>
2025-04-05 03:27:40 -0300

    [auto] Updated map for member 'sarahstierch'

--- people/sarahvain/countries.py
+++ people/sarahvain/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'US': ['United States', 12656, 17028],
-  'CN': ['China', 17, 77],
-  'NL': ['Netherlands', 8, 8],
-  'AT': ['Austria', 18, 28],
-  'HU': ['Hungary', 18, 39],
-  'IL': ['Israel', 9, 31],
-  'DE': ['Germany', 25, 49],
   'JP': ['Japan', 28, 46],
-  'CL': ['Chile', 37, 119],
-  'FR': ['France', 50, 103],
-  'GB': ['United Kingdom', 165, 439],
-  'BE': ['Belgium', 14, 93],
+  'CN': ['China', 17, 77],
   'CZ': ['Czech Republic', 12, 17],
-  'FI': ['Finland', 38, 83]
+  'FI': ['Finland', 38, 83],
+  'IL': ['Israel', 9, 31],
+  'HU': ['Hungary', 18, 39],
+  'GB': ['United Kingdom', 165, 439],
+  'CL': ['Chile', 37, 119],
+  'US': ['United States', 12660, 17035],
+  'AT': ['Austria', 18, 28],
+  'NL': ['Netherlands', 8, 8],
+  'BE': ['Belgium', 14, 93],
+  'DE': ['Germany', 25, 49],
+  'FR': ['France', 50, 103]
 }
--- people/sarahvain/user.py
+++ people/sarahvain/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/sarahvain/',
   'location': 'Sonoma, CA, USA',
   'countries': 14,
-  'markers': 13096,
-  'photos': 18167
+  'markers': 13100,
+  'photos': 18174
 }
