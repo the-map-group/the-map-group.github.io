c957b4ff384e2dbebdd0b441295a965c46de93cf
The Map Group <admin@the-map-group.top>
2025-04-09 03:37:00 -0300

    [auto] Updated map for member 'Kevin Povenz'

--- people/kevinpovenz/countries.py
+++ people/kevinpovenz/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'SK': ['Slovakia', 3, 6],
-  'US': ['United States', 849, 2916],
-  'PR': ['Puerto Rico', 2, 2],
+  'US': ['United States', 849, 2917],
   'GP': ['Guadeloupe', 1, 2],
+  'SX': ['Sint Maarten', 1, 1],
   'BB': ['Barbados', 1, 1],
-  'LC': ['St Lucia', 2, 2],
-  'SX': ['Sint Maarten', 1, 1]
+  'PR': ['Puerto Rico', 2, 2],
+  'LC': ['St Lucia', 2, 2]
 }
--- people/kevinpovenz/user.py
+++ people/kevinpovenz/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Jenison Michigan, United States',
   'countries': 7,
   'markers': 861,
-  'photos': 2932
+  'photos': 2933
 }
