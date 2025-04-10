ba4fca1bde89a8088a591043f4d7a9a3384b92cb
The Map Group <admin@the-map-group.top>
2025-04-10 03:04:17 -0300

    [auto] Updated map for member 'pascalvu1'

--- people/quanito/user.py
+++ people/quanito/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/quanito/',
   'location': 'Nogent sur Marne, FRANCE',
   'countries': 53,
-  'markers': 5386,
-  'photos': 31693
+  'markers': 5409,
+  'photos': 31726
 }
