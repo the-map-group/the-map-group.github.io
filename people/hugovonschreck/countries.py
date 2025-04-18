f36c7258600c5959f436742e15eb7796406888e9
The Map Group <admin@the-map-group.top>
2025-04-18 03:12:34 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/countries.py
+++ people/hugovonschreck/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'FR': ['France', 91, 202],
-  'CH': ['Switzerland', 18, 43],
-  'SM': ['San Marino', 2, 2],
-  'PL': ['Poland', 1, 3],
   'NL': ['Netherlands', 40, 70],
-  'DE': ['Germany', 1282, 5715],
-  'LU': ['Luxembourg', 6, 15],
-  'IT': ['Italy', 99, 273],
   'DK': ['Denmark', 8, 9],
+  'DE': ['Germany', 1282, 5715],
   'HR': ['Croatia', 7, 17],
-  'AT': ['Austria', 16, 27]
+  'LU': ['Luxembourg', 6, 15],
+  'PL': ['Poland', 1, 3],
+  'AT': ['Austria', 16, 27],
+  'CH': ['Switzerland', 18, 43],
+  'FR': ['France', 91, 202],
+  'IT': ['Italy', 99, 274],
+  'SM': ['San Marino', 2, 2]
 }
--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Linsengericht, Germany',
   'countries': 11,
   'markers': 1570,
-  'photos': 6376
+  'photos': 6377
 }
