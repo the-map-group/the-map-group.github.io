d0a266133704cc124d6a43761b61da28a5db9a80
The Map Group <admin@the-map-group.top>
2025-04-18 03:21:16 -0300

    [auto] Updated map for member 'Luigibalza'

--- people/123864860@N07/user.py
+++ people/123864860@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/123864860@N07/',
   'location': 'BRIVIO, Italia',
   'countries': 50,
-  'markers': 6544,
-  'photos': 9959
+  'markers': 6545,
+  'photos': 9963
 }
