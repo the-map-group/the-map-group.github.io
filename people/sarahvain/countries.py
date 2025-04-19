7e48c6155ff1249877d12ff586bd6274ce913df7
The Map Group <admin@the-map-group.top>
2025-04-19 03:27:50 -0300

    [auto] Updated map for member 'sarahstierch'

--- people/sarahvain/countries.py
+++ people/sarahvain/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'IL': ['Israel', 9, 31],
-  'HU': ['Hungary', 18, 39],
-  'JP': ['Japan', 28, 46],
-  'FR': ['France', 50, 103],
-  'AT': ['Austria', 18, 28],
-  'FI': ['Finland', 38, 83],
-  'US': ['United States', 12670, 17045],
   'DE': ['Germany', 25, 49],
   'CZ': ['Czech Republic', 12, 17],
-  'BE': ['Belgium', 14, 93],
-  'NL': ['Netherlands', 8, 8],
-  'CN': ['China', 17, 77],
+  'US': ['United States', 12671, 17046],
+  'IL': ['Israel', 9, 31],
   'GB': ['United Kingdom', 165, 439],
-  'CL': ['Chile', 37, 119]
+  'JP': ['Japan', 28, 46],
+  'AT': ['Austria', 18, 28],
+  'NL': ['Netherlands', 8, 8],
+  'FR': ['France', 50, 103],
+  'CL': ['Chile', 37, 119],
+  'FI': ['Finland', 38, 83],
+  'BE': ['Belgium', 14, 93],
+  'CN': ['China', 17, 77],
+  'HU': ['Hungary', 18, 39]
 }
