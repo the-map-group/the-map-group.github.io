b63948520d08cd6ed8edfd49e92e62681b737ecc
The Map Group <admin@the-map-group.top>
2025-04-20 03:27:35 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'US': ['United States', 1729, 3373],
+  'GB': ['United Kingdom', 17, 34],
   'UA': ['Ukraine', 1, 1],
   'NO': ['Norway', 1, 1],
-  'DE': ['Germany', 2, 3],
-  'EE': ['Estonia', 1, 1],
-  'IT': ['Italy', 34, 51],
-  'GB': ['United Kingdom', 17, 34],
   'CA': ['Canada', 3, 3],
+  'US': ['United States', 1729, 3374],
+  'AT': ['Austria', 6, 10],
+  'EE': ['Estonia', 1, 1],
+  'DE': ['Germany', 2, 3],
+  'JP': ['Japan', 141, 164],
+  'GL': ['Greenland', 5, 5],
+  'IT': ['Italy', 34, 51],
   'CN': ['China', 72, 96],
-  'LI': ['Liechtenstein', 4, 5],
   'CH': ['Switzerland', 226, 390],
   'IS': ['Iceland', 66, 101],
-  'AT': ['Austria', 6, 10],
-  'JP': ['Japan', 141, 163],
-  'GL': ['Greenland', 5, 5]
+  'LI': ['Liechtenstein', 4, 5]
 }
--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Cupertino, USA',
   'countries': 15,
   'markers': 2309,
-  'photos': 4238
+  'photos': 4240
 }
