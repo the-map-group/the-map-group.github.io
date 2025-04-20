9519a72134da089898201e07fd5eb94719ad14f7
The Map Group <admin@the-map-group.top>
2025-04-20 03:27:16 -0300

    [auto] Updated map for member 'diffendale'

--- people/dandiffendale/user.py
+++ people/dandiffendale/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/dandiffendale/',
   'location': 'Pisa, Italy',
   'countries': 16,
-  'markers': 2126,
-  'photos': 15557
+  'markers': 2128,
+  'photos': 15568
 }
