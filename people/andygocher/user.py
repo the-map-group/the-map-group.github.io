618710217122b27d221fb6a031afb48493a7be20
The Map Group <admin@the-map-group.top>
2025-04-21 03:02:19 -0300

    [auto] Updated map for member 'Andy.Gocher'

--- people/andygocher/user.py
+++ people/andygocher/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/andygocher/',
   'location': 'South Wales, UK',
   'countries': 51,
-  'markers': 633,
-  'photos': 1545
+  'markers': 635,
+  'photos': 1548
 }
