4697c0667b1e3c4b040c140739e5a37697c9293d
The Map Group <admin@the-map-group.top>
2025-04-22 03:27:55 -0300

    [auto] Updated map for member 'ceri1965'

--- people/10096006@N07/countries.py
+++ people/10096006@N07/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'GB': ['United Kingdom', 124, 155]
+  'GB': ['United Kingdom', 125, 156]
 }
--- people/10096006@N07/user.py
+++ people/10096006@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/10096006@N07/',
   'location': '',
   'countries': 1,
-  'markers': 124,
-  'photos': 155
+  'markers': 125,
+  'photos': 156
 }
