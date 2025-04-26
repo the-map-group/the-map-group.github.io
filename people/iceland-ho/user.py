d76da720eceda8db437d6664eaa06403cf6bb16e
The Map Group <admin@the-map-group.top>
2025-04-26 03:07:01 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/iceland-ho/',
   'location': 'Reykjavík, Iceland',
   'countries': 17,
-  'markers': 3129,
-  'photos': 3132
+  'markers': 3130,
+  'photos': 3133
 }
