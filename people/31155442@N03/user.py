5df2c70a1b315c999bfab4653e887245588ad297
The Map Group <admin@the-map-group.top>
2025-04-26 03:02:22 -0300

    [auto] Updated map for member 'Joseph Hollick'

--- people/31155442@N03/user.py
+++ people/31155442@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/31155442@N03/',
   'location': 'Hamilton, Canada',
   'countries': 61,
-  'markers': 13596,
-  'photos': 20093
+  'markers': 13604,
+  'photos': 20109
 }
