2d0da22d5986035b65ce90a9578086958d690bb2
The Map Group <admin@the-map-group.top>
2025-05-02 03:36:26 -0300

    [auto] Updated map for member 'Edouard55'

--- people/19239093@N03/user.py
+++ people/19239093@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/19239093@N03/',
   'location': 'Strasbourg, France',
   'countries': 3,
-  'markers': 8198,
-  'photos': 10771
+  'markers': 8201,
+  'photos': 10776
 }
