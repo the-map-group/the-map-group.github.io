1ffa363218b2ba4c454a39f592c1e8e08399c69b
The Map Group <admin@the-map-group.top>
2025-05-04 03:23:26 -0300

    [auto] Updated map for member 'Steve O'Day'

--- people/22875671@N04/countries.py
+++ people/22875671@N04/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'IT': ['Italy', 3, 3],
   'FR': ['France', 14, 15],
   'GB': ['United Kingdom', 1, 1],
+  'IT': ['Italy', 3, 3],
   'IE': ['Ireland', 4, 4],
-  'US': ['United States', 263, 288]
+  'US': ['United States', 264, 289]
 }
--- people/22875671@N04/user.py
+++ people/22875671@N04/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/22875671@N04/',
   'location': '',
   'countries': 5,
-  'markers': 285,
-  'photos': 311
+  'markers': 286,
+  'photos': 312
 }
