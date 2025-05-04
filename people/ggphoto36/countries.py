37f1234b9256903e1803c988ee41d98416b7a926
The Map Group <admin@the-map-group.top>
2025-05-04 03:19:49 -0300

    [auto] Updated map for member 'The Gidinski'

--- people/ggphoto36/countries.py
+++ people/ggphoto36/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'IT': ['Italy', 4, 4],
-  'BS': ['Bahamas', 1, 1],
   'CR': ['Costa Rica', 1, 1],
-  'CA': ['Canada', 4, 4],
-  'US': ['United States', 956, 1002]
+  'IT': ['Italy', 4, 4],
+  'US': ['United States', 957, 1003],
+  'BS': ['Bahamas', 1, 1],
+  'CA': ['Canada', 4, 4]
 }
--- people/ggphoto36/user.py
+++ people/ggphoto36/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/ggphoto36/',
   'location': 'Monmouth, Oregon, United States',
   'countries': 5,
-  'markers': 966,
-  'photos': 1012
+  'markers': 967,
+  'photos': 1013
 }
