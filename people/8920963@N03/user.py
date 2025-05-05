d1f28e6e067a8e82885f22fc8b22eb23e97e9c78
The Map Group <admin@the-map-group.top>
2025-05-05 03:01:21 -0300

    [auto] Updated map for member 'Steenjep'

--- people/8920963@N03/user.py
+++ people/8920963@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/8920963@N03/',
   'location': 'Herning, Denmark',
   'countries': 30,
-  'markers': 12184,
-  'photos': 17530
+  'markers': 12187,
+  'photos': 17534
 }
