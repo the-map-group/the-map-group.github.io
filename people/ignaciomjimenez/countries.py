1746d132a30fdcb853a6f8767265ffe589bf62e1
The Map Group <admin@the-map-group.top>
2025-05-07 03:17:14 -0300

    [auto] Updated map for member 'Ignacio M. Jiménez'

--- people/ignaciomjimenez/countries.py
+++ people/ignaciomjimenez/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'FR': ['France', 12, 14],
   'BE': ['Belgium', 7, 7],
-  'GB': ['United Kingdom', 25, 25],
   'AT': ['Austria', 1, 1],
+  'GB': ['United Kingdom', 25, 25],
+  'FR': ['France', 12, 14],
   'PT': ['Portugal', 20, 20],
-  'ES': ['Spain', 1074, 1433]
+  'ES': ['Spain', 1075, 1434]
 }
--- people/ignaciomjimenez/user.py
+++ people/ignaciomjimenez/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/ignaciomjimenez/',
   'location': 'Úbeda, España',
   'countries': 6,
-  'markers': 1139,
-  'photos': 1500
+  'markers': 1140,
+  'photos': 1501
 }
