ad4107f646ec997c08d4f8ee721e146261b99508
The Map Group <admin@the-map-group.top>
2025-05-08 03:27:30 -0300

    [auto] Updated map for member 'Al Case'

--- people/al_case/user.py
+++ people/al_case/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/al_case/',
   'location': 'Ashland, Oregon, USA',
   'countries': 19,
-  'markers': 743,
-  'photos': 858
+  'markers': 744,
+  'photos': 859
 }
