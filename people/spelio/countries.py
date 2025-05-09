a64d7c4329a1bc275948b1bf02275fcfaaa2f87f
The Map Group <admin@the-map-group.top>
2025-05-09 03:43:22 -0300

    [auto] Updated map for member 'spelio'

--- people/spelio/countries.py
+++ people/spelio/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'US': ['United States', 90, 90],
-  'FR': ['France', 8, 8],
+  'GB': ['United Kingdom', 9, 9],
   'CH': ['Switzerland', 2, 3],
   'NZ': ['New Zealand', 1, 1],
-  'AU': ['Australia', 3697, 4878],
-  'GB': ['United Kingdom', 9, 9],
-  'CA': ['Canada', 4, 4]
+  'AU': ['Australia', 3700, 4882],
+  'CA': ['Canada', 4, 4],
+  'FR': ['France', 8, 8]
 }
--- people/spelio/user.py
+++ people/spelio/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/spelio/',
   'location': '',
   'countries': 7,
-  'markers': 3811,
-  'photos': 4993
+  'markers': 3814,
+  'photos': 4997
 }
