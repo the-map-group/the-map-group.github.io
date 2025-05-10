84e467bd442df37bd05bfa35f0cc8575a01b7d78
The Map Group <admin@the-map-group.top>
2025-05-10 03:36:58 -0300

    [auto] Updated map for member 'spelio'

--- people/spelio/countries.py
+++ people/spelio/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'US': ['United States', 90, 90],
-  'GB': ['United Kingdom', 9, 9],
-  'CH': ['Switzerland', 2, 3],
   'NZ': ['New Zealand', 1, 1],
-  'AU': ['Australia', 3700, 4882],
-  'CA': ['Canada', 4, 4],
-  'FR': ['France', 8, 8]
+  'AU': ['Australia', 3726, 4948],
+  'GB': ['United Kingdom', 9, 9],
+  'US': ['United States', 90, 90],
+  'CH': ['Switzerland', 2, 3],
+  'FR': ['France', 13, 14],
+  'CA': ['Canada', 4, 4]
 }
