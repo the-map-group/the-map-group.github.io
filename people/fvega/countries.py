8271a11dba20b93e60507178d64046297e097d27
The Map Group <admin@the-map-group.top>
2025-05-12 03:20:25 -0300

    [auto] Updated map for member 'Fernando Sa Rapita'

--- people/fvega/countries.py
+++ people/fvega/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'IE': ['Ireland', 208, 255],
+  'ES': ['Spain', 1437, 2845],
   'FI': ['Finland', 25, 25],
-  'NO': ['Norway', 185, 233],
-  'DE': ['Germany', 23, 24],
-  'NL': ['Netherlands', 19, 20],
   'SE': ['Sweden', 43, 59],
-  'ES': ['Spain', 1437, 2844],
-  'IS': ['Iceland', 822, 1071],
+  'IE': ['Ireland', 208, 255],
   'CH': ['Switzerland', 7, 7],
-  'GB': ['United Kingdom', 777, 917]
+  'NO': ['Norway', 185, 233],
+  'NL': ['Netherlands', 19, 20],
+  'GB': ['United Kingdom', 777, 917],
+  'DE': ['Germany', 23, 24],
+  'IS': ['Iceland', 822, 1071]
 }
--- people/fvega/user.py
+++ people/fvega/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Sa Rapita, Spain',
   'countries': 10,
   'markers': 3546,
-  'photos': 5455
+  'photos': 5456
 }
