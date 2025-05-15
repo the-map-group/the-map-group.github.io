ec99a9def081ffe7c995f3e268b97cd78cd03a02
The Map Group <admin@the-map-group.top>
2025-05-15 03:12:37 -0300

    [auto] Updated map for member 'Edouard55'

--- people/19239093@N03/countries.py
+++ people/19239093@N03/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'FR': ['France', 7076, 9263],
   'CH': ['Switzerland', 79, 106],
-  'DE': ['Germany', 1066, 1435]
+  'DE': ['Germany', 1068, 1438],
+  'FR': ['France', 7076, 9263]
 }
--- people/19239093@N03/user.py
+++ people/19239093@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/19239093@N03/',
   'location': 'Strasbourg, France',
   'countries': 3,
-  'markers': 8221,
-  'photos': 10804
+  'markers': 8223,
+  'photos': 10807
 }
