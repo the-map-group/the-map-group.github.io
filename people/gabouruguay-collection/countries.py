9d234efa1e72c3f1265e2d9403e0f1fcd59df46e
The Map Group <admin@the-map-group.top>
2025-05-23 03:25:18 -0300

    [auto] Updated map for member 'Gabriel Paladino Ibáñez'

--- people/gabouruguay-collection/countries.py
+++ people/gabouruguay-collection/countries.py
@@ -2,7 +2,7 @@ countries_dict = {
   'UY': ['Uruguay', 594, 2074],
   'MX': ['Mexico', 26, 295],
   'ES': ['Spain', 38, 93],
-  'KR': ['South Korea', 6, 6],
+  'KR': ['South Korea', 7, 7],
   'PE': ['Peru', 40, 86],
   'JP': ['Japan', 41, 95],
   'ZA': ['South Africa', 59, 139],
--- people/gabouruguay-collection/user.py
+++ people/gabouruguay-collection/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/gabouruguay-collection/',
   'location': '',
   'countries': 10,
-  'markers': 869,
-  'photos': 2956
+  'markers': 870,
+  'photos': 2957
 }
