49fd632e81e648864f1b358c5f2cf5956ad7375b
The Map Group <admin@the-map-group.top>
2025-05-26 03:46:15 -0300

    [auto] Updated map for member 'kimbar/Thanks for 6.7 million views!'

--- people/kimbar/countries.py
+++ people/kimbar/countries.py
@@ -27,7 +27,7 @@ countries_dict = {
   'CN': ['China', 35, 94],
   'TW': ['Taiwan', 13, 24],
   'VN': ['Vietnam', 13, 25],
-  'US': ['United States', 3404, 4179],
+  'US': ['United States', 3406, 4181],
   'PL': ['Poland', 3, 12],
   'OM': ['Oman', 4, 11],
   'MG': ['Madagascar', 9, 13],
--- people/kimbar/user.py
+++ people/kimbar/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/kimbar/',
   'location': 'Berkeley, CA, U.S.',
   'countries': 68,
-  'markers': 6463,
-  'photos': 8832
+  'markers': 6465,
+  'photos': 8834
 }
