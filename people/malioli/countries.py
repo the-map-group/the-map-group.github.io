e6d373ac706dec64288d23b5d13ea499d86a4c3f
The Map Group <admin@the-map-group.top>
2025-05-27 03:11:21 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/countries.py
+++ people/malioli/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'HR': ['Croatia', 2374, 5087],
+  'HR': ['Croatia', 2374, 5088],
   'AT': ['Austria', 5, 5],
   'SI': ['Slovenia', 3, 16],
   'BA': ['Bosnia and Herzegovina', 1, 1],
--- people/malioli/user.py
+++ people/malioli/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Karlovac, Hrvatska',
   'countries': 5,
   'markers': 2399,
-  'photos': 5153
+  'photos': 5154
 }
