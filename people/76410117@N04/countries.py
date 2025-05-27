c4b4ff94388d1551883ed3617748a41385d145ca
The Map Group <admin@the-map-group.top>
2025-05-27 09:40:18 -0300

    [auto] Updated map for member 'yves62160'

--- people/76410117@N04/countries.py
+++ people/76410117@N04/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'GR': ['Greece', 36, 40],
   'FR': ['France', 1755, 3199],
-  'BE': ['Belgium', 136, 259],
+  'BE': ['Belgium', 137, 260],
   'AT': ['Austria', 17, 24],
   'PT': ['Portugal', 9, 29],
   'LU': ['Luxembourg', 4, 53]
--- people/76410117@N04/user.py
+++ people/76410117@N04/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/76410117@N04/',
   'location': '',
   'countries': 6,
-  'markers': 1957,
-  'photos': 3604
+  'markers': 1958,
+  'photos': 3605
 }
