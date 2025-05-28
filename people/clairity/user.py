d0617c0f4e97af9430c29bd152fdc9bdc913c862
The Map Group <admin@the-map-group.top>
2025-05-28 14:11:49 -0300

    [auto] Updated map for member 'Sharon Mollerus'

--- people/clairity/user.py
+++ people/clairity/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/clairity/',
   'location': 'Minneapolis, United States',
   'countries': 7,
-  'markers': 3602,
-  'photos': 14186
+  'markers': 3603,
+  'photos': 14187
 }
