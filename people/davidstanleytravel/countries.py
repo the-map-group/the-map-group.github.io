6f8c0474d0abb09d1984fbb28cc1bff730920d8b
The Map Group <admin@the-map-group.top>
2025-05-29 03:30:40 -0300

    [auto] Updated map for member 'D-Stanley'

--- people/davidstanleytravel/countries.py
+++ people/davidstanleytravel/countries.py
@@ -15,7 +15,7 @@ countries_dict = {
   'IT': ['Italy', 153, 187],
   'EC': ['Ecuador', 81, 110],
   'CC': ['Cocos (Keeling) Islands', 45, 46],
-  'ES': ['Spain', 49, 53],
+  'ES': ['Spain', 52, 56],
   'AQ': ['Antarctica', 275, 347],
   'FJ': ['Fiji', 1, 1],
   'LR': ['Liberia', 10, 10],
--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
   'location': 'Nanaimo, Canada',
   'countries': 123,
-  'markers': 7864,
-  'photos': 9137
+  'markers': 7867,
+  'photos': 9140
 }
