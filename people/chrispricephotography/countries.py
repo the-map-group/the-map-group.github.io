d0a2a6fbf33836d62f15b5d6c913c4b4cf4cc172
The Map Group <admin@the-map-group.top>
2025-05-29 03:00:52 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -3,7 +3,7 @@ countries_dict = {
   'GB': ['United Kingdom', 26, 28],
   'NZ': ['New Zealand', 9, 9],
   'PA': ['Panama', 1, 2],
-  'US': ['United States', 753, 1059],
+  'US': ['United States', 753, 1060],
   'RU': ['Russia', 1, 1],
   'CA': ['Canada', 90, 158],
   'AU': ['Australia', 11, 11],
--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Blacksburg, VA, USA',
   'countries': 10,
   'markers': 1108,
-  'photos': 1554
+  'photos': 1555
 }
