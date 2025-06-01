05833f3d8290c83820d7115ba034c3d40aaad236
The Map Group <admin@the-map-group.top>
2025-06-01 03:06:55 -0300

    [auto] Updated map for member 'karen_ds1101'

--- people/karen_ds1101/countries.py
+++ people/karen_ds1101/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'NO': ['Norway', 4, 7],
   'AU': ['Australia', 911, 1036],
-  'GB': ['United Kingdom', 19, 20],
   'BN': ['Brunei', 2, 3],
-  'JP': ['Japan', 10, 10],
+  'SG': ['Singapore', 66, 78],
+  'MY': ['Malaysia', 8, 11],
+  'CN': ['China', 79, 125],
+  'GB': ['United Kingdom', 18, 19],
   'DE': ['Germany', 1, 1],
+  'IT': ['Italy', 5, 6],
+  'JP': ['Japan', 10, 10],
+  'IE': ['Ireland', 3, 3],
   'US': ['United States', 11, 19],
   'NZ': ['New Zealand', 10, 10],
-  'IT': ['Italy', 5, 6],
-  'CN': ['China', 79, 125],
   'CL': ['Chile', 5, 9],
-  'MY': ['Malaysia', 8, 11],
   'PE': ['Peru', 11, 16],
-  'SG': ['Singapore', 66, 78],
-  'IE': ['Ireland', 3, 3]
+  'NO': ['Norway', 4, 7]
 }
--- people/karen_ds1101/user.py
+++ people/karen_ds1101/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/karen_ds1101/',
   'location': 'On the way to Cairns, Australia',
   'countries': 15,
-  'markers': 1145,
-  'photos': 1354
+  'markers': 1144,
+  'photos': 1353
 }
