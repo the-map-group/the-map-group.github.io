033c5cb43d1362482bc02ab1379a4a8ffb0980f3
The Map Group <admin@the-map-group.top>
2025-06-01 03:13:02 -0300

    [auto] Updated map for member 'Harald Schulz'

--- people/121195437@N06/user.py
+++ people/121195437@N06/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/121195437@N06/',
   'location': '',
   'countries': 25,
-  'markers': 4346,
-  'photos': 6411
+  'markers': 4347,
+  'photos': 6413
 }
