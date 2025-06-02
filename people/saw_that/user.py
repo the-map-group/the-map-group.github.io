b10d2f6fc2ffeaac6d9c308f7a4d3805128df675
The Map Group <admin@the-map-group.top>
2025-06-02 03:29:32 -0300

    [auto] Updated map for member 'I saw_that'

--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/saw_that/',
   'location': 'Iqaluit, Nunavut, Canada',
   'countries': 18,
-  'markers': 1334,
-  'photos': 2630
+  'markers': 1338,
+  'photos': 2639
 }
