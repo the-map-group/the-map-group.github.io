84efc9f2f812d659062a6b6cb8ebbe51112fd60c
The Map Group <admin@the-map-group.top>
2025-06-02 03:21:40 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -23,6 +23,6 @@ countries_dict = {
   'AT': ['Austria', 8, 10],
   'MA': ['Morocco', 96, 135],
   'EG': ['Egypt', 119, 157],
-  'BR': ['Brazil', 2499, 3532],
+  'BR': ['Brazil', 2500, 3534],
   'NL': ['Netherlands', 10, 17]
 }
