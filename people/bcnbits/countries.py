f091365f727eb3498c0b2f0c1ba1b83523948d07
The Map Group <admin@the-map-group.top>
2025-06-06 03:58:23 -0300

    [auto] Updated map for member 'MorBCN'

---
+++ people/bcnbits/countries.py
@@ -0,0 +1,16 @@
+countries_dict = {
+  'IT': ['Italy', 360, 431],
+  'ES': ['Spain', 290, 339],
+  'FR': ['France', 111, 130],
+  'NL': ['Netherlands', 58, 68],
+  'DE': ['Germany', 105, 112],
+  'AT': ['Austria', 11, 11],
+  'BE': ['Belgium', 26, 26],
+  'CH': ['Switzerland', 57, 66],
+  'NO': ['Norway', 159, 177],
+  'DK': ['Denmark', 78, 87],
+  'IS': ['Iceland', 282, 319],
+  'HR': ['Croatia', 8, 8],
+  'CZ': ['Czech Republic', 4, 4],
+  'GB': ['United Kingdom', 14, 15]
+}
---
+++ people/bcnbits/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '32744903@N00',
+  'alias': 'bcnbits',
+  'name': 'MorBCN',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/32744903@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/bcnbits/',
+  'location': 'Barcelona, Catalonia',
+  'countries': 14,
+  'markers': 1563,
+  'photos': 1793
+}
