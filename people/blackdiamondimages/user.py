0174e882a6648881bc0c081e6319ff4a87933bb0
The Map Group <admin@the-map-group.top>
2025-06-11 07:32:30 -0300

    [auto] Updated map for member 'Black Diamond Images'

--- people/blackdiamondimages/user.py
+++ people/blackdiamondimages/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/blackdiamondimages/',
   'location': 'Mid North Coast, NSW, Australia',
   'countries': 36,
-  'markers': 17128,
-  'photos': 22023
+  'markers': 17137,
+  'photos': 22033
 }
