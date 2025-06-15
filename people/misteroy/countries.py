a6c65d62b26b5f2f0aba321bfc06f8e47a3fb73d
The Map Group <admin@the-map-group.top>
2025-06-15 05:04:54 -0300

    [auto] Updated map for member 'Mister Oy'

--- people/misteroy/countries.py
+++ people/misteroy/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'GB': ['United Kingdom', 4255, 6088],
+  'GB': ['United Kingdom', 4256, 6089],
   'PT': ['Portugal', 4, 5]
 }
--- people/misteroy/user.py
+++ people/misteroy/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/misteroy/',
   'location': '',
   'countries': 2,
-  'markers': 4260,
-  'photos': 6094
+  'markers': 4261,
+  'photos': 6095
 }
