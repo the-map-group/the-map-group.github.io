e225176478c507d85ec94a9b3fb887d008759edd
The Map Group <admin@the-map-group.top>
2025-06-17 04:10:35 -0300

    [auto] Updated map for member 'fam_nordstrom'

--- people/fam_nordstrom/countries.py
+++ people/fam_nordstrom/countries.py
@@ -11,7 +11,7 @@ countries_dict = {
   'OM': ['Oman', 2, 2],
   'KE': ['Kenya', 5, 5],
   'CZ': ['Czech Republic', 46, 135],
-  'PE': ['Peru', 505, 1750],
+  'PE': ['Peru', 508, 1755],
   'DK': ['Denmark', 9, 12],
   'GB': ['United Kingdom', 17, 24],
   'SE': ['Sweden', 571, 1173],
