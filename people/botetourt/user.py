59665ffc84385375417ceb5b1f17f1fa143f0ee0
The Map Group <admin@the-map-group.top>
2025-06-18 03:52:36 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/botetourt/',
   'location': 'Near Fincastle, Botetourt County, Va., USA',
   'countries': 11,
-  'markers': 4767,
-  'photos': 8623
+  'markers': 4768,
+  'photos': 8624
 }
