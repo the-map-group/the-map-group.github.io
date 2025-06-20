b23f55b8583bee1b2b9d648f95e45846df58b0a0
The Map Group <admin@the-map-group.top>
2025-06-20 03:56:03 -0300

    [auto] Updated map for member 'CoronaViking (mountain photography)'

--- people/starcitizen/user.py
+++ people/starcitizen/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/starcitizen/',
   'location': '',
   'countries': 10,
-  'markers': 3578,
-  'photos': 3686
+  'markers': 3579,
+  'photos': 3687
 }
