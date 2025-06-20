88802be8fa76d398a80c38455c8394e7baf62fcd
The Map Group <admin@the-map-group.top>
2025-06-20 03:38:26 -0300

    [auto] Updated map for member 'xd_travel'

--- people/111314495@N05/countries.py
+++ people/111314495@N05/countries.py
@@ -15,7 +15,7 @@ countries_dict = {
   'BB': ['Barbados', 1, 1],
   'JP': ['Japan', 147, 198],
   'CK': ['Cook Islands', 2, 2],
-  'NO': ['Norway', 28, 56],
+  'NO': ['Norway', 34, 62],
   'SE': ['Sweden', 2, 2],
   'PF': ['French Polynesia', 142, 210],
   'HU': ['Hungary', 83, 87],
