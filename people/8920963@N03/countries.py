b34f9adefd54dc078506a1df61abd0b454c3d469
The Map Group <admin@the-map-group.top>
2025-06-21 05:01:23 -0300

    [auto] Updated map for member 'Steenjep'

--- people/8920963@N03/countries.py
+++ people/8920963@N03/countries.py
@@ -2,7 +2,7 @@ countries_dict = {
   'ME': ['Montenegro', 3, 3],
   'BA': ['Bosnia and Herzegovina', 4, 4],
   'PL': ['Poland', 28, 29],
-  'GR': ['Greece', 2242, 3041],
+  'GR': ['Greece', 2246, 3047],
   'FR': ['France', 57, 85],
   'BE': ['Belgium', 1, 1],
   'DK': ['Denmark', 7048, 10732],
--- people/8920963@N03/user.py
+++ people/8920963@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/8920963@N03/',
   'location': 'Herning, Denmark',
   'countries': 30,
-  'markers': 12222,
-  'photos': 17582
+  'markers': 12226,
+  'photos': 17588
 }
