43281fce2d42df69a635d09f04cbacf9b2bb76a4
The Map Group <admin@the-map-group.top>
2025-06-23 03:03:38 -0300

    [auto] Updated map for member 'Brit 70013 fan'

--- people/93936890@N02/countries.py
+++ people/93936890@N02/countries.py
@@ -40,6 +40,6 @@ countries_dict = {
   'SE': ['Sweden', 2, 2],
   'BE': ['Belgium', 2, 3],
   'BZ': ['Belize', 1, 1],
-  'US': ['United States', 250, 343],
+  'US': ['United States', 251, 344],
   'CN': ['China', 15, 18]
 }
