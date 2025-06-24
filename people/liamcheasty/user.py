866d1c9fea991e42d362f67b33cb404fc4dc46be
The Map Group <admin@the-map-group.top>
2025-06-24 03:36:20 -0300

    [auto] Updated map for member 'Liam Cheasty'

--- people/liamcheasty/user.py
+++ people/liamcheasty/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/liamcheasty/',
   'location': 'Waterford, Ireland',
   'countries': 29,
-  'markers': 3522,
-  'photos': 17933
+  'markers': 3525,
+  'photos': 17944
 }
