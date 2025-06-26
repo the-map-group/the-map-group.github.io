f4303550603ebe0b47ffeb1c26c00b19a2ca3577
The Map Group <admin@the-map-group.top>
2025-06-26 03:40:45 -0300

    [auto] Updated map for member 'fam_nordstrom'

--- people/fam_nordstrom/user.py
+++ people/fam_nordstrom/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/fam_nordstrom/',
   'location': 'Sanlúcar de Barrameda, Spain',
   'countries': 25,
-  'markers': 6503,
-  'photos': 19555
+  'markers': 6505,
+  'photos': 19560
 }
