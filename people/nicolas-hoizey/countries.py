64c840a426a6f1a372d4d51c2d37bd1e5649c050
The Map Group <admin@the-map-group.top>
2025-06-27 05:49:57 -0300

    [auto] Updated map for member 'Nicolas Hoizey'

--- people/nicolas-hoizey/countries.py
+++ people/nicolas-hoizey/countries.py
@@ -5,7 +5,7 @@ countries_dict = {
   'VA': ['Vatican City', 2, 2],
   'GB': ['United Kingdom', 15, 15],
   'GR': ['Greece', 40, 41],
-  'FR': ['France', 154, 183],
+  'FR': ['France', 155, 184],
   'IT': ['Italy', 13, 15],
   'ES': ['Spain', 7, 8],
   'KE': ['Kenya', 73, 83],
--- people/nicolas-hoizey/user.py
+++ people/nicolas-hoizey/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/nicolas-hoizey/',
   'location': 'Paris, France',
   'countries': 19,
-  'markers': 437,
-  'photos': 489
+  'markers': 438,
+  'photos': 490
 }
