78a9abd189929467aa1f35aaa7716646f5c2fdca
The Map Group <admin@the-map-group.top>
2025-06-27 05:50:27 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/countries.py
+++ people/diwan/countries.py
@@ -14,7 +14,7 @@ countries_dict = {
   'FI': ['Finland', 73, 75],
   'MC': ['Monaco', 14, 14],
   'LV': ['Latvia', 41, 42],
-  'ES': ['Spain', 413, 453],
+  'ES': ['Spain', 415, 455],
   'PL': ['Poland', 23, 25],
   'DK': ['Denmark', 131, 133],
   'US': ['United States', 241, 247],
