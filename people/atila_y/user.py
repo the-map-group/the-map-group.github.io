d1b011f4ce3049e55d6873dd10167da366266433
The Map Group <admin@the-map-group.top>
2025-06-30 07:11:37 -0300

    [auto] Updated map for member 'Atila Yumusakkaya'

--- people/atila_y/user.py
+++ people/atila_y/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/atila_y/',
   'location': 'Istanbul, Turkey',
   'countries': 22,
-  'markers': 1391,
-  'photos': 1571
+  'markers': 1392,
+  'photos': 1572
 }
