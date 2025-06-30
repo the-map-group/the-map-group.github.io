2f966f70a53045a50f6a2397b0c9774d4cacbc49
The Map Group <admin@the-map-group.top>
2025-06-30 07:20:53 -0300

    [auto] Updated map for member 'Patrice Dardoize'

--- people/192276370@N06/countries.py
+++ people/192276370@N06/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
   'IS': ['Iceland', 28, 31],
-  'FR': ['France', 664, 832]
+  'FR': ['France', 665, 833]
 }
--- people/192276370@N06/user.py
+++ people/192276370@N06/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/192276370@N06/',
   'location': '',
   'countries': 2,
-  'markers': 692,
-  'photos': 863
+  'markers': 693,
+  'photos': 864
 }
