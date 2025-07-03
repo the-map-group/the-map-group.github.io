21912bf1f674239fd18babd35ba26883e4f7502f
The Map Group <admin@the-map-group.top>
2025-07-03 03:49:36 -0300

    [auto] Updated map for member 'fam_nordstrom'

--- people/fam_nordstrom/countries.py
+++ people/fam_nordstrom/countries.py
@@ -11,7 +11,7 @@ countries_dict = {
   'OM': ['Oman', 2, 2],
   'KE': ['Kenya', 5, 5],
   'CZ': ['Czech Republic', 48, 145],
-  'PE': ['Peru', 511, 1760],
+  'PE': ['Peru', 514, 1765],
   'DK': ['Denmark', 9, 12],
   'GB': ['United Kingdom', 17, 24],
   'SE': ['Sweden', 571, 1173],
--- people/fam_nordstrom/user.py
+++ people/fam_nordstrom/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/fam_nordstrom/',
   'location': 'Sanlúcar de Barrameda, Spain',
   'countries': 25,
-  'markers': 6512,
-  'photos': 19580
+  'markers': 6515,
+  'photos': 19585
 }
