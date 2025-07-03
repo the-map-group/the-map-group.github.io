5d69fd55b0c3b02ba9af0306df470b1578855862
The Map Group <admin@the-map-group.top>
2025-07-03 04:17:09 -0300

    [auto] Updated map for member 'kimbar/Thanks for 6.7 million views!'

--- people/kimbar/countries.py
+++ people/kimbar/countries.py
@@ -27,7 +27,7 @@ countries_dict = {
   'CN': ['China', 35, 94],
   'TW': ['Taiwan', 13, 24],
   'VN': ['Vietnam', 13, 25],
-  'US': ['United States', 3452, 4234],
+  'US': ['United States', 3455, 4237],
   'PL': ['Poland', 3, 12],
   'OM': ['Oman', 4, 11],
   'MG': ['Madagascar', 9, 13],
