3a9619e3f6252b0f4573f17152819ce3d8122ce9
The Map Group <admin@the-map-group.top>
2025-07-05 03:51:42 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/user.py
+++ people/39904644@N05/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '39904644@N05',
   'alias': '39904644@N05',
   'name': 'Martin Thum',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/39904644@N05_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/39904644@N05/',
   'location': 'digitale Suppe, Deutschland',
   'countries': 18,
-  'markers': 1770,
-  'photos': 2062
+  'markers': 1771,
+  'photos': 2063
 }
