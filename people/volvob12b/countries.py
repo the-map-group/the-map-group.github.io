f0a81364525d6aa202172c022e6fd67e55108b79
The Map Group <admin@the-map-group.top>
2025-07-05 03:38:30 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/countries.py
+++ people/volvob12b/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'NZ': ['New Zealand', 2625, 3424],
+  'NZ': ['New Zealand', 2623, 3422],
   'CA': ['Canada', 166, 232],
   'AU': ['Australia', 237, 337],
-  'US': ['United States', 324, 478],
+  'US': ['United States', 323, 478],
   'BN': ['Brunei', 39, 51],
   'CN': ['China', 147, 178],
   'MY': ['Malaysia', 35, 50],
--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '88123769@N02',
   'alias': 'volvob12b',
   'name': 'Bernard Spragg. NZ',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/volvob12b/',
   'location': 'Christchurch, New Zealand',
   'countries': 11,
-  'markers': 3660,
-  'photos': 4881
+  'markers': 3657,
+  'photos': 4879
 }
