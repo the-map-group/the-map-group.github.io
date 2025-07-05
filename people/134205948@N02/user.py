96ca74c2739229d7d30a9cb07157ce791a23726c
The Map Group <admin@the-map-group.top>
2025-07-05 03:45:24 -0300

    [auto] Updated map for member 'giorgiorodano46'

--- people/134205948@N02/user.py
+++ people/134205948@N02/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '134205948@N02',
   'alias': '134205948@N02',
   'name': 'Giorgio Rodano',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/134205948@N02_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/134205948@N02/',
   'location': 'Roma, Italy',
   'countries': 8,
-  'markers': 7021,
-  'photos': 7779
+  'markers': 7022,
+  'photos': 7780
 }
