f21ce6f4e27e664af4ee26e645e2f575ea872bd5
The Map Group <admin@the-map-group.top>
2025-07-05 03:57:23 -0300

    [auto] Updated map for member 'D-Stanley'

--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '79721788@N00',
   'alias': 'davidstanleytravel',
   'name': 'David Stanley',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/79721788@N00_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
   'location': 'Nanaimo, Canada',
   'countries': 124,
-  'markers': 8018,
-  'photos': 9319
+  'markers': 8021,
+  'photos': 9323
 }
