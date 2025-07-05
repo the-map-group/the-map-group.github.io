8283ea10996e6288d107b989ca5ed44893ce11e8
The Map Group <admin@the-map-group.top>
2025-07-05 04:01:00 -0300

    [auto] Updated map for member 'Al Case'

--- people/al_case/user.py
+++ people/al_case/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '167430854@N07',
   'alias': 'al_case',
   'name': 'Al Case',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/167430854@N07_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/al_case/',
   'location': 'Ashland, Oregon, USA',
   'countries': 19,
-  'markers': 748,
-  'photos': 863
+  'markers': 749,
+  'photos': 864
 }
