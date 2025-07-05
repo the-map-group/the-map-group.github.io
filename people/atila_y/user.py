fdbacb63f7d050b941ab45c4da84e85faf3b850b
The Map Group <admin@the-map-group.top>
2025-07-05 03:45:39 -0300

    [auto] Updated map for member 'Atila Yumusakkaya'

--- people/atila_y/user.py
+++ people/atila_y/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '8009375@N03',
   'alias': 'atila_y',
   'name': 'Atila Yumusakkaya',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8009375@N03_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/atila_y/',
   'location': 'Istanbul, Turkey',
   'countries': 22,
-  'markers': 1396,
-  'photos': 1580
+  'markers': 1397,
+  'photos': 1582
 }
