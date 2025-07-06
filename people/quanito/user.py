e7b2da2650dc81ca17d47d13acecb1b0bf767718
The Map Group <admin@the-map-group.top>
2025-07-06 03:31:06 -0300

    [auto] Updated map for member 'pascalvu1'

--- people/quanito/user.py
+++ people/quanito/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '135298455@N04',
   'alias': 'quanito',
   'name': 'Pascal VU',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/135298455@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/quanito/',
   'location': 'Nogent sur Marne, FRANCE',
   'countries': 53,
-  'markers': 6298,
-  'photos': 33610
+  'markers': 6312,
+  'photos': 33636
 }
