eacbf065a219188f1c15b5c0a3883128cadadee6
The Map Group <admin@the-map-group.top>
2025-07-06 03:00:58 -0300

    [auto] Updated map for member 'hans pohl'

--- people/51839301@N00/user.py
+++ people/51839301@N00/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '51839301@N00',
   'alias': '51839301@N00',
   'name': 'Hans Pohl',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/51839301@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/51839301@N00/',
   'location': '',
   'countries': 12,
   'markers': 3860,
-  'photos': 5864
+  'photos': 5865
 }
