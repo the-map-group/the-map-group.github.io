3f35a6758e4b828dd586edd1889fcf694174f356
The Map Group <admin@the-map-group.top>
2025-07-07 05:22:58 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/countries.py
+++ people/zimwiz/countries.py
@@ -2,6 +2,6 @@ countries_dict = {
   'AU': ['Australia', 1, 1],
   'GL': ['Greenland', 1, 1],
   'VN': ['Vietnam', 2, 2],
-  'US': ['United States', 677, 968],
+  'US': ['United States', 678, 969],
   'BM': ['Bermuda', 9, 9]
 }
--- people/zimwiz/user.py
+++ people/zimwiz/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/zimwiz/',
   'location': '',
   'countries': 5,
-  'markers': 690,
-  'photos': 981
+  'markers': 691,
+  'photos': 982
 }
