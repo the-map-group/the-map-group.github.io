15e791ff55d94bfa96b111de0d0c9269ffaf9bfc
The Map Group <admin@the-map-group.top>
2025-07-07 05:10:52 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/user.py
+++ people/malioli/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/malioli/',
   'location': 'Karlovac, Hrvatska',
   'countries': 5,
-  'markers': 2413,
-  'photos': 5177
+  'markers': 2414,
+  'photos': 5178
 }
