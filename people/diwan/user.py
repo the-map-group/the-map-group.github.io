883fead4714c9f98a426c669c18d4a536ae3204c
The Map Group <admin@the-map-group.top>
2025-07-07 05:09:52 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/user.py
+++ people/diwan/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '81504125@N00',
   'alias': 'diwan',
   'name': 'Dirk Wandel',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/81504125@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/diwan/',
   'location': 'Magdeburg, Germany',
   'countries': 24,
-  'markers': 8560,
-  'photos': 9089
+  'markers': 8561,
+  'photos': 9090
 }
