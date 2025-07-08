0bff7bfbd413092b0558407d5d987c5c4eb6aaba
The Map Group <admin@the-map-group.top>
2025-07-08 03:21:19 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/user.py
+++ people/23244764@N08/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '23244764@N08',
   'alias': '23244764@N08',
   'name': 'Jim Smith',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/23244764@N08/',
   'location': 'east Tennessee, USA',
   'countries': 11,
   'markers': 287,
-  'photos': 550
+  'photos': 551
 }
