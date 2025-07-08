a23b2f692f9cfca495053d32766c8a257733e45c
The Map Group <admin@the-map-group.top>
2025-07-08 03:26:18 -0300

    [auto] Updated map for member 'Urban Florida Photographer'

--- people/miamiboy/user.py
+++ people/miamiboy/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '126251698@N03',
   'alias': 'miamiboy',
   'name': 'Urban Florida Photographer',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/126251698@N03_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/miamiboy/',
   'location': '',
   'countries': 26,
-  'markers': 8207,
-  'photos': 13311
+  'markers': 8209,
+  'photos': 13313
 }
