4144ba55a45e7f1b4a89d9ed84c764e47cc2b449
The Map Group <admin@the-map-group.top>
2025-07-08 03:01:42 -0300

    [auto] Updated map for member 'haroldmoses'

--- people/mosesharold/coords.py
+++ people/mosesharold/coords.py
@@ -2027,5 +2027,7 @@ coords_dict = {
   '55.615205,-1.655256': ['GB', 'United Kingdom'],
   '55.615213,-1.655209': ['GB', 'United Kingdom'],
   '55.615333,-1.655206': ['GB', 'United Kingdom'],
-  '55.615213,-1.655164': ['GB', 'United Kingdom']
+  '55.615213,-1.655164': ['GB', 'United Kingdom'],
+  '55.615238,-1.655125': ['GB', 'United Kingdom'],
+  '55.6152,-1.655117': ['GB', 'United Kingdom']
 }
--- people/mosesharold/user.py
+++ people/mosesharold/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/mosesharold/',
   'location': 'SPENNYMOOR, United Kingdom',
   'countries': 76,
-  'markers': 3454,
-  'photos': 4774
+  'markers': 3456,
+  'photos': 4776
 }
