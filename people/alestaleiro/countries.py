2c1f00a60219e84c1014eb1cb39860757eb7ae32
The Map Group <admin@the-map-group.top>
2025-07-08 03:19:33 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'BR': ['Brazil', 2576, 3646],
+  'BR': ['Brazil', 2578, 3648],
   'AR': ['Argentina', 218, 242],
   'CL': ['Chile', 26, 31],
   'PE': ['Peru', 14, 24],
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '99949037@N00',
   'alias': 'alestaleiro',
   'name': 'Alejandro David Olivera',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'location': 'Praia do Estaleiro - B. Camboriú, BRASIL',
   'countries': 26,
-  'markers': 3931,
-  'photos': 5361
+  'markers': 3933,
+  'photos': 5363
 }
