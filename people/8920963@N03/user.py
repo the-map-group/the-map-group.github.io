4f9ce62e3bfb95c80d102bdd8b254a3126cab44f
The Map Group <admin@the-map-group.top>
2025-07-08 03:01:14 -0300

    [auto] Updated map for member 'Steenjep'

--- people/8920963@N03/user.py
+++ people/8920963@N03/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '8920963@N03',
   'alias': '8920963@N03',
   'name': 'Steen Jeppesen',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8920963@N03_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/8920963@N03/',
   'location': 'Herning, Denmark',
   'countries': 30,
-  'markers': 12264,
-  'photos': 17648
+  'markers': 12270,
+  'photos': 17657
 }
