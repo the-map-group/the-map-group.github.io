8466cddd43492142b318f3366a99623e00edf113
The Map Group <admin@the-map-group.top>
2025-07-09 03:20:07 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -10,7 +10,7 @@ countries_dict = {
   'LV': ['Latvia', 149, 149],
   'HR': ['Croatia', 214, 244],
   'FI': ['Finland', 126, 127],
-  'IT': ['Italy', 2697, 3469],
+  'IT': ['Italy', 2711, 3485],
   'AZ': ['Azerbaijan', 2, 2],
   'EE': ['Estonia', 170, 170],
   'TR': ['Turkey', 338, 445]
--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
   'location': 'Munich, Germany',
   'countries': 15,
-  'markers': 7396,
-  'photos': 35054
+  'markers': 7410,
+  'photos': 35070
 }
