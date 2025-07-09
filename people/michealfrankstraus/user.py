2bdc5c71a5abc622909439c9364410f025040bca
The Map Group <admin@the-map-group.top>
2025-07-09 03:25:12 -0300

    [auto] Updated map for member 'mstraus52'

--- people/michealfrankstraus/user.py
+++ people/michealfrankstraus/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '188257341@N04',
   'alias': 'michealfrankstraus',
   'name': 'Micheal Straus',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188257341@N04_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/michealfrankstraus/',
   'location': 'Portland-on-Willamette, USA',
   'countries': 35,
-  'markers': 1549,
-  'photos': 1647
+  'markers': 1550,
+  'photos': 1648
 }
