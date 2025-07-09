ef85e97fa225aee5d983e0b81b3913afc204888f
The Map Group <admin@the-map-group.top>
2025-07-09 03:06:36 -0300

    [auto] Updated map for member 'Howard Somerville (10 million views)'

--- people/byhowardsomerville/user.py
+++ people/byhowardsomerville/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '14463685@N07',
   'alias': 'byhowardsomerville',
   'name': 'Howard Somerville',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/byhowardsomerville/',
   'location': 'London, UK',
   'countries': 37,
-  'markers': 855,
-  'photos': 1020
+  'markers': 864,
+  'photos': 1038
 }
