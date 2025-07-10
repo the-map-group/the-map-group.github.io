7acae1e788d277ac8d6adb1cebd199373b927f6e
The Map Group <admin@the-map-group.top>
2025-07-10 18:27:37 -0300

    [auto] Updated map for member 'xd_travel'

--- people/111314495@N05/user.py
+++ people/111314495@N05/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '111314495@N05',
   'alias': '111314495@N05',
   'name': 'Mike X-d',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/111314495@N05_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7396/buddyicons/111314495@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/111314495@N05/',
   'location': 'Beachwood, US',
   'countries': 60,
   'markers': 5674,
-  'photos': 10133
+  'photos': 10139
 }
