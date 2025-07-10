6ce8a2e8a70842e4fc2c6ac5526b6d633c95d324
The Map Group <admin@the-map-group.top>
2025-07-10 18:32:15 -0300

    [auto] Updated map for member 'Kevin Povenz'

--- people/kevinpovenz/user.py
+++ people/kevinpovenz/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '19613665@N08',
   'alias': 'kevinpovenz',
   'name': 'Kevin Povenz',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/19613665@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/kevinpovenz/',
   'location': 'Jenison Michigan, United States',
   'countries': 7,
   'markers': 872,
-  'photos': 2968
+  'photos': 2969
 }
