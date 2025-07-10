01370a4c72a8a8f6bf48fe05b2d583715eb48b52
The Map Group <admin@the-map-group.top>
2025-07-10 18:17:27 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '8121206@N07',
   'alias': 'chrispricephotography',
   'name': 'Chris Price',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm3.staticflickr.com/2846/buddyicons/8121206@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/chrispricephotography/',
   'location': 'Blacksburg, VA, USA',
   'countries': 10,
-  'markers': 1122,
-  'photos': 1578
+  'markers': 1123,
+  'photos': 1579
 }
