27d8af19a9b8095268a6247f32bc4cd11b5f7b79
The Map Group <admin@the-map-group.top>
2025-07-10 18:53:15 -0300

    [auto] Updated map for member 'Wayloncash'

--- people/wayloncash/countries.py
+++ people/wayloncash/countries.py
@@ -21,7 +21,7 @@ countries_dict = {
   'NL': ['Netherlands', 15, 15],
   'TR': ['Turkey', 36, 55],
   'SM': ['San Marino', 6, 29],
-  'ES': ['Spain', 19174, 31555],
+  'ES': ['Spain', 19176, 31557],
   'IN': ['India', 1, 1],
   'PL': ['Poland', 17, 122],
   'HR': ['Croatia', 5, 12],
--- people/wayloncash/user.py
+++ people/wayloncash/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '97555832@N08',
   'alias': 'wayloncash',
   'name': 'Wayloncash',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm6.staticflickr.com/5339/buddyicons/97555832@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/wayloncash/',
   'location': '',
   'countries': 28,
-  'markers': 22826,
-  'photos': 47515
+  'markers': 22828,
+  'photos': 47517
 }
