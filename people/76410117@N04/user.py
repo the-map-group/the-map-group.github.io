25c3a33bb68c2546c63328533a17866250e107db
The Map Group <admin@the-map-group.top>
2025-07-10 18:46:29 -0300

    [auto] Updated map for member 'yves62160'

--- people/76410117@N04/user.py
+++ people/76410117@N04/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '76410117@N04',
   'alias': '76410117@N04',
   'name': 'YVES NEVEJANS',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm9.staticflickr.com/8039/buddyicons/76410117@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/76410117@N04/',
   'location': '',
   'countries': 6,
   'markers': 2001,
-  'photos': 3695
+  'photos': 3697
 }
