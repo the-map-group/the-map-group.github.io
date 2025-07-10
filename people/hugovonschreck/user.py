e02d37f62bf55b7f0a764f0fb0b4b89f80fcca37
The Map Group <admin@the-map-group.top>
2025-07-10 18:32:33 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '74758246@N08',
   'alias': 'hugovonschreck',
   'name': 'Heinz Dieter  Heil',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm6.staticflickr.com/5549/buddyicons/74758246@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/hugovonschreck/',
   'location': 'Linsengericht, Germany',
   'countries': 11,
   'markers': 1578,
-  'photos': 6430
+  'photos': 6431
 }
