d8d9eaac263ab09645ab981fb8a7de78a77deb9f
The Map Group <admin@the-map-group.top>
2025-07-10 18:25:21 -0300

    [auto] Updated map for member 'm01229'

--- people/39908901@N06/user.py
+++ people/39908901@N06/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '39908901@N06',
   'alias': '39908901@N06',
   'name': 'm01229',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/39908901@N06_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/39908901@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/39908901@N06/',
   'location': '',
   'countries': 19,
-  'markers': 11778,
-  'photos': 36200
+  'markers': 11782,
+  'photos': 36207
 }
