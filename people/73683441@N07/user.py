4fdb11c46392bcf26c1220cedcea277ce3b564e0
The Map Group <admin@the-map-group.top>
2025-07-10 18:33:35 -0300

    [auto] Updated map for member 'Gerald (Wayne) Prout'

--- people/73683441@N07/user.py
+++ people/73683441@N07/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '73683441@N07',
   'alias': '73683441@N07',
   'name': 'Gerald (Wayne) Prout',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm4.staticflickr.com/3793/buddyicons/73683441@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/73683441@N07/',
   'location': '',
   'countries': 8,
   'markers': 4202,
-  'photos': 9068
+  'photos': 9069
 }
