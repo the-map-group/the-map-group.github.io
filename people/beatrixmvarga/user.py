94dce0bfceca1c2676b4c317046c215f651d7439
The Map Group <admin@the-map-group.top>
2025-07-10 03:04:41 -0300

    [auto] Updated map for member 'Beatrix MK'

--- people/beatrixmvarga/user.py
+++ people/beatrixmvarga/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '33989856@N04',
   'alias': 'beatrixmvarga',
   'name': 'Beatrix',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/33989856@N04_r.jpg',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/beatrixmvarga/',
   'location': 'USA',
   'countries': 4,
-  'markers': 757,
-  'photos': 761
+  'markers': 758,
+  'photos': 762
 }
