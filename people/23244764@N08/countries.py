79d310c4d633b4e20809fb463aede228cf5733ff
The Map Group <admin@the-map-group.top>
2025-07-11 11:05:00 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/countries.py
+++ people/23244764@N08/countries.py
@@ -3,11 +3,11 @@ countries_dict = {
   'HT': ['Haiti', 1, 1],
   'BS': ['Bahamas', 2, 2],
   'MA': ['Morocco', 14, 14],
-  'GI': ['Gibraltar', 20, 20],
+  'GI': ['Gibraltar', 21, 21],
   'AW': ['Aruba', 13, 14],
   'ES': ['Spain', 46, 69],
-  'US': ['United States', 171, 407],
+  'US': ['United States', 173, 421],
   'AS': ['American Samoa', 1, 1],
   'PT': ['Portugal', 1, 1],
-  'BZ': ['Belize', 5, 9]
+  'BZ': ['Belize', 5, 10]
 }
--- people/23244764@N08/user.py
+++ people/23244764@N08/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '23244764@N08',
   'alias': '23244764@N08',
   'name': 'Jim Smith',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/23244764@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/23244764@N08/',
   'location': 'east Tennessee, USA',
   'countries': 11,
-  'markers': 287,
-  'photos': 551
+  'markers': 290,
+  'photos': 567
 }
