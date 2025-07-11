185bf0a5d1730b801cbef37f0f568d2bd2a6c85d
The Map Group <admin@the-map-group.top>
2025-07-11 00:27:48 -0300

    [auto] Updated map for member 'Brit 70013 fan'

--- people/93936890@N02/user.py
+++ people/93936890@N02/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '93936890@N02',
   'alias': '93936890@N02',
   'name': 'CLIFF BANCROFT',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/93936890@N02_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7844/buddyicons/93936890@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/93936890@N02/',
   'location': 'Victoria, B.C., Canada',
   'countries': 43,
-  'markers': 878,
-  'photos': 1268
+  'markers': 881,
+  'photos': 1272
 }
