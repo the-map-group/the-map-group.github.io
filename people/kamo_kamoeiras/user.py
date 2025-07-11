1286812cb653b9c2cbbc1249dfeeb634a2a500c9
The Map Group <admin@the-map-group.top>
2025-07-11 04:04:19 -0300

    [auto] Updated map for member 'K@moeiras'

--- people/kamo_kamoeiras/user.py
+++ people/kamo_kamoeiras/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '163799240@N04',
   'alias': 'kamo_kamoeiras',
   'name': 'KAMO KAMOEIRAS',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/163799240@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/kamo_kamoeiras/',
   'location': 'Salamanca, ESPAÑA',
   'countries': 6,
-  'markers': 2113,
-  'photos': 2739
+  'markers': 2135,
+  'photos': 2762
 }
