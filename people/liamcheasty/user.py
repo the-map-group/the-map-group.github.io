038a072684db230182642d04a56e4f5485a87664
The Map Group <admin@the-map-group.top>
2025-07-11 03:23:32 -0300

    [auto] Updated map for member 'Liam Cheasty'

--- people/liamcheasty/user.py
+++ people/liamcheasty/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '43413480@N07',
   'alias': 'liamcheasty',
   'name': 'Liam Cheasty',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm4.staticflickr.com/3742/buddyicons/43413480@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/liamcheasty/',
   'location': 'Waterford, Ireland',
   'countries': 29,
-  'markers': 3538,
-  'photos': 17965
+  'markers': 3578,
+  'photos': 18229
 }
