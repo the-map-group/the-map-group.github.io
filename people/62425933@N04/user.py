38aac7226d677664ba79c519f8979baf6f460641
The Map Group <admin@the-map-group.top>
2025-07-11 13:12:41 -0300

    [auto] Updated map for member 'Wes Iversen'

--- people/62425933@N04/user.py
+++ people/62425933@N04/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '62425933@N04',
   'alias': '62425933@N04',
   'name': 'Wes Iversen',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm3.staticflickr.com/2562/buddyicons/62425933@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/62425933@N04/',
   'location': '',
   'countries': 2,
-  'markers': 2282,
-  'photos': 3561
+  'markers': 2283,
+  'photos': 3570
 }
