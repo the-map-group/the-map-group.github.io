e5a23d9367523155649e263f2ecabc23f7e09fb9
The Map Group <admin@the-map-group.top>
2025-07-11 17:46:41 -0300

    Reset map for member 'beatrixmvarga'

--- people/beatrixmvarga/countries.py
+++ people/beatrixmvarga/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'HU': ['Hungary', 86, 87],
-  'IT': ['Italy', 10, 10],
   'US': ['United States', 535, 538],
-  'AT': ['Austria', 127, 127]
+  'AT': ['Austria', 127, 127],
+  'HU': ['Hungary', 86, 87],
+  'IT': ['Italy', 10, 10]
 }
--- people/beatrixmvarga/user.py
+++ people/beatrixmvarga/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '33989856@N04',
   'alias': 'beatrixmvarga',
   'name': 'Beatrix',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm6.staticflickr.com/5579/buddyicons/33989856@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/beatrixmvarga/',
   'location': 'USA',
   'countries': 4,
