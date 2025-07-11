8b8e31bccc48a33a2959f5d1789414cfb018797b
The Map Group <admin@the-map-group.top>
2025-07-11 09:55:22 -0300

    [auto] Updated map for member 'Jo 0405'

--- people/basses-lumieres/countries.py
+++ people/basses-lumieres/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'FR': ['France', 178, 764],
+  'FR': ['France', 179, 768],
   'IT': ['Italy', 2, 4],
   'ES': ['Spain', 1, 1]
 }
--- people/basses-lumieres/user.py
+++ people/basses-lumieres/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '108339617@N08',
   'alias': 'basses-lumieres',
   'name': 'Jo 0405',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/108339617@N08_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7828/buddyicons/108339617@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/basses-lumieres/',
   'location': 'Terre, France',
   'countries': 3,
-  'markers': 181,
-  'photos': 769
+  'markers': 182,
+  'photos': 773
 }
