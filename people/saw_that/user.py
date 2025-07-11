236122daf6e0b905e2b1bc5596e8758e5a8c77b4
The Map Group <admin@the-map-group.top>
2025-07-11 17:40:10 -0300

    Reset map for member 'saw_that'

--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '88196436@N04',
   'alias': 'saw_that',
   'name': 'I saw_that',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/88196436@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/saw_that/',
   'location': 'Iqaluit, Nunavut, Canada',
   'countries': 18,
