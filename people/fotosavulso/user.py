b97bbf15eed8ee9e251ae945cbeee244841ba566
The Map Group <admin@the-map-group.top>
2025-07-11 07:28:05 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/user.py
+++ people/fotosavulso/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '69623157@N05',
   'alias': 'fotosavulso',
   'name': 'Fotos Avulso',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/69623157@N05_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/69623157@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/fotosavulso/',
   'location': '',
   'countries': 10,
-  'markers': 578,
-  'photos': 623
+  'markers': 579,
+  'photos': 625
 }
