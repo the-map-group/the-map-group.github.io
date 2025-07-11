f5688938d6a3d7022026df7f4407bc847fc45049
The Map Group <admin@the-map-group.top>
2025-07-11 04:56:48 -0300

    [auto] Updated map for member 'TIA International Photography'

--- people/tiascapes/user.py
+++ people/tiascapes/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '12744878@N02',
   'alias': 'tiascapes',
   'name': 'TIA INTERNATIONAL PHOTOGRAPHY',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/12744878@N02_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/12744878@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/tiascapes/',
   'location': 'Seattle, Washington, USA',
   'countries': 11,
   'markers': 871,
-  'photos': 1243
+  'photos': 1244
 }
