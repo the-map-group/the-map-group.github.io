2629fd678416f6fa7995900ea1754758430348cf
The Map Group <admin@the-map-group.top>
2025-07-11 13:23:20 -0300

    [auto] Updated map for member 'Pieter Lommerse'

--- people/charlesinshanghai/user.py
+++ people/charlesinshanghai/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '161392673@N02',
   'alias': 'charlesinshanghai',
   'name': 'Charles in Shanghai',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/161392673@N02_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7908/buddyicons/161392673@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/charlesinshanghai/',
   'location': 'The Netherlands',
   'countries': 17,
   'markers': 261,
-  'photos': 386
+  'photos': 398
 }
