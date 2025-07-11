813bf45379ca06e7c5f0edcab9c0201f3076d18f
The Map Group <admin@the-map-group.top>
2025-07-11 15:05:36 -0300

    Reset map for member 'sarahvain'

--- people/sarahvain/countries.py
+++ people/sarahvain/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'CZ': ['Czech Republic', 12, 17],
-  'FR': ['France', 50, 103],
-  'HU': ['Hungary', 18, 39],
-  'US': ['United States', 12810, 17199],
-  'CN': ['China', 17, 77],
+  'US': ['United States', 12811, 17200],
   'IL': ['Israel', 9, 31],
+  'NL': ['Netherlands', 8, 8],
   'CL': ['Chile', 37, 119],
+  'DE': ['Germany', 25, 49],
   'FI': ['Finland', 38, 83],
   'BE': ['Belgium', 14, 93],
-  'JP': ['Japan', 28, 46],
-  'DE': ['Germany', 25, 49],
-  'NL': ['Netherlands', 8, 8],
+  'FR': ['France', 50, 103],
   'GB': ['United Kingdom', 165, 439],
-  'AT': ['Austria', 18, 28]
+  'HU': ['Hungary', 18, 39],
+  'AT': ['Austria', 18, 28],
+  'CZ': ['Czech Republic', 12, 17],
+  'JP': ['Japan', 28, 46],
+  'CN': ['China', 17, 77]
 }
--- people/sarahvain/user.py
+++ people/sarahvain/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '7633518@N08',
   'alias': 'sarahvain',
   'name': 'Sarah Stierch',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm1.staticflickr.com/205/buddyicons/7633518@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/sarahvain/',
   'location': 'Sonoma, CA, USA',
   'countries': 14,
-  'markers': 13250,
-  'photos': 18338
+  'markers': 13251,
+  'photos': 18339
 }
