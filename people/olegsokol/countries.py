b6ed0746f72bd19851abcb135a313a6ac5742b4b
The Map Group <admin@the-map-group.top>
2025-07-11 04:20:02 -0300

    [auto] Updated map for member 'Oleg S .'

--- people/olegsokol/countries.py
+++ people/olegsokol/countries.py
@@ -14,7 +14,7 @@ countries_dict = {
   'SE': ['Sweden', 11, 12],
   'TW': ['Taiwan', 5, 5],
   'IT': ['Italy', 2, 2],
-  'US': ['United States', 443, 570],
+  'US': ['United States', 443, 571],
   'IS': ['Iceland', 23, 29],
   'DK': ['Denmark', 1, 1],
   'AT': ['Austria', 6, 6],
--- people/olegsokol/user.py
+++ people/olegsokol/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '36531876@N00',
   'alias': 'olegsokol',
   'name': 'Oleg S',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm4.staticflickr.com/3783/buddyicons/36531876@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/olegsokol/',
   'location': 'Philadelphia, USA',
   'countries': 27,
   'markers': 746,
-  'photos': 913
+  'photos': 914
 }
