65f6d9874c0dc36ae84d1a5e132108445ad3d9db
The Map Group <admin@the-map-group.top>
2025-07-11 12:41:07 -0300

    [auto] Updated map for member 'Gérard Cachon'

--- people/bennett-cachon/countries.py
+++ people/bennett-cachon/countries.py
@@ -2,5 +2,5 @@ countries_dict = {
   'CA': ['Canada', 5, 7],
   'CL': ['Chile', 7, 12],
   'US': ['United States', 40, 50],
-  'BQ': ['Bonaire', 61, 96]
+  'BQ': ['Bonaire', 61, 98]
 }
--- people/bennett-cachon/user.py
+++ people/bennett-cachon/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '96671573@N04',
   'alias': 'bennett-cachon',
   'name': 'Gérard  Cachon',
-  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/96671573@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/bennett-cachon/',
   'location': '',
   'countries': 4,
   'markers': 113,
-  'photos': 165
+  'photos': 167
 }
