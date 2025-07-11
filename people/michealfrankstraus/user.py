3d2e69c7bc8983789e89b46c84face291e04982d
The Map Group <admin@the-map-group.top>
2025-07-11 11:14:16 -0300

    [auto] Updated map for member 'mstraus52'

--- people/michealfrankstraus/user.py
+++ people/michealfrankstraus/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/michealfrankstraus/',
   'location': 'Portland-on-Willamette, USA',
   'countries': 35,
-  'markers': 1551,
-  'photos': 1649
+  'markers': 1559,
+  'photos': 1709
 }
