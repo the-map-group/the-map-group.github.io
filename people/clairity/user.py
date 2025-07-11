5c2932d81e8dcab3abca506950aafb9923536199
The Map Group <admin@the-map-group.top>
2025-07-11 10:51:25 -0300

    [auto] Updated map for member 'Sharon Mollerus'

--- people/clairity/user.py
+++ people/clairity/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '38315261@N00',
   'alias': 'clairity',
   'name': 'Sharon Mollerus',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38315261@N00_r.jpg',
+  'avatar': 'https://farm4.staticflickr.com/3720/buddyicons/38315261@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/clairity/',
   'location': 'Minneapolis, United States',
   'countries': 7,
-  'markers': 3616,
-  'photos': 14202
+  'markers': 3617,
+  'photos': 14203
 }
