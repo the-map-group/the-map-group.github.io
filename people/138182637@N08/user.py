8cf622a53e2867db0c5367b6af17d88f55b39a77
The Map Group <admin@the-map-group.top>
2025-07-11 04:05:57 -0300

    [auto] Updated map for member 'dimitri.verledens'

--- people/138182637@N08/user.py
+++ people/138182637@N08/user.py
@@ -2,10 +2,10 @@ user_info = {
   'id': '138182637@N08',
   'alias': '138182637@N08',
   'name': 'Dimitri Verledens',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/138182637@N08_r.jpg',
+  'avatar': 'https://farm2.staticflickr.com/1453/buddyicons/138182637@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/138182637@N08/',
   'location': 'Gent, België',
   'countries': 27,
-  'markers': 1010,
-  'photos': 1316
+  'markers': 1055,
+  'photos': 1370
 }
