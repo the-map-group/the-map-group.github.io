2ed8278a3bd36b2407a84cc2cc3c319670328681
The Map Group <admin@the-map-group.top>
2025-07-12 15:55:43 -0300

    [auto] Updated map for member 'Robert C. Abraham'

--- people/79080/countries.py
+++ people/79080/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'MX': ['Mexico', 3, 3],
-  'AG': ['Antigua and Barbuda', 1, 1],
-  'GB': ['United Kingdom', 2, 2],
-  'JM': ['Jamaica', 4, 4],
-  'TH': ['Thailand', 11, 11],
-  'CN': ['China', 2, 2],
   'CA': ['Canada', 172, 201],
+  'JP': ['Japan', 1, 1],
+  'MX': ['Mexico', 3, 3],
+  'JM': ['Jamaica', 4, 4],
+  'GB': ['United Kingdom', 2, 2],
+  'LC': ['St Lucia', 1, 1],
+  'CN': ['China', 2, 2],
+  'AG': ['Antigua and Barbuda', 1, 1],
   'US': ['United States', 20, 20],
   'VG': ['British Virgin Islands', 1, 1],
   'BB': ['Barbados', 1, 1],
-  'TR': ['Turkey', 2, 2],
   'SG': ['Singapore', 17, 17],
+  'TH': ['Thailand', 11, 11],
   'MY': ['Malaysia', 7, 7],
-  'JP': ['Japan', 1, 1],
-  'LC': ['St Lucia', 1, 1]
+  'TR': ['Turkey', 2, 2]
 }
--- people/79080/user.py
+++ people/79080/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '31693521@N08',
   'alias': '79080',
   'name': 'Robert C. Abraham',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/31693521@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/31693521@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/79080/',
   'location': 'Halifax, Nova Scotia, Canada',
   'countries': 15,
