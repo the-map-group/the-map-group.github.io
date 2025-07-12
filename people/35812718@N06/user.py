fe5c7b30691af09a00c62700686b6866d2795eb1
The Map Group <admin@the-map-group.top>
2025-07-12 13:16:41 -0300

    [auto] Updated map for member 'Mars Ryan Hartdegen'

--- people/35812718@N06/user.py
+++ people/35812718@N06/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '35812718@N06',
   'alias': '35812718@N06',
   'name': 'Mars Ryan Hartdegen',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/35812718@N06_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7766/buddyicons/35812718@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/35812718@N06/',
   'location': '',
   'countries': 1,
