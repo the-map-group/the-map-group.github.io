a8d0b121a9bca7b8aa841fbde269197c904f1dc6
The Map Group <admin@the-map-group.top>
2025-07-12 11:21:11 -0300

    [auto] Updated map for member 'javiernoval'

--- people/jnoval/countries.py
+++ people/jnoval/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'FR': ['France', 15, 18],
-  'HR': ['Croatia', 1, 1],
   'ES': ['Spain', 118, 267],
-  'IT': ['Italy', 4, 5],
-  'US': ['United States', 2, 2],
-  'SE': ['Sweden', 16, 18],
-  'GB': ['United Kingdom', 22, 24],
-  'AT': ['Austria', 1, 1],
-  'CU': ['Cuba', 1, 3],
-  'PT': ['Portugal', 3, 3],
   'DE': ['Germany', 188, 262],
-  'GR': ['Greece', 12, 13]
+  'SE': ['Sweden', 16, 18],
+  'FR': ['France', 15, 18],
+  'PT': ['Portugal', 3, 3],
+  'GB': ['United Kingdom', 22, 24],
+  'GR': ['Greece', 12, 13],
+  'HR': ['Croatia', 1, 1],
+  'IT': ['Italy', 4, 5],
+  'AT': ['Austria', 1, 1],
+  'US': ['United States', 2, 2],
+  'CU': ['Cuba', 1, 3]
 }
--- people/jnoval/user.py
+++ people/jnoval/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '66031636@N00',
   'alias': 'jnoval',
   'name': 'Javier Noval',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/66031636@N00_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/66031636@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/jnoval/',
   'location': 'München, Germany',
   'countries': 12,
