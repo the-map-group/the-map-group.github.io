f5449d99f3fb0a581c1434fc080df8f5eade1e70
The Map Group <admin@the-map-group.top>
2025-07-12 12:57:34 -0300

    [auto] Updated map for member 'pepemartin2006'

--- people/mariomartins/user.py
+++ people/mariomartins/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '188218905@N02',
   'alias': 'mariomartins',
   'name': 'Mário  Martins (Pepe Martín)',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188218905@N02_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/188218905@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/mariomartins/',
   'location': 'Figueira da Foz e Ovar, Portugal',
   'countries': 4,
