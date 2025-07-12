fa0429f9e4f671333c718aa32fd5a0447a76372c
The Map Group <admin@the-map-group.top>
2025-07-12 12:34:48 -0300

    [auto] Updated map for member 'Miguel - AF'

--- people/189316105@N08/user.py
+++ people/189316105@N08/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '189316105@N08',
   'alias': '189316105@N08',
   'name': 'Miguel Angel Flores',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/189316105@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/189316105@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/189316105@N08/',
   'location': '',
   'countries': 1,
