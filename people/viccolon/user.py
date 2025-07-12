44a8da290e98425efaa1df7a7d3d5c8954339628
The Map Group <admin@the-map-group.top>
2025-07-12 13:59:59 -0300

    [auto] Updated map for member 'VicColon'

--- people/viccolon/user.py
+++ people/viccolon/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '90347682@N00',
   'alias': 'viccolon',
   'name': 'VicColon',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/90347682@N00_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/90347682@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/viccolon/',
   'location': 'St. Petersburg, Russia',
   'countries': 34,
