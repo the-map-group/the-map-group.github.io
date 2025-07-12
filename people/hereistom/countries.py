552c2794136bcab0f611e42cd4e3a38776834dcf
The Map Group <admin@the-map-group.top>
2025-07-12 13:29:28 -0300

    [auto] Updated map for member 'HereIsTom'

--- people/hereistom/countries.py
+++ people/hereistom/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'HR': ['Croatia', 1, 1],
-  'TR': ['Turkey', 63, 63],
-  'ES': ['Spain', 49, 49],
   'NL': ['Netherlands', 1213, 1313],
+  'TH': ['Thailand', 48, 56],
+  'TR': ['Turkey', 63, 63],
+  'HR': ['Croatia', 1, 1],
+  'SI': ['Slovenia', 1, 1],
   'IT': ['Italy', 1, 1],
+  'PT': ['Portugal', 95, 96],
+  'ES': ['Spain', 49, 49],
   'GR': ['Greece', 1, 1],
   'BR': ['Brazil', 2, 2],
-  'TH': ['Thailand', 48, 56],
-  'SI': ['Slovenia', 1, 1],
-  'BE': ['Belgium', 3, 3],
   'DE': ['Germany', 2, 2],
-  'PT': ['Portugal', 95, 96]
+  'BE': ['Belgium', 3, 3]
 }
--- people/hereistom/user.py
+++ people/hereistom/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '47035764@N03',
   'alias': 'hereistom',
   'name': 'Tom Jutte',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/47035764@N03_r.jpg',
+  'avatar': 'https://farm9.staticflickr.com/8488/buddyicons/47035764@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/hereistom/',
   'location': 'Odijk, Netherlands',
   'countries': 12,
