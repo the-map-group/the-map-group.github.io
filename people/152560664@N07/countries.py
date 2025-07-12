fba4b30ecf65362dbfc30c36f097a9a766af10be
The Map Group <admin@the-map-group.top>
2025-07-12 15:02:51 -0300

    [auto] Updated map for member 'Jеdd'

--- people/152560664@N07/countries.py
+++ people/152560664@N07/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'CZ': ['Czech Republic', 2, 2],
-  'ES': ['Spain', 27, 31],
-  'MT': ['Malta', 2, 2],
-  'IT': ['Italy', 3, 3],
-  'UA': ['Ukraine', 4, 4],
-  'VA': ['Vatican City', 1, 1],
-  'FR': ['France', 15, 22],
-  'TH': ['Thailand', 32, 38],
   'RU': ['Russia', 56, 64],
+  'GB': ['United Kingdom', 12, 16],
+  'IT': ['Italy', 3, 3],
+  'VA': ['Vatican City', 1, 1],
+  'ES': ['Spain', 27, 31],
+  'TH': ['Thailand', 32, 38],
+  'FR': ['France', 15, 22],
+  'CZ': ['Czech Republic', 2, 2],
+  'MT': ['Malta', 2, 2],
   'CH': ['Switzerland', 1, 1],
-  'GB': ['United Kingdom', 12, 16]
+  'UA': ['Ukraine', 4, 4]
 }
