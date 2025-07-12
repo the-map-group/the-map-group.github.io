06b5d98d25ffe0cf75b925567610bcf64d7de29e
The Map Group <admin@the-map-group.top>
2025-07-12 13:09:00 -0300

    [auto] Updated map for member 'Francois Le Rumeur'

--- people/100112015@N06/user.py
+++ people/100112015@N06/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '100112015@N06',
   'alias': '100112015@N06',
   'name': 'Francois Le Rumeur',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/100112015@N06_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/100112015@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/100112015@N06/',
   'location': 'France',
   'countries': 3,
