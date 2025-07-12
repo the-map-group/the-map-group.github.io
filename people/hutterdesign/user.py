772006fa59d89129f0ca2f25345eec8658056566
The Map Group <admin@the-map-group.top>
2025-07-12 13:12:38 -0300

    [auto] Updated map for member 'panoround hutter'

--- people/hutterdesign/user.py
+++ people/hutterdesign/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '35555507@N03',
   'alias': 'hutterdesign',
   'name': 'Frederic Hutter',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/35555507@N03_r.jpg',
+  'avatar': 'https://farm4.staticflickr.com/3853/buddyicons/35555507@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/hutterdesign/',
   'location': 'Austria',
   'countries': 24,
