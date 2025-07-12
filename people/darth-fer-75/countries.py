85ef9c8e0aaa29d609ea96372350b1d0f163336e
The Map Group <admin@the-map-group.top>
2025-07-12 15:23:07 -0300

    [auto] Updated map for member 'Fernando Forniés Revuelta'

--- people/darth-fer-75/countries.py
+++ people/darth-fer-75/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'MT': ['Malta', 129, 524],
   'ES': ['Spain', 1082, 8025],
-  'EG': ['Egypt', 89, 515],
-  'GB': ['United Kingdom', 52, 529],
-  'DE': ['Germany', 135, 345],
   'FR': ['France', 180, 792],
   'IT': ['Italy', 131, 523],
+  'EG': ['Egypt', 89, 515],
+  'MT': ['Malta', 129, 524],
   'PH': ['Philippines', 19, 418],
-  'AT': ['Austria', 63, 270],
+  'QA': ['Qatar', 1, 10],
   'VA': ['Vatican City', 4, 59],
-  'QA': ['Qatar', 1, 10]
+  'GB': ['United Kingdom', 52, 529],
+  'DE': ['Germany', 135, 345],
+  'AT': ['Austria', 63, 270]
 }
--- people/darth-fer-75/user.py
+++ people/darth-fer-75/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '30148069@N05',
   'alias': 'darth-fer-75',
   'name': 'Fernando Forniés Revuelta',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/30148069@N05_r.jpg',
+  'avatar': 'https://farm9.staticflickr.com/8134/buddyicons/30148069@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/darth-fer-75/',
   'location': 'Zaragoza, España',
   'countries': 11,
