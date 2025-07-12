0fbb29af07456e753896fae63a7ccee2f980819b
The Map Group <admin@the-map-group.top>
2025-07-12 13:44:29 -0300

    [auto] Updated map for member 'victor san'

--- people/victor-san/user.py
+++ people/victor-san/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '54916020@N06',
   'alias': 'victor-san',
   'name': 'victor san',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/54916020@N06_r.jpg',
+  'avatar': 'https://farm3.staticflickr.com/2853/buddyicons/54916020@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/victor-san/',
   'location': '',
   'countries': 1,
