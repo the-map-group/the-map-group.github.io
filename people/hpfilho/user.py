7bd75dbd2254de7cf7f843212049e208f41a9dd6
The Map Group <admin@the-map-group.top>
2025-07-12 09:52:29 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '30622510@N02',
   'alias': 'hpfilho',
   'name': 'Haraldo Albergaria',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
   'location': 'Campinas, Brazil',
   'countries': 9,
