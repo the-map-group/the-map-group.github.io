4f8bc84bada3a9ef25704f5ee27873b8a90a600a
The Map Group <admin@the-map-group.top>
2025-07-12 10:14:21 -0300

    [auto] Updated map for member 'Lemmo2009'

--- people/lemmo2009/user.py
+++ people/lemmo2009/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '40749798@N07',
   'alias': 'lemmo2009',
   'name': 'Paul',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/40749798@N07_r.jpg',
+  'avatar': 'https://farm3.staticflickr.com/2919/buddyicons/40749798@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/lemmo2009/',
   'location': 'Cambridge, United Kingdom',
   'countries': 55,
