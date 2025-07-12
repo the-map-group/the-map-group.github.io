3332c7c98964d5ca3700a12da07158de086ad478
The Map Group <admin@the-map-group.top>
2025-07-12 15:43:41 -0300

    [auto] Updated map for member 'asarstudios'

--- people/motionage/user.py
+++ people/motionage/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '59509714@N06',
   'alias': 'motionage',
   'name': 'Asar Studios',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509714@N06_r.jpg',
+  'avatar': 'https://farm9.staticflickr.com/8004/buddyicons/59509714@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/motionage/',
   'location': 'Chicago, USA',
   'countries': 5,
