44eae3b750eafd17c7d7df4a779fd2918d4567db
The Map Group <admin@the-map-group.top>
2025-07-12 15:46:00 -0300

    [auto] Updated map for member 'sculptorli'

--- people/sculptorli/user.py
+++ people/sculptorli/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '23631104@N02',
   'alias': 'sculptorli',
   'name': 'sculptorli',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23631104@N02_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7606/buddyicons/23631104@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/sculptorli/',
   'location': '',
   'countries': 38,
