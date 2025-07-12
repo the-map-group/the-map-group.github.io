5664a4add9165cf99c01c5649af09fde06dd5e9c
The Map Group <admin@the-map-group.top>
2025-07-12 15:45:41 -0300

    [auto] Updated map for member 'MesphotosAmateur.'

--- people/191886689@N08/user.py
+++ people/191886689@N08/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '191886689@N08',
   'alias': '191886689@N08',
   'name': 'MesphotosAmateur.',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/191886689@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/191886689@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/191886689@N08/',
   'location': 'dans le 04, France',
   'countries': 3,
