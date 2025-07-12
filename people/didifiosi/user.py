321bc5caee953f5a57982f0b6fa5a1bcf0addec4
The Map Group <admin@the-map-group.top>
2025-07-12 15:46:52 -0300

    [auto] Updated map for member 'jeanpaulneveu'

--- people/didifiosi/user.py
+++ people/didifiosi/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '131237713@N04',
   'alias': 'didifiosi',
   'name': 'JEAN PAUL NEVEU',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/131237713@N04_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/131237713@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/didifiosi/',
   'location': 'Montelimar, France',
   'countries': 1,
