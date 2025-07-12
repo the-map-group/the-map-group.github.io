a766279ed08c378bcdc5b021d30927cda64f234a
The Map Group <admin@the-map-group.top>
2025-07-12 15:29:20 -0300

    [auto] Updated map for member 'MountMan Photo temp. OFF/ON'

--- people/mountmanphoto/user.py
+++ people/mountmanphoto/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '125923483@N06',
   'alias': 'mountmanphoto',
   'name': 'MountMan Photo',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/125923483@N06_r.jpg',
+  'avatar': 'https://farm6.staticflickr.com/5616/buddyicons/125923483@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/mountmanphoto/',
   'location': 'Rijeka, Croatia',
   'countries': 2,
