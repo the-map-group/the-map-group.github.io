73291f7545143db49df3ecb59b2919cae1b56cfa
The Map Group <admin@the-map-group.top>
2025-07-12 12:31:09 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/user.py
+++ people/147135779@N02/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '147135779@N02',
   'alias': '147135779@N02',
   'name': 'giovanni Campus',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/147135779@N02_r.jpg',
+  'avatar': 'https://farm5.staticflickr.com/4174/buddyicons/147135779@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/147135779@N02/',
   'location': 'Venturina, Italia',
   'countries': 19,
