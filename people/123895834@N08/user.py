447a40c0fb2979cb2c2f2a82a22abce0bc77686d
The Map Group <admin@the-map-group.top>
2025-07-12 13:12:16 -0300

    [auto] Updated map for member 'tquist24'

--- people/123895834@N08/user.py
+++ people/123895834@N08/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '123895834@N08',
   'alias': '123895834@N08',
   'name': 'tquist24',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/123895834@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/123895834@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/123895834@N08/',
   'location': 'United States',
   'countries': 12,
