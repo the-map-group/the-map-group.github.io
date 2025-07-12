d3dbdf39a574848f8d6a39c0d21aaa8d1ed1d091
The Map Group <admin@the-map-group.top>
2025-07-12 11:20:37 -0300

    [auto] Updated map for member 'Eugenio GV Costa'

--- people/eugeniocosta/user.py
+++ people/eugeniocosta/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '169904704@N04',
   'alias': 'eugeniocosta',
   'name': 'Eugenio Costa',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/169904704@N04_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/169904704@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/eugeniocosta/',
   'location': 'Livorno, Italy',
   'countries': 2,
