d325c189798d1fbe7602fdf6f695c17d038aea7d
The Map Group <admin@the-map-group.top>
2025-07-12 15:56:59 -0300

    [auto] Updated map for member 'Joe Bob Brim'

--- people/jbrimacombe/user.py
+++ people/jbrimacombe/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '192536922@N07',
   'alias': 'jbrimacombe',
   'name': 'Joseph Brimacombe',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/192536922@N07_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/192536922@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/jbrimacombe/',
   'location': '',
   'countries': 4,
