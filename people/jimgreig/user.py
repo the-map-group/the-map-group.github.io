a93bce124b223a80976f8854922d11232ac631b5
The Map Group <admin@the-map-group.top>
2025-07-12 12:31:39 -0300

    [auto] Updated map for member 'jamesgreig@dr.com'

--- people/jimgreig/user.py
+++ people/jimgreig/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '38286807@N03',
   'alias': 'jimgreig',
   'name': 'James Greig',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/38286807@N03_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/38286807@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/jimgreig/',
   'location': 'Leicestershire, England',
   'countries': 2,
