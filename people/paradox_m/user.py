c47437ecaa7cff90539b293d4d45365719030a3d
The Map Group <admin@the-map-group.top>
2025-07-12 15:38:10 -0300

    [auto] Updated map for member 'mark.paradox'

--- people/paradox_m/user.py
+++ people/paradox_m/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '98488245@N03',
   'alias': 'paradox_m',
   'name': 'Mark  PARADOX',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/98488245@N03_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7204/buddyicons/98488245@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/paradox_m/',
   'location': '',
   'countries': 2,
