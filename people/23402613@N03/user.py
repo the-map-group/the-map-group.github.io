d4c6398376a21638bef2518be4da04525fac1391
The Map Group <admin@the-map-group.top>
2025-07-12 15:09:56 -0300

    [auto] Updated map for member 'Joao de Mio'

--- people/23402613@N03/user.py
+++ people/23402613@N03/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '23402613@N03',
   'alias': '23402613@N03',
   'name': 'Joao de Mio',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23402613@N03_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/23402613@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/23402613@N03/',
   'location': 'Pesaro, Itália',
   'countries': 4,
