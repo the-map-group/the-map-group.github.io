983d5737cb0a20cf500db21bd7fa72ab2df7e282
The Map Group <admin@the-map-group.top>
2025-07-12 15:09:37 -0300

    [auto] Updated map for member 'Ludo's World'

--- people/ludomichel/countries.py
+++ people/ludomichel/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'VN': ['Vietnam', 385, 427],
-  'US': ['United States', 81, 85],
-  'FR': ['France', 25, 25],
   'BE': ['Belgium', 2339, 2581],
-  'TH': ['Thailand', 627, 722],
-  'KH': ['Cambodia', 432, 448],
-  'DO': ['Dominican Republic', 25, 27],
-  'LU': ['Luxembourg', 87, 87],
   'DE': ['Germany', 373, 413],
-  'NL': ['Netherlands', 51, 51]
+  'US': ['United States', 81, 85],
+  'NL': ['Netherlands', 51, 51],
+  'TH': ['Thailand', 627, 722],
+  'FR': ['France', 25, 25],
+  'LU': ['Luxembourg', 87, 87],
+  'KH': ['Cambodia', 432, 448],
+  'VN': ['Vietnam', 385, 427],
+  'DO': ['Dominican Republic', 25, 27]
 }
--- people/ludomichel/user.py
+++ people/ludomichel/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '124072682@N04',
   'alias': 'ludomichel',
   'name': 'Lцdо\/іс M',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/124072682@N04_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/124072682@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/ludomichel/',
   'location': 'Belgique',
   'countries': 10,
