9ac3118e75304b0b49ef2211749989bd69bcf6a4
The Map Group <admin@the-map-group.top>
2025-07-12 15:27:56 -0300

    [auto] Updated map for member 'MiroFedurco'

--- people/134373885@N03/countries.py
+++ people/134373885@N03/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'PL': ['Poland', 1, 1],
   'SK': ['Slovakia', 117, 161],
+  'CZ': ['Czech Republic', 23, 25],
   'RU': ['Russia', 51, 55],
   'DE': ['Germany', 23, 24],
+  'FR': ['France', 12, 15],
   'UA': ['Ukraine', 5, 6],
-  'CZ': ['Czech Republic', 23, 25],
-  'FR': ['France', 12, 15]
+  'PL': ['Poland', 1, 1]
 }
--- people/134373885@N03/user.py
+++ people/134373885@N03/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '134373885@N03',
   'alias': '134373885@N03',
   'name': 'Miroslav Fedurco',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/134373885@N03_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/134373885@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/134373885@N03/',
   'location': 'Slovakia',
   'countries': 7,
