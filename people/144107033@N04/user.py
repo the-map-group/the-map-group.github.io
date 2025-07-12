1dcf0f876d0918aaf03b89fa3d6a635e67639acc
The Map Group <admin@the-map-group.top>
2025-07-12 13:08:18 -0300

    [auto] Updated map for member 'sebastiánaguilar'

--- people/144107033@N04/user.py
+++ people/144107033@N04/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '144107033@N04',
   'alias': '144107033@N04',
   'name': 'Sebastián Aguilar',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/144107033@N04_r.jpg',
+  'avatar': 'https://farm6.staticflickr.com/5199/buddyicons/144107033@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/144107033@N04/',
   'location': 'Cádiz, España',
   'countries': 3,
