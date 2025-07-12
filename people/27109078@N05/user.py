c7d92858fca82bfa52ba5bd0f41537a5a6e45760
The Map Group <admin@the-map-group.top>
2025-07-12 10:43:14 -0300

    [auto] Updated map for member 'normafincher'

--- people/27109078@N05/user.py
+++ people/27109078@N05/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '27109078@N05',
   'alias': '27109078@N05',
   'name': 'Norma Fincher',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/27109078@N05_r.jpg',
+  'avatar': 'https://farm5.staticflickr.com/4631/buddyicons/27109078@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/27109078@N05/',
   'location': '',
   'countries': 27,
