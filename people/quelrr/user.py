d40031bd42164066c8ee6bbb3b54015a3d4c8e04
The Map Group <admin@the-map-group.top>
2025-07-12 11:20:47 -0300

    [auto] Updated map for member 'Quel.R.R'

--- people/quelrr/user.py
+++ people/quelrr/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '132838898@N03',
   'alias': 'quelrr',
   'name': 'Raquel Rampasio',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/132838898@N03_r.jpg',
+  'avatar': 'https://farm8.staticflickr.com/7919/buddyicons/132838898@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/quelrr/',
   'location': '',
   'countries': 6,
