e1688ac6f56086aaf9c9100c6cd4a634aafa11dc
The Map Group <admin@the-map-group.top>
2025-07-12 15:37:50 -0300

    [auto] Updated map for member 'Visavis..'

--- people/visavis/user.py
+++ people/visavis/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '90411943@N00',
   'alias': 'visavis',
   'name': 'Viktor Kirilko',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/90411943@N00_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/90411943@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/visavis/',
   'location': 'Kyiv, Ukraine',
   'countries': 22,
