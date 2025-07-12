e2892ff106087d5323adb1f389d2d5576c8c9fd9
The Map Group <admin@the-map-group.top>
2025-07-12 14:01:51 -0300

    [auto] Updated map for member 'S LYNN LEE'

--- people/slynnlee/countries.py
+++ people/slynnlee/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'NL': ['Netherlands', 29, 39],
-  'VA': ['Vatican City', 1, 1],
-  'GD': ['Grenada', 8, 9],
-  'IE': ['Ireland', 214, 966],
-  'GB': ['United Kingdom', 390, 1861],
-  'CU': ['Cuba', 1, 1],
-  'IT': ['Italy', 3, 3],
-  'AT': ['Austria', 3, 11],
-  'US': ['United States', 423, 2202],
-  'FR': ['France', 76, 107],
-  'RU': ['Russia', 2, 2],
+  'US': ['United States', 423, 2198],
+  'GB': ['United Kingdom', 390, 1859],
+  'IE': ['Ireland', 214, 964],
   'DE': ['Germany', 70, 286],
+  'IT': ['Italy', 3, 3],
+  'VA': ['Vatican City', 1, 1],
+  'RU': ['Russia', 2, 2],
+  'CU': ['Cuba', 1, 1],
+  'NL': ['Netherlands', 29, 39],
   'AU': ['Australia', 11, 13],
-  'PT': ['Portugal', 1, 2]
+  'AT': ['Austria', 3, 11],
+  'PT': ['Portugal', 1, 2],
+  'FR': ['France', 76, 107],
+  'GD': ['Grenada', 8, 9]
 }
