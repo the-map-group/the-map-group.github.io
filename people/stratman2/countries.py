ddc9de5c3b0c60de146520e41581e3ec6148c240
The Map Group <admin@the-map-group.top>
2025-07-13 09:28:14 -0300

    [auto] Updated map for member 'Stratman2'

--- people/stratman2/countries.py
+++ people/stratman2/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'MY': ['Malaysia', 476, 4376],
-  'SG': ['Singapore', 27, 200],
   'MW': ['Malawi', 1, 1],
+  'SG': ['Singapore', 27, 200],
   'ID': ['Indonesia', 12, 27]
 }
--- people/stratman2/user.py
+++ people/stratman2/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '8278193@N06',
   'alias': 'stratman2',
   'name': 'Stratman2 (taking care of Joey',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8278193@N06_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/8278193@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/stratman2/',
   'location': 'Kuala Lumpur, Malaysia',
   'countries': 4,
