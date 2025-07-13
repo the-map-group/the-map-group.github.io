e826815b86789cab960473004ac9047ae2c6510e
The Map Group <admin@the-map-group.top>
2025-07-13 09:54:49 -0300

    [auto] Updated map for member 'instagram: Alex_Premoli'

--- people/alex_premoli/user.py
+++ people/alex_premoli/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '154861655@N08',
   'alias': 'alex_premoli',
   'name': 'Alex Premoli',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/154861655@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/154861655@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/alex_premoli/',
   'location': '',
   'countries': 1,
