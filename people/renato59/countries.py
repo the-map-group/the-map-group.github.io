66a2a9d0f58d7f306802bd9ab6e65f7af3d3177b
The Map Group <admin@the-map-group.top>
2025-07-13 10:36:17 -0300

    [auto] Updated map for member 'ReNato '59'

--- people/renato59/countries.py
+++ people/renato59/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'AD': ['Andorra', 12, 21],
-  'IT': ['Italy', 13, 13],
   'ES': ['Spain', 445, 1590],
+  'IT': ['Italy', 13, 13],
+  'AD': ['Andorra', 12, 21],
   'FR': ['France', 4, 6]
 }
--- people/renato59/user.py
+++ people/renato59/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '87940223@N08',
   'alias': 'renato59',
   'name': 'ReNato \'59',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/87940223@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/87940223@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/renato59/',
   'location': 'Barcelona, Spain',
   'countries': 4,
