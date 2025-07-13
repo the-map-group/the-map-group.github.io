111db77dfaaa2b483682c4c1409f29c470e26df0
The Map Group <admin@the-map-group.top>
2025-07-13 06:50:58 -0300

    [auto] Updated map for member 'tinokaartovuori'

--- people/tinokaartovuori/user.py
+++ people/tinokaartovuori/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '99502057@N02',
   'alias': 'tinokaartovuori',
   'name': 'Tino Kaartovuori',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/99502057@N02_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/99502057@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/tinokaartovuori/',
   'location': 'Tampere, Finland',
   'countries': 3,
