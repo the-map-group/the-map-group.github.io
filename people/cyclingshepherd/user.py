44c67930b9f0ff938b7efa744372d7dd6990a41d
The Map Group <admin@the-map-group.top>
2025-07-13 06:39:25 -0300

    [auto] Updated map for member 'cyclingshepherd'

--- people/cyclingshepherd/user.py
+++ people/cyclingshepherd/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '59692385@N00',
   'alias': 'cyclingshepherd',
   'name': 'cyclingshepherd',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59692385@N00_r.jpg',
+  'avatar': 'https://farm6.staticflickr.com/5350/buddyicons/59692385@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/cyclingshepherd/',
   'location': '',
   'countries': 17,
