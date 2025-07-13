3fbbd79119bcbd24720316f9641d80fcc4515eec
The Map Group <admin@the-map-group.top>
2025-07-13 08:03:26 -0300

    [auto] Updated map for member 'Massimo Francesconi'

--- people/massimo_francesconi/user.py
+++ people/massimo_francesconi/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '63558969@N08',
   'alias': 'massimo_francesconi',
   'name': 'Massimo Francesconi',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/63558969@N08_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/63558969@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/massimo_francesconi/',
   'location': 'Bologna, Italia',
   'countries': 2,
