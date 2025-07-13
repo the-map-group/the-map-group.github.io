a4e48c200c6468f428647fe58c15f623dcc5d5f6
The Map Group <admin@the-map-group.top>
2025-07-13 09:38:00 -0300

    [auto] Updated map for member 'OMNIBUS DUBITANDUM'

--- people/max7777/countries.py
+++ people/max7777/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GB': ['United Kingdom', 1, 1],
-  'TJ': ['Tajikistan', 1, 1],
-  'CN': ['China', 1, 5],
-  'PK': ['Pakistan', 29, 40],
-  'IR': ['Iran', 4, 4],
-  'IN': ['India', 1, 1],
-  'IQ': ['Iraq', 1, 1],
-  'US': ['United States', 3, 4],
   'AF': ['Afghanistan', 20, 30],
+  'PK': ['Pakistan', 29, 40],
+  'IQ': ['Iraq', 1, 1],
+  'JO': ['Jordan', 1, 1],
   'EG': ['Egypt', 3, 3],
-  'JO': ['Jordan', 1, 1]
+  'GB': ['United Kingdom', 1, 1],
+  'IN': ['India', 1, 1],
+  'US': ['United States', 3, 4],
+  'TJ': ['Tajikistan', 1, 1],
+  'IR': ['Iran', 4, 4],
+  'CN': ['China', 1, 5]
 }
--- people/max7777/user.py
+++ people/max7777/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '30866147@N05',
   'alias': 'max7777',
   'name': 'OMNIBUS DUBITANDUM',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/30866147@N05_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/30866147@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/max7777/',
   'location': '',
   'countries': 11,
