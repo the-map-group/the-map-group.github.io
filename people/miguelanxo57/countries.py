7a8477c6918c6317b2e1c28a95c0474b59cafee5
The Map Group <admin@the-map-group.top>
2025-07-13 08:34:31 -0300

    [auto] Updated map for member 'Miguelanxo'

--- people/miguelanxo57/countries.py
+++ people/miguelanxo57/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'HU': ['Hungary', 3, 3],
-  'SK': ['Slovakia', 1, 1],
-  'PT': ['Portugal', 52, 58],
   'ES': ['Spain', 276, 311],
-  'DE': ['Germany', 4, 4],
-  'GB': ['United Kingdom', 29, 36],
-  'AT': ['Austria', 1, 1],
+  'PT': ['Portugal', 52, 58],
   'FR': ['France', 5, 5],
-  'CZ': ['Czech Republic', 4, 4]
+  'HU': ['Hungary', 3, 3],
+  'AT': ['Austria', 1, 1],
+  'SK': ['Slovakia', 1, 1],
+  'CZ': ['Czech Republic', 4, 4],
+  'DE': ['Germany', 4, 4],
+  'GB': ['United Kingdom', 29, 36]
 }
--- people/miguelanxo57/user.py
+++ people/miguelanxo57/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '59150926@N02',
   'alias': 'miguelanxo57',
   'name': 'Miguel RC',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59150926@N02_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/59150926@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/miguelanxo57/',
   'location': 'Santiago de Compostela, Spain',
   'countries': 9,
