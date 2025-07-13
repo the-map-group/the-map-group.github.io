9021031e124cc16576685fcb021bd2f54168917c
The Map Group <admin@the-map-group.top>
2025-07-13 10:41:47 -0300

    [auto] Updated map for member 'wa2wider'

--- people/wa2wider/countries.py
+++ people/wa2wider/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
   'CA': ['Canada', 202, 222],
-  'CH': ['Switzerland', 13, 13],
-  'US': ['United States', 10, 13]
+  'US': ['United States', 10, 13],
+  'CH': ['Switzerland', 13, 13]
 }
