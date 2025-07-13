52c73c07f77263086d27a4e9e2a3b97862567090
The Map Group <admin@the-map-group.top>
2025-07-13 07:05:55 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
   'location': 'Munich, Germany',
   'countries': 15,
-  'markers': 7493,
-  'photos': 35194
+  'markers': 7509,
+  'photos': 35190
 }
