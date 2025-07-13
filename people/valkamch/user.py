2344fdedbfad11b026db3206af6d20bfaa989742
The Map Group <admin@the-map-group.top>
2025-07-13 08:37:28 -0300

    [auto] Updated map for member 'ValKamch'

--- people/valkamch/user.py
+++ people/valkamch/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '143593890@N07',
   'alias': 'valkamch',
   'name': 'ValKamch',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/143593890@N07_r.jpg',
+  'avatar': 'https://farm5.staticflickr.com/4498/buddyicons/143593890@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/valkamch/',
   'location': 'Moscow region, Russia',
   'countries': 12,
