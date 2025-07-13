245c1f53151a87f39eaef667fbb7e1e3c191f8a4
The Map Group <admin@the-map-group.top>
2025-07-13 10:26:14 -0300

    [auto] Updated map for member 'enrico377'

--- people/enricoveneruso/user.py
+++ people/enricoveneruso/user.py
@@ -2,7 +2,7 @@ user_info = {
   'id': '193758259@N04',
   'alias': 'enricoveneruso',
   'name': 'Enrico Veneruso',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/193758259@N04_r.jpg',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/193758259@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/enricoveneruso/',
   'location': 'Napoli, Italia',
   'countries': 10,
