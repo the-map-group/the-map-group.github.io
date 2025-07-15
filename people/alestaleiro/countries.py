dcd924c0f01128fa434a8a333b73b0f56a9577d0
The Map Group <admin@the-map-group.top>
2025-07-15 03:36:05 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'BR': ['Brazil', 2582, 3655],
+  'BR': ['Brazil', 2583, 3657],
   'AR': ['Argentina', 218, 242],
   'CL': ['Chile', 26, 31],
   'PE': ['Peru', 14, 24],
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'location': 'Praia do Estaleiro - B. Camboriú, BRASIL',
   'countries': 26,
-  'markers': 3937,
-  'photos': 5370
+  'markers': 3938,
+  'photos': 5372
 }
