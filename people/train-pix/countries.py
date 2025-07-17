bffe422a5c24dc70b54e5fb4fa10e6deb6d9a8d3
The Map Group <admin@the-map-group.top>
2025-07-17 03:18:44 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/countries.py
+++ people/train-pix/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'GB': ['United Kingdom', 9178, 20832],
+  'GB': ['United Kingdom', 9178, 20834],
   'HR': ['Croatia', 5, 9],
   'DE': ['Germany', 375, 485],
   'AT': ['Austria', 71, 95],
