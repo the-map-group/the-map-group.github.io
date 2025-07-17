efa4e7ea2c1a1db7f7afbf3e6fb50bc9244f6e9a
The Map Group <admin@the-map-group.top>
2025-07-17 03:01:51 -0300

    [auto] Updated map for member 'michael_jeddah'

--- people/michael_from_berlin/user.py
+++ people/michael_from_berlin/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/michael_from_berlin/',
   'location': 'Berlin, Germany',
   'countries': 107,
-  'markers': 8286,
-  'photos': 8831
+  'markers': 8288,
+  'photos': 8833
 }
