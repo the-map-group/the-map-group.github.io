8c93275d69225343bef17423c1d8fe1f6c1a03b8
The Map Group <admin@the-map-group.top>
2025-07-19 03:10:35 -0300

    [auto] Updated map for member 'Harald Schulz'

--- people/121195437@N06/countries.py
+++ people/121195437@N06/countries.py
@@ -21,9 +21,9 @@ countries_dict = {
   'HU': ['Hungary', 5, 6],
   'HR': ['Croatia', 4, 11],
   'GR': ['Greece', 9, 13],
-  'SE': ['Sweden', 87, 96],
+  'SE': ['Sweden', 89, 98],
   'NL': ['Netherlands', 270, 321],
   'DK': ['Denmark', 67, 70],
   'GB': ['United Kingdom', 135, 170],
-  'FI': ['Finland', 55, 57]
+  'FI': ['Finland', 56, 58]
 }
