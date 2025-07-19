028f3bb231b165c4d05bfa6c0955ca467553bd37
The Map Group <admin@the-map-group.top>
2025-07-19 03:20:52 -0300

    [auto] Updated map for member 'Adam Swaine'

--- people/adam_swaine/countries.py
+++ people/adam_swaine/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'GB': ['United Kingdom', 17351, 25568],
+  'GB': ['United Kingdom', 17354, 25571],
   'FR': ['France', 154, 370],
   'MC': ['Monaco', 7, 16],
   'ES': ['Spain', 73, 104],
--- people/adam_swaine/user.py
+++ people/adam_swaine/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/adam_swaine/',
   'location': 'london, england',
   'countries': 5,
-  'markers': 17648,
-  'photos': 26127
+  'markers': 17651,
+  'photos': 26130
 }
