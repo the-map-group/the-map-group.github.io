c7178519032827e36c30a1a0cffc50ec9ccc2feb
The Map Group <admin@the-map-group.top>
2025-07-19 03:05:47 -0300

    [auto] Updated map for member '█ Slices of Light ✴ █▀ ▀ ▀'

--- people/justaslice/countries.py
+++ people/justaslice/countries.py
@@ -4,7 +4,7 @@ countries_dict = {
   'NL': ['Netherlands', 14, 18],
   'US': ['United States', 804, 1207],
   'CN': ['China', 357, 485],
-  'MY': ['Malaysia', 352, 483],
+  'MY': ['Malaysia', 353, 484],
   'JP': ['Japan', 47, 61],
   'VA': ['Vatican City', 141, 454],
   'IT': ['Italy', 903, 1423],
--- people/justaslice/user.py
+++ people/justaslice/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/justaslice/',
   'location': '',
   'countries': 30,
-  'markers': 3243,
-  'photos': 4961
+  'markers': 3244,
+  'photos': 4962
 }
