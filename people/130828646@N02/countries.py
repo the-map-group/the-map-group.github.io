af4cac57ea88871bac25116af427a1643d32d805
The Map Group <admin@the-map-group.top>
2025-07-20 03:03:49 -0300

    [auto] Updated map for member 'philippeguillot21'

--- people/130828646@N02/countries.py
+++ people/130828646@N02/countries.py
@@ -35,7 +35,7 @@ countries_dict = {
   'AU': ['Australia', 442, 644],
   'PT': ['Portugal', 768, 1266],
   'GB': ['United Kingdom', 762, 1246],
-  'UZ': ['Uzbekistan', 373, 979],
+  'UZ': ['Uzbekistan', 374, 1001],
   'IS': ['Iceland', 2, 2],
   'BE': ['Belgium', 1, 1]
 }
--- people/130828646@N02/user.py
+++ people/130828646@N02/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/130828646@N02/',
   'location': 'Entre-Deux, Réunion',
   'countries': 39,
-  'markers': 16435,
-  'photos': 42496
+  'markers': 16436,
+  'photos': 42518
 }
