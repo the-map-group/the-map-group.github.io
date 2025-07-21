a6a20fc5621ee817c02ac485146ff16f7ff63fb9
The Map Group <admin@the-map-group.top>
2025-07-21 03:29:13 -0300

    [auto] Updated map for member 'sarahstierch'

--- people/sarahvain/countries.py
+++ people/sarahvain/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'US': ['United States', 12838, 17228],
+  'US': ['United States', 12842, 17234],
   'IL': ['Israel', 9, 31],
   'NL': ['Netherlands', 8, 8],
   'CL': ['Chile', 37, 119],
--- people/sarahvain/user.py
+++ people/sarahvain/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/sarahvain/',
   'location': 'Sonoma, CA, USA',
   'countries': 14,
-  'markers': 13278,
-  'photos': 18367
+  'markers': 13282,
+  'photos': 18373
 }
