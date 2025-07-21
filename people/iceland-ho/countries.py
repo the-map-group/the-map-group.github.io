2b88eecbc6ea92edcb7da6203286dce44360b864
The Map Group <admin@the-map-group.top>
2025-07-21 03:07:06 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/countries.py
+++ people/iceland-ho/countries.py
@@ -16,5 +16,5 @@ countries_dict = {
   'GE': ['Georgia', 5, 5],
   'KR': ['South Korea', 13, 13],
   'RO': ['Romania', 4, 4],
-  'PT': ['Portugal', 16, 16]
+  'PT': ['Portugal', 17, 17]
 }
--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/iceland-ho/',
   'location': 'Reykjavík, Iceland',
   'countries': 18,
-  'markers': 3186,
-  'photos': 3189
+  'markers': 3187,
+  'photos': 3190
 }
