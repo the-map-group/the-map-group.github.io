c425bd4e58ad9df3054aad9a666fb2d360eda9e0
The Map Group <admin@the-map-group.top>
2025-07-21 03:05:39 -0300

    [auto] Updated map for member 'Black Diamond Images'

--- people/blackdiamondimages/countries.py
+++ people/blackdiamondimages/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'AU': ['Australia', 13082, 16096],
+  'AU': ['Australia', 13084, 16098],
   'NZ': ['New Zealand', 716, 892],
   'IT': ['Italy', 16, 16],
   'AL': ['Albania', 2, 2],
--- people/blackdiamondimages/user.py
+++ people/blackdiamondimages/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/blackdiamondimages/',
   'location': 'Mid North Coast, NSW, Australia',
   'countries': 36,
-  'markers': 17308,
-  'photos': 22227
+  'markers': 17310,
+  'photos': 22229
 }
