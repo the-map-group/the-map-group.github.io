09373524a3c46590686bdeedff299203ced5dd1b
The Map Group <admin@the-map-group.top>
2025-07-23 03:21:26 -0300

    [auto] Updated map for member 'Gaz 1984'

--- people/shabbagaz/countries.py
+++ people/shabbagaz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'ES': ['Spain', 21, 103],
-  'GB': ['United Kingdom', 12631, 19066],
+  'GB': ['United Kingdom', 12651, 19129],
   'DE': ['Germany', 25, 230],
   'IE': ['Ireland', 54, 58]
 }
--- people/shabbagaz/user.py
+++ people/shabbagaz/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/shabbagaz/',
   'location': 'Preston, England',
   'countries': 4,
-  'markers': 12812,
-  'photos': 19586
+  'markers': 12832,
+  'photos': 19649
 }
