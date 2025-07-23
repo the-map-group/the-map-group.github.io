d3774c9d9be0d365e3dcf2da1a0236920f78b8f6
The Map Group <admin@the-map-group.top>
2025-07-23 03:03:35 -0300

    [auto] Updated map for member 'Joseph Hollick'

--- people/31155442@N03/countries.py
+++ people/31155442@N03/countries.py
@@ -45,7 +45,7 @@ countries_dict = {
   'VA': ['Vatican City', 25, 31],
   'BE': ['Belgium', 60, 66],
   'VI': ['U.S. Virgin Islands', 8, 19],
-  'PR': ['Puerto Rico', 6, 10],
+  'PR': ['Puerto Rico', 11, 17],
   'BR': ['Brazil', 4, 5],
   'MQ': ['Martinique', 10, 12],
   'VE': ['Venezuela', 9, 11],
--- people/31155442@N03/user.py
+++ people/31155442@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/31155442@N03/',
   'location': 'Hamilton, Canada',
   'countries': 61,
-  'markers': 13817,
-  'photos': 20660
+  'markers': 13822,
+  'photos': 20667
 }
