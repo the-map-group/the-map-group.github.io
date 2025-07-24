8e68d1a6d240b23721fc607255e0a290e0e19ab9
The Map Group <admin@the-map-group.top>
2025-07-24 03:32:25 -0300

    [auto] Updated map for member 'kimbar/Thanks for 6.7 million views!'

--- people/kimbar/countries.py
+++ people/kimbar/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'GB': ['United Kingdom', 323, 412],
   'CH': ['Switzerland', 40, 55],
-  'US': ['United States', 3507, 4298],
+  'US': ['United States', 3510, 4301],
   'IT': ['Italy', 351, 531],
   'MX': ['Mexico', 263, 359],
   'ES': ['Spain', 151, 235],
--- people/kimbar/user.py
+++ people/kimbar/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/kimbar/',
   'location': 'Berkeley, CA, U.S.',
   'countries': 68,
-  'markers': 6595,
-  'photos': 8992
+  'markers': 6598,
+  'photos': 8995
 }
