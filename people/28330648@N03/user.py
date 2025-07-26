d71885969e8c9a21eb066f37dba73ccdb4e0007a
The Map Group <admin@the-map-group.top>
2025-07-26 10:37:03 -0300

    [auto] Updated map for member 'Florian106'

--- people/28330648@N03/user.py
+++ people/28330648@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/28330648@N03/',
   'location': 'Germany',
   'countries': 7,
-  'markers': 5361,
-  'photos': 6086
+  'markers': 5362,
+  'photos': 6087
 }
