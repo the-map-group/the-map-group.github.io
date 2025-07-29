eb0789e0be2dc96504279424471c185342dc9a6d
The Map Group <admin@the-map-group.top>
2025-07-29 03:31:18 -0300

    [auto] Updated map for member 'yves62160'

--- people/76410117@N04/countries.py
+++ people/76410117@N04/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'FR': ['France', 1804, 3292],
+  'FR': ['France', 1804, 3294],
   'AT': ['Austria', 17, 24],
   'GR': ['Greece', 36, 40],
-  'BE': ['Belgium', 150, 310],
+  'BE': ['Belgium', 150, 312],
   'LU': ['Luxembourg', 4, 53],
   'PT': ['Portugal', 9, 29]
 }
