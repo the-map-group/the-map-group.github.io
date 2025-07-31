37c670ab639ba258c09ff1def33ba59c0732f1ee
The Map Group <admin@the-map-group.top>
2025-07-31 03:08:57 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/countries.py
+++ people/diwan/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'DE': ['Germany', 5781, 6180],
   'GB': ['United Kingdom', 134, 137],
-  'ES': ['Spain', 448, 488],
+  'ES': ['Spain', 450, 490],
   'IT': ['Italy', 321, 339],
   'VA': ['Vatican City', 49, 55],
   'FI': ['Finland', 73, 75],
--- people/diwan/user.py
+++ people/diwan/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/diwan/',
   'location': 'Magdeburg, Germany',
   'countries': 24,
-  'markers': 8590,
-  'photos': 9119
+  'markers': 8592,
+  'photos': 9121
 }
