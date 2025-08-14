dc611c42329509118bfb73e092a81655d16dd96a
The Map Group <admin@the-map-group.top>
2025-08-14 03:06:14 -0300

    [auto] Updated map for member 'pascalvu1'

--- people/quanito/countries.py
+++ people/quanito/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'TH': ['Thailand', 11, 51],
-  'FR': ['France', 3058, 15455],
+  'FR': ['France', 3058, 15456],
   'LA': ['Laos', 5, 57],
   'KH': ['Cambodia', 1, 24],
   'IT': ['Italy', 391, 2046],
