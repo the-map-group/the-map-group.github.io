effc8e7a1aadc91689543508f9345f26df8f4e90
The Map Group <admin@the-map-group.top>
2025-08-24 03:34:11 -0300

    [auto] Updated map for member 'stepanov2876'

---
+++ people/145335276@N02/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '145335276@N02',
+  'alias': '145335276@N02',
+  'name': 'Pasha Vonapets',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/145335276@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/145335276@N02/',
+  'location': 'Moscow',
+  'countries': 1,
+  'markers': 39,
+  'photos': 182
+}
