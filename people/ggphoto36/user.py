1de43db9caf53902da9037ffef342e138475fa3c
The Map Group <admin@the-map-group.top>
2025-08-24 03:22:34 -0300

    [auto] Updated map for member 'The Gidinski'

--- people/ggphoto36/user.py
+++ people/ggphoto36/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/ggphoto36/',
   'location': 'Monmouth, Oregon, United States',
   'countries': 5,
-  'markers': 977,
-  'photos': 1024
+  'markers': 979,
+  'photos': 1026
 }
