c58feb0f72ad0072d85dbae89dc055baa73cc825
The Map Group <admin@the-map-group.top>
2025-08-24 03:10:27 -0300

    [auto] Updated map for member 'Harald Schulz'

--- people/121195437@N06/countries.py
+++ people/121195437@N06/countries.py
@@ -6,7 +6,7 @@ countries_dict = {
   'LV': ['Latvia', 2, 2],
   'EE': ['Estonia', 1, 1],
   'LT': ['Lithuania', 3, 4],
-  'DE': ['Germany', 1678, 2609],
+  'DE': ['Germany', 1679, 2610],
   'IT': ['Italy', 20, 37],
   'SM': ['San Marino', 1, 2],
   'MC': ['Monaco', 1, 1],
@@ -21,9 +21,9 @@ countries_dict = {
   'HU': ['Hungary', 5, 6],
   'HR': ['Croatia', 4, 11],
   'GR': ['Greece', 9, 13],
-  'SE': ['Sweden', 122, 133],
+  'SE': ['Sweden', 123, 134],
   'NL': ['Netherlands', 270, 321],
   'DK': ['Denmark', 67, 70],
   'GB': ['United Kingdom', 135, 170],
-  'FI': ['Finland', 88, 94]
+  'FI': ['Finland', 89, 95]
 }
--- people/121195437@N06/last_total.py
+++ people/121195437@N06/last_total.py
@@ -1,1 +1,1 @@
-number = 9332
+number = 9335
