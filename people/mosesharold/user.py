84f6c98593c64b0a89a83c08a134990f76edfcec
The Map Group <admin@the-map-group.top>
2025-08-30 03:01:23 -0300

    [auto] Updated map for member 'haroldmoses'

--- people/mosesharold/user.py
+++ people/mosesharold/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/mosesharold/',
   'location': 'SPENNYMOOR, United Kingdom',
   'countries': 76,
-  'markers': 3511,
-  'photos': 4832
+  'markers': 3513,
+  'photos': 4834
 }
