4c62aa25b4a294b13f8ade28d1c62c122e00f063
The Map Group <admin@the-map-group.top>
2025-08-31 03:30:38 -0300

    [auto] Updated map for member 'Joseph Hollick'

--- people/31155442@N03/countries.py
+++ people/31155442@N03/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'US': ['United States', 6527, 10542],
+  'US': ['United States', 6528, 10543],
   'AR': ['Argentina', 279, 473],
   'AQ': ['Antarctica', 67, 94],
-  'CA': ['Canada', 1758, 2539],
+  'CA': ['Canada', 1762, 2546],
   'IT': ['Italy', 269, 353],
   'ES': ['Spain', 215, 306],
   'AU': ['Australia', 207, 263],
