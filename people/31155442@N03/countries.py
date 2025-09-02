5e61684819d839d9ecc01e7949a87e4818e2e6e1
The Map Group <admin@the-map-group.top>
2025-09-02 03:46:31 -0300

    [auto] Updated map for member 'Joseph Hollick'

--- people/31155442@N03/countries.py
+++ people/31155442@N03/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'US': ['United States', 6528, 10543],
+  'US': ['United States', 6530, 10545],
   'AR': ['Argentina', 279, 473],
   'AQ': ['Antarctica', 67, 94],
-  'CA': ['Canada', 1762, 2552],
+  'CA': ['Canada', 1768, 2559],
   'IT': ['Italy', 269, 353],
   'ES': ['Spain', 215, 306],
   'AU': ['Australia', 207, 263],
--- people/31155442@N03/user.py
+++ people/31155442@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/31155442@N03/',
   'location': 'Hamilton, Canada',
   'countries': 61,
-  'markers': 13946,
-  'photos': 20872
+  'markers': 13954,
+  'photos': 20881
 }
