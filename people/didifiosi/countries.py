3fef2a95f2407e2dcb8c649ad1bb77068dbda810
The Map Group <admin@the-map-group.top>
2025-09-03 03:42:11 -0300

    [auto] Updated map for member 'jeanpaulneveu'

--- people/didifiosi/countries.py
+++ people/didifiosi/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'FR': ['France', 40, 1705]
+  'FR': ['France', 40, 1706]
 }
--- people/didifiosi/user.py
+++ people/didifiosi/user.py
@@ -7,5 +7,5 @@ user_info = {
   'location': 'Montelimar, France',
   'countries': 1,
   'markers': 41,
-  'photos': 1706
+  'photos': 1707
 }
