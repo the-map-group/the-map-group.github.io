f2a1bbb8f91670f1de50f48fe7cb691620af46f0
The Map Group <admin@the-map-group.top>
2025-09-11 03:41:46 -0300

    [auto] Updated map for member 'NiBe60'

--- people/162684392@N05/user.py
+++ people/162684392@N05/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/162684392@N05/',
   'location': 'Germany',
   'countries': 7,
-  'markers': 541,
-  'photos': 671
+  'markers': 542,
+  'photos': 672
 }
