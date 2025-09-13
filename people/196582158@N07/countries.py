43ffdd8850076e471437a857aeb3458aa543e11b
The Map Group <admin@the-map-group.top>
2025-09-13 04:20:27 -0300

    [auto] Updated map for member 'Seppi49'

---
+++ people/196582158@N07/countries.py
@@ -0,0 +1,10 @@
+countries_dict = {
+  'DE': ['Germany', 22, 22],
+  'CH': ['Switzerland', 388, 499],
+  'FR': ['France', 8, 8],
+  'NL': ['Netherlands', 7, 7],
+  'NO': ['Norway', 2, 2],
+  'ES': ['Spain', 12, 14],
+  'IT': ['Italy', 26, 31],
+  'SE': ['Sweden', 1, 1]
+}
