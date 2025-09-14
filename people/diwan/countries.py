a890af4f72b47823c9f4d902f3cf763cbb28b437
The Map Group <admin@the-map-group.top>
2025-09-14 03:44:04 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/countries.py
+++ people/diwan/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'DE': ['Germany', 5794, 6198],
+  'DE': ['Germany', 5795, 6199],
   'GB': ['United Kingdom', 134, 137],
   'ES': ['Spain', 466, 508],
   'IT': ['Italy', 321, 339],
