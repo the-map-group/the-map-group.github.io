60f0d4489c41e9680a3198465c25ce4bc0ed9917
The Map Group <admin@the-map-group.top>
2025-09-17 03:33:57 -0300

    [auto] Updated map for member 'Liam Cheasty'

--- people/liamcheasty/countries.py
+++ people/liamcheasty/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'IE': ['Ireland', 713, 6089],
+  'IE': ['Ireland', 714, 6090],
   'FR': ['France', 405, 1752],
   'DE': ['Germany', 374, 1543],
   'ZA': ['South Africa', 8, 97],
--- people/liamcheasty/user.py
+++ people/liamcheasty/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/liamcheasty/',
   'location': 'Waterford, Ireland',
   'countries': 29,
-  'markers': 3632,
-  'photos': 18282
+  'markers': 3633,
+  'photos': 18283
 }
