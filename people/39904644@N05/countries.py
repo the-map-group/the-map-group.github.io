ff1416ebd30f1c0ae5dcb23158428c485b2d0c99
The Map Group <admin@the-map-group.top>
2025-09-24 03:22:36 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/countries.py
+++ people/39904644@N05/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'IT': ['Italy', 85, 104],
   'DK': ['Denmark', 20, 22],
-  'DE': ['Germany', 362, 398],
+  'DE': ['Germany', 363, 399],
   'NZ': ['New Zealand', 285, 373],
   'HR': ['Croatia', 1, 1],
   'FR': ['France', 3, 3],
--- people/39904644@N05/user.py
+++ people/39904644@N05/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/39904644@N05/',
   'location': 'digitale Suppe, Deutschland',
   'countries': 18,
-  'markers': 1785,
-  'photos': 2080
+  'markers': 1786,
+  'photos': 2081
 }
