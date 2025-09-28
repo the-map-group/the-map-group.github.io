99068e0427657635b245fb0fac9138e12948c7f2
The Map Group <admin@the-map-group.top>
2025-09-28 03:27:00 -0300

    [auto] Updated map for member 'Gabriel Paladino Ibáñez'

--- people/gabouruguay-collection/user.py
+++ people/gabouruguay-collection/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/gabouruguay-collection/',
   'location': '',
   'countries': 10,
-  'markers': 916,
-  'photos': 3063
+  'markers': 917,
+  'photos': 3064
 }
