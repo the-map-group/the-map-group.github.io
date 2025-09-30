60d910834f9ea13f77a70222eda2f187590481cc
The Map Group <admin@the-map-group.top>
2025-09-30 03:51:32 -0300

    [auto] Updated map for member 'koothenholly'

---
+++ people/26975404@N06/countries.py
@@ -0,0 +1,15 @@
+countries_dict = {
+  'SI': ['Slovenia', 5, 25],
+  'AT': ['Austria', 2, 4],
+  'GB': ['United Kingdom', 1244, 1858],
+  'FR': ['France', 5, 8],
+  'PL': ['Poland', 2, 3],
+  'FI': ['Finland', 3, 7],
+  'ES': ['Spain', 7, 7],
+  'CH': ['Switzerland', 27, 35],
+  'IE': ['Ireland', 1, 2],
+  'GR': ['Greece', 4, 4],
+  'GG': ['Guernsey', 1, 1],
+  'LV': ['Latvia', 10, 13],
+  'PT': ['Portugal', 14, 16]
+}
---
+++ people/26975404@N06/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '26975404@N06',
+  'alias': '26975404@N06',
+  'name': 'Chris',
+  'avatar': 'https://farm5.staticflickr.com/4492/buddyicons/26975404@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/26975404@N06/',
+  'location': '',
+  'countries': 13,
+  'markers': 1325,
+  'photos': 1983
+}
