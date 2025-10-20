a8da657dd580475d43540b458fc1db66ef0daf9e
The Map Group <admin@the-map-group.top>
2025-10-20 03:33:30 -0300

    [auto] Updated map for member 'Gaz 1984'

--- people/shabbagaz/last_total.py
+++ people/shabbagaz/last_total.py
@@ -1,1 +1,1 @@
-number = 20352
+number = 20413
--- people/shabbagaz/user.py
+++ people/shabbagaz/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/shabbagaz/',
   'location': 'Preston, England',
   'countries': 4,
-  'markers': 12926,
-  'photos': 20341
+  'markers': 12927,
+  'photos': 20402
 }
