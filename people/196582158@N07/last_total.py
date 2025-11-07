b2a914d96c21d2154af1abdd9a1ad07b43871349
The Map Group <admin@the-map-group.top>
2025-11-07 18:32:05 -0300

    [auto] Updated map for member 'Seppi49'

--- people/196582158@N07/last_total.py
+++ people/196582158@N07/last_total.py
@@ -1,1 +1,1 @@
-number = 3943
+number = 3990
--- people/196582158@N07/user.py
+++ people/196582158@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/196582158@N07/',
   'location': 'Mümliswil, Schweiz/Switzerland',
   'countries': 8,
-  'markers': 494,
-  'photos': 619
+  'markers': 527,
+  'photos': 655
 }
