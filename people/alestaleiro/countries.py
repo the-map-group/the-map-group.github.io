a7b9f0f6ba4c8c9636a05e7c9764a590e2a4e12e
The Map Group <admin@the-map-group.top>
2025-11-11 03:30:41 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -12,7 +12,7 @@ countries_dict = {
   'ZA': ['South Africa', 131, 168],
   'EG': ['Egypt', 119, 157],
   'AE': ['United Arab Emirates', 27, 40],
-  'GB': ['United Kingdom', 89, 112],
+  'GB': ['United Kingdom', 90, 114],
   'ES': ['Spain', 242, 319],
   'MA': ['Morocco', 96, 135],
   'PT': ['Portugal', 84, 97],
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'location': 'Praia do Estaleiro - B. Camboriú, BRASIL',
   'countries': 27,
-  'markers': 4328,
-  'photos': 5833
+  'markers': 4329,
+  'photos': 5835
 }
