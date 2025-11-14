a907172886ae5dc2e25f39f6187d54fd941e00c8
The Map Group <admin@the-map-group.top>
2025-11-14 03:52:23 -0300

    [auto] Updated map for member 'Cranky Oldman'

---
+++ people/39442644@N03/countries.py
@@ -0,0 +1,18 @@
+countries_dict = {
+  'ES': ['Spain', 333, 360],
+  'FI': ['Finland', 332, 339],
+  'FR': ['France', 140, 156],
+  'US': ['United States', 837, 886],
+  'CZ': ['Czech Republic', 5, 5],
+  'DE': ['Germany', 178, 189],
+  'CU': ['Cuba', 4, 4],
+  'BE': ['Belgium', 8, 9],
+  'CN': ['China', 1, 1],
+  'PL': ['Poland', 4, 5],
+  'NO': ['Norway', 10, 10],
+  'CA': ['Canada', 13, 13],
+  'SE': ['Sweden', 47, 47],
+  'IT': ['Italy', 1, 1],
+  'LU': ['Luxembourg', 3, 3],
+  'DK': ['Denmark', 3, 3]
+}
---
+++ people/39442644@N03/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '39442644@N03',
+  'alias': '39442644@N03',
+  'name': 'Cranky Oldman',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/39442644@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/39442644@N03/',
+  'location': 'Espoo, Finland / Suomi',
+  'countries': 16,
+  'markers': 1919,
+  'photos': 2031
+}
