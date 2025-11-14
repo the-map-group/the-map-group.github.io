3e04dd556337bf9047802eaedfb613b16345c2d3
The Map Group <admin@the-map-group.top>
2025-11-14 03:52:40 -0300

    [auto] Updated map for member 'datsamkong'

---
+++ people/samkong33/countries.py
@@ -0,0 +1,3 @@
+countries_dict = {
+  'US': ['United States', 57, 58]
+}
---
+++ people/samkong33/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '199019123@N05',
+  'alias': 'samkong33',
+  'name': 'Sam Kong',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/199019123@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/samkong33/',
+  'location': 'Madison, USA',
+  'countries': 1,
+  'markers': 57,
+  'photos': 58
+}
