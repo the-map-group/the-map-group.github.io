a61301d2cc5893a29ca39a9f21067512f02a14bb
The Map Group <admin@the-map-group.top>
2025-11-18 03:38:10 -0300

    [auto] Updated map for member 'Gérard Cachon'

--- people/bennett-cachon/countries.py
+++ people/bennett-cachon/countries.py
@@ -3,5 +3,5 @@ countries_dict = {
   'US': ['United States', 40, 54],
   'CA': ['Canada', 5, 7],
   'CL': ['Chile', 7, 12],
-  'GB': ['United Kingdom', 4, 4]
+  'GB': ['United Kingdom', 5, 5]
 }
--- people/bennett-cachon/user.py
+++ people/bennett-cachon/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/bennett-cachon/',
   'location': '',
   'countries': 5,
-  'markers': 118,
-  'photos': 177
+  'markers': 119,
+  'photos': 178
 }
