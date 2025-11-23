f5a8a1104cf99b738ed9e5690e57865b547b3867
The Map Group <admin@the-map-group.top>
2025-11-23 04:28:56 -0300

    [auto] Updated map for member 'gabrielrodriguesbezerra3'

---
+++ people/194892858@N03/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '194892858@N03',
+  'alias': '194892858@N03',
+  'name': 'Gabriel Bezerra',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/194892858@N03/',
+  'location': '',
+  'countries': 0,
+  'markers': 0,
+  'photos': 0
+}
