0deecf9db35f7c6993536dce948a56027fddb637
The Map Group <admin@the-map-group.top>
2025-11-23 04:28:45 -0300

    [auto] Updated map for member 'benlarhome'

---
+++ people/benandlarry/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '52909585@N02',
+  'alias': 'benandlarry',
+  'name': 'Ben',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/52909585@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/benandlarry/',
+  'location': 'Canada',
+  'countries': 33,
+  'markers': 10415,
+  'photos': 50510
+}
