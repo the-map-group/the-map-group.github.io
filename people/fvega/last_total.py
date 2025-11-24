14d1bfe731abc5a53c66809e53290171cad6e3a5
The Map Group <admin@the-map-group.top>
2025-11-24 03:16:40 -0300

    [auto] Updated map for member 'Fernando Sa Rapita'

--- people/fvega/last_total.py
+++ people/fvega/last_total.py
@@ -1,1 +1,1 @@
-number = 5732
+number = 5735
--- people/fvega/user.py
+++ people/fvega/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/fvega/',
   'location': 'Sa Rapita, Spain',
   'countries': 10,
-  'markers': 3672,
-  'photos': 5726
+  'markers': 3675,
+  'photos': 5729
 }
