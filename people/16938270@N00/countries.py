9919376dfb6329c292a7b85364994ac90a7fbc8c
The Map Group <admin@the-map-group.top>
2025-11-30 03:36:55 -0300

    [auto] Updated map for member 'Ian & Marg'

--- people/16938270@N00/countries.py
+++ people/16938270@N00/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'AU': ['Australia', 546, 592],
-  'TO': ['Tonga', 13, 13],
+  'TO': ['Tonga', 17, 17],
   'CN': ['China', 3, 3],
   'US': ['United States', 12, 12],
   'ID': ['Indonesia', 58, 67],
