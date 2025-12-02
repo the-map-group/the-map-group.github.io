33ea60812e281967f212b415d77ff5e541f7854b
The Map Group <admin@the-map-group.top>
2025-12-02 03:41:02 -0300

    [auto] Updated map for member 'dabowers50'

---
+++ people/201620502@N04/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '201620502@N04',
+  'alias': '201620502@N04',
+  'name': 'Darcy Bowers',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/201620502@N04/',
+  'location': '',
+  'countries': 0,
+  'markers': 0,
+  'photos': 0
+}
