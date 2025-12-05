0a1f973c7899a439b6c317190b3ed99489f72459
The Map Group <admin@the-map-group.top>
2025-12-05 03:37:42 -0300

    [auto] Updated map for member 'D-Stanley'

--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
   'location': 'Nanaimo, Canada',
   'countries': 129,
-  'markers': 8351,
-  'photos': 9748
+  'markers': 8354,
+  'photos': 9753
 }
