b92cf69aefe2f9c3dc411b9a4f8cc1eddaf98a4e
The Map Group <admin@the-map-group.top>
2025-12-12 03:26:17 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/countries.py
+++ people/153072396@N07/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'GB': ['United Kingdom', 52, 85],
-  'FR': ['France', 95, 352],
+  'FR': ['France', 96, 353],
   'DK': ['Denmark', 58, 95],
   'IE': ['Ireland', 22, 29],
   'BE': ['Belgium', 1, 1],
--- people/153072396@N07/last_total.py
+++ people/153072396@N07/last_total.py
@@ -1,1 +1,1 @@
-number = 681
+number = 684
