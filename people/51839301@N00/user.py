3513895731beb6f78b1c52d7ba6b7668d77c2435
The Map Group <admin@the-map-group.top>
2025-12-12 03:00:51 -0300

    [auto] Updated map for member 'hans pohl'

--- people/51839301@N00/user.py
+++ people/51839301@N00/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/51839301@N00/',
   'location': '',
   'countries': 12,
-  'markers': 3928,
-  'photos': 6025
+  'markers': 3929,
+  'photos': 6026
 }
