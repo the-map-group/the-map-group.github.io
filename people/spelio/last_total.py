4cf215da66c62a2c7f2c7425574ed5cc7e9fc791
The Map Group <admin@the-map-group.top>
2025-12-13 03:41:55 -0300

    [auto] Updated map for member 'spelio'

--- people/spelio/last_total.py
+++ people/spelio/last_total.py
@@ -1,1 +1,1 @@
-number = 55413
+number = 55430
--- people/spelio/user.py
+++ people/spelio/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/spelio/',
   'location': '',
   'countries': 7,
-  'markers': 4462,
-  'photos': 5870
+  'markers': 4472,
+  'photos': 5880
 }
