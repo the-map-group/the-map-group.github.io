bf3f43c78b1047f5b717afc1f6a7b768f047c8b0
The Map Group <admin@the-map-group.top>
2025-12-22 03:43:17 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/user.py
+++ people/153072396@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/153072396@N07/',
   'location': 'France',
   'countries': 11,
-  'markers': 293,
-  'photos': 665
+  'markers': 291,
+  'photos': 664
 }
