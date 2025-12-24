7077e8cc36d09e2c88b68cc55aacf6e9ecf21782
The Map Group <admin@the-map-group.top>
2025-12-24 03:42:30 -0300

    [auto] Updated map for member 'benlarhome'

--- people/benandlarry/countries.py
+++ people/benandlarry/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'CA': ['Canada', 8863, 43910],
+  'CA': ['Canada', 8865, 43914],
   'CN': ['China', 16, 16],
   'AU': ['Australia', 2, 2],
   'US': ['United States', 1275, 5229],
