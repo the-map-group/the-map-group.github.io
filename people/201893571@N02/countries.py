42998c8692b34c7abe5cc5afb7791416aaa7cefa
The Map Group <admin@the-map-group.top>
2025-12-30 03:30:35 -0300

    [auto] Updated map for member 'dccwoodward'

---
+++ people/201893571@N02/countries.py
@@ -0,0 +1,14 @@
+countries_dict = {
+  'BS': ['Bahamas', 11, 15],
+  'US': ['United States', 42, 59],
+  'IE': ['Ireland', 8, 8],
+  'CZ': ['Czech Republic', 5, 7],
+  'SK': ['Slovakia', 3, 4],
+  'MA': ['Morocco', 2, 6],
+  'HU': ['Hungary', 6, 8],
+  'AT': ['Austria', 6, 9],
+  'DE': ['Germany', 3, 5],
+  'AD': ['Andorra', 3, 6],
+  'ES': ['Spain', 5, 8],
+  'PT': ['Portugal', 2, 5]
+}
---
+++ people/201893571@N02/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '201893571@N02',
+  'alias': '201893571@N02',
+  'name': 'Dominic Woodward',
+  'avatar': 'https://raw.githubusercontent.com/the-map-group/the-map-group.github.io/refs/heads/main/icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/201893571@N02/',
+  'location': '',
+  'countries': 12,
+  'markers': 96,
+  'photos': 140
+}
