2f380f8a22cbc15b20d53f9d922c64f910c5a670
The Map Group <admin@the-map-group.top>
2026-02-07 04:29:03 -0300

    [auto] Updated map for member 'kpmst7'

---
+++ people/kpmst7/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '8574514@N02',
+  'alias': 'kpmst7',
+  'name': 'kpmst7',
+  'avatar': 'https://farm8.staticflickr.com/7412/buddyicons/8574514@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/kpmst7/',
+  'location': '',
+  'countries': 60,
+  'markers': 3803,
+  'photos': 6351
+}
