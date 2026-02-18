fc20e7ad5bf19c374feac946e3e7ffa8895d9c37
The Map Group <admin@the-map-group.top>
2026-02-18 03:30:47 -0300

    [auto] Updated map for member 'Doug McGr'

---
+++ people/pixscapes/countries.py
@@ -0,0 +1,8 @@
+countries_dict = {
+  'CA': ['Canada', 12, 12],
+  'US': ['United States', 18, 23],
+  'CU': ['Cuba', 5, 8],
+  'GB': ['United Kingdom', 4, 7],
+  'FR': ['France', 1, 1],
+  'DE': ['Germany', 2, 2]
+}
---
+++ people/pixscapes/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '10442345@N07',
+  'alias': 'pixscapes',
+  'name': 'Doug McGr',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/10442345@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/pixscapes/',
+  'location': '',
+  'countries': 6,
+  'markers': 43,
+  'photos': 54
+}
