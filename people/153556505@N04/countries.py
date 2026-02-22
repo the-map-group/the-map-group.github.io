b32c037bcf0c4c3f75c4354712c1c470ae3a0e35
The Map Group <admin@the-map-group.top>
2026-02-22 03:35:34 -0300

    [auto] Updated map for member 'Andrea48Blz on/off'

---
+++ people/153556505@N04/countries.py
@@ -0,0 +1,12 @@
+countries_dict = {
+  'IT': ['Italy', 81, 88],
+  'GR': ['Greece', 16, 17],
+  'FR': ['France', 4, 4],
+  'HR': ['Croatia', 4, 4],
+  'IL': ['Israel', 1, 1],
+  'ME': ['Montenegro', 1, 1],
+  'CY': ['Cyprus', 4, 5],
+  'TY': ['Northern Cyprus', 1, 1],
+  'CH': ['Switzerland', 4, 4],
+  'AT': ['Austria', 1, 1]
+}
---
+++ people/153556505@N04/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '153556505@N04',
+  'alias': '153556505@N04',
+  'name': 'Andrea48Blz on/off',
+  'avatar': 'https://farm66.staticflickr.com/65535/buddyicons/153556505@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/153556505@N04/',
+  'location': '',
+  'countries': 10,
+  'markers': 117,
+  'photos': 126
+}
