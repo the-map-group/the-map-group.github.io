885c1d1ed4edea566b1bf2ab04179ba9824ccfd1
The Map Group <admin@the-map-group.top>
2026-02-23 03:26:24 -0300

    [auto] Updated map for member 'Urban Florida Photographer'

--- people/miamiboy/countries.py
+++ people/miamiboy/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'PE': ['Peru', 7, 12],
-  'US': ['United States', 8764, 13693],
+  'US': ['United States', 8768, 13697],
   'GT': ['Guatemala', 15, 23],
   'ES': ['Spain', 17, 32],
   'CA': ['Canada', 6, 13],
--- people/miamiboy/user.py
+++ people/miamiboy/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/miamiboy/',
   'location': '',
   'countries': 26,
-  'markers': 8951,
-  'photos': 14132
+  'markers': 8955,
+  'photos': 14136
 }
