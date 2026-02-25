20375ef66e30e0cf678d12fa0fddb6510d0fd28f
The Map Group <admin@the-map-group.top>
2026-02-25 03:28:34 -0300

    [auto] Updated map for member 'dccwoodward'

--- people/201893571@N02/countries.py
+++ people/201893571@N02/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'BS': ['Bahamas', 9, 10],
-  'US': ['United States', 46, 58],
+  'BS': ['Bahamas', 8, 9],
+  'US': ['United States', 45, 57],
   'CA': ['Canada', 1, 1],
   'IE': ['Ireland', 8, 9],
   'CZ': ['Czech Republic', 5, 7],
@@ -12,5 +12,5 @@ countries_dict = {
   'AD': ['Andorra', 3, 6],
   'ES': ['Spain', 5, 8],
   'PT': ['Portugal', 2, 5],
-  'BE': ['Belgium', 4, 4]
+  'BE': ['Belgium', 5, 5]
 }
