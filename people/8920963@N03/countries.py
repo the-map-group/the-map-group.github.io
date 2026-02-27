09134bb82f3e24839e37acd4f812948a02570958
The Map Group <admin@the-map-group.top>
2026-02-27 03:01:10 -0300

    [auto] Updated map for member 'Steenjep'

--- people/8920963@N03/countries.py
+++ people/8920963@N03/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'DK': ['Denmark', 7153, 10897],
+  'DK': ['Denmark', 7156, 10900],
   'CY': ['Cyprus', 411, 481],
   'NL': ['Netherlands', 6, 6],
   'TY': ['Northern Cyprus', 10, 10],
--- people/8920963@N03/last_total.py
+++ people/8920963@N03/last_total.py
@@ -1,1 +1,1 @@
-number = 18126
+number = 18129
