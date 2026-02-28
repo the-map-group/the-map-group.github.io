6a0859e7ea6a5e09e5a3792cbc5a1fd42166387d
The Map Group <admin@the-map-group.top>
2026-02-28 03:27:44 -0300

    [auto] Updated map for member 'Urban Florida Photographer'

--- people/miamiboy/user.py
+++ people/miamiboy/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/miamiboy/',
   'location': '',
   'countries': 26,
-  'markers': 8968,
-  'photos': 14149
+  'markers': 8973,
+  'photos': 14154
 }
