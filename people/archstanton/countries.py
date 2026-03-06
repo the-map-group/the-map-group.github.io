954bb6dfddddcfa9e9b71e19350b0433887e74ad
The Map Group <admin@the-map-group.top>
2026-03-06 03:38:28 -0300

    [auto] Updated map for member '- Ozymandias -'

--- people/archstanton/countries.py
+++ people/archstanton/countries.py
@@ -28,7 +28,7 @@ countries_dict = {
   'RS': ['Serbia', 1300, 1401],
   'NL': ['Netherlands', 1, 4],
   'LU': ['Luxembourg', 76, 79],
-  'KR': ['South Korea', 4918, 5457],
+  'KR': ['South Korea', 4946, 5488],
   'CN': ['China', 13, 13],
   'RU': ['Russia', 3, 3],
   'EG': ['Egypt', 11, 11]
--- people/archstanton/last_total.py
+++ people/archstanton/last_total.py
@@ -1,1 +1,1 @@
-number = 47625
+number = 47656
