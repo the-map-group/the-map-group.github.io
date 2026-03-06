ef0560ba4d55533afed1e5e0e20df9299e13beea
The Map Group <admin@the-map-group.top>
2026-03-06 03:54:06 -0300

    [auto] Updated map for member 'benlarhome'

--- people/benandlarry/last_total.py
+++ people/benandlarry/last_total.py
@@ -1,1 +1,1 @@
-number = 57169
+number = 57174
--- people/benandlarry/user.py
+++ people/benandlarry/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/benandlarry/',
   'location': 'Canada',
   'countries': 33,
-  'markers': 10609,
-  'photos': 50818
+  'markers': 10612,
+  'photos': 50823
 }
