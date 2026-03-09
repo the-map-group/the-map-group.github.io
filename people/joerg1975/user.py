75211f384df1dffccf31af8e153048bd919cdeb2
The Map Group <admin@the-map-group.top>
2026-03-09 05:10:38 -0300

    [auto] Updated map for member 'Joerg1975'

---
+++ people/joerg1975/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '12533858@N04',
+  'alias': 'joerg1975',
+  'name': 'Joersch',
+  'avatar': 'https://farm6.staticflickr.com/5643/buddyicons/12533858@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/joerg1975/',
+  'location': 'Germany',
+  'countries': 42,
+  'markers': 3644,
+  'photos': 8009
+}
