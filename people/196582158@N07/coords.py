2b65677c14a90af79abdc3b7e2c5e0b10735cbc9
The Map Group <admin@the-map-group.top>
2026-03-11 03:28:26 -0300

    [auto] Updated map for member 'Seppi49'

--- people/196582158@N07/coords.py
+++ people/196582158@N07/coords.py
@@ -608,5 +608,6 @@ coords_dict = {
   '46.793177,8.457509': ['CH', 'Switzerland'],
   '46.547203,9.901438': ['CH', 'Switzerland'],
   '46.547084,9.900934': ['CH', 'Switzerland'],
-  '46.431719,9.753885': ['CH', 'Switzerland']
+  '46.431719,9.753885': ['CH', 'Switzerland'],
+  '47.200847,7.483047': ['CH', 'Switzerland']
 }
