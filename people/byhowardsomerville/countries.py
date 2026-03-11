8801f98ce7086b31a3e714be9ee9c25b21a68ff6
The Map Group <admin@the-map-group.top>
2026-03-11 03:04:55 -0300

    [auto] Updated map for member 'Howard Somerville (11 million views)'

--- people/byhowardsomerville/countries.py
+++ people/byhowardsomerville/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'GB': ['United Kingdom', 720, 784],
+  'GB': ['United Kingdom', 721, 785],
   'PF': ['French Polynesia', 2, 2],
   'ES': ['Spain', 40, 42],
   'PT': ['Portugal', 7, 8],
--- people/byhowardsomerville/user.py
+++ people/byhowardsomerville/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/byhowardsomerville/',
   'location': 'London, UK',
   'countries': 38,
-  'markers': 1052,
-  'photos': 1240
+  'markers': 1053,
+  'photos': 1241
 }
