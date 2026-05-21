d4ff1b5f48c1a865bf6fe8b0c3e24c508e432591
The Map Group <admin@the-map-group.top>
2026-05-21 03:26:30 -0300

    [auto] Updated map for member 'I saw_that'

--- people/saw_that/countries.py
+++ people/saw_that/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'US': ['United States', 346, 716],
   'IT': ['Italy', 39, 48],
-  'CA': ['Canada', 933, 1914],
+  'CA': ['Canada', 933, 1915],
   'GB': ['United Kingdom', 19, 23],
   'FR': ['France', 14, 19],
   'DE': ['Germany', 3, 4],
