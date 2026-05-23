4a04896c27ea3140ed1f07e155d723cec76c4d11
The Map Group <admin@the-map-group.top>
2026-05-23 03:27:32 -0300

    [auto] Updated map for member 'Cranky Oldman'

--- people/39442644@N03/last_total.py
+++ people/39442644@N03/last_total.py
@@ -1,1 +1,1 @@
-number = 2609
+number = 2612
--- people/39442644@N03/user.py
+++ people/39442644@N03/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/39442644@N03/',
   'location': 'Espoo, Finland / Suomi',
   'countries': 16,
-  'markers': 2054,
-  'photos': 2170
+  'markers': 2057,
+  'photos': 2173
 }
