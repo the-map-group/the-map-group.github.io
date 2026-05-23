854021b9290a090b1863600b8e237be6ab1ae4cd
The Map Group <admin@the-map-group.top>
2026-05-23 03:15:58 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'IT': ['Italy', 3893, 5367],
-  'DE': ['Germany', 3145, 30381],
+  'DE': ['Germany', 3148, 30385],
   'HR': ['Croatia', 214, 244],
   'GE': ['Georgia', 155, 176],
   'AM': ['Armenia', 159, 170],
--- people/jenskoschitzki/last_total.py
+++ people/jenskoschitzki/last_total.py
@@ -1,1 +1,1 @@
-number = 47303
+number = 47307
