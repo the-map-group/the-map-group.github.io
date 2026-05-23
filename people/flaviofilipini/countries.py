263ad4a0bc8400259a08ce80c3d87e816a2955af
The Map Group <admin@the-map-group.top>
2026-05-23 03:28:53 -0300

    [auto] Updated map for member 'Flávio Filipini'

---
+++ people/flaviofilipini/countries.py
@@ -0,0 +1,6 @@
+countries_dict = {
+  'BR': ['Brazil', 90, 106],
+  'PT': ['Portugal', 2, 2],
+  'US': ['United States', 4, 4],
+  'ES': ['Spain', 1, 1]
+}
---
+++ people/flaviofilipini/user.py
@@ -0,0 +1,11 @@
+user_info = {
+  'id': '8099310@N07',
+  'alias': 'flaviofilipini',
+  'name': 'Flavio Filipini',
+  'avatar': 'https://farm9.staticflickr.com/8394/buddyicons/8099310@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/flaviofilipini/',
+  'location': 'São Paulo',
+  'countries': 4,
+  'markers': 97,
+  'photos': 113
+}
