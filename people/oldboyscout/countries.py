b943a552efd36ad992ce1cce12e94caa99ea5749
The Map Group <admin@the-map-group.top>
2026-06-02 03:14:35 -0300

    [auto] Updated map for member 'gsegelken'

--- people/oldboyscout/countries.py
+++ people/oldboyscout/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'US': ['United States', 1082, 1215],
+  'US': ['United States', 1084, 1217],
   'CA': ['Canada', 7, 8],
   'JO': ['Jordan', 1, 1],
   'DE': ['Germany', 22, 22],
--- people/oldboyscout/user.py
+++ people/oldboyscout/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/oldboyscout/',
   'location': '',
   'countries': 31,
-  'markers': 2848,
-  'photos': 3149
+  'markers': 2850,
+  'photos': 3151
 }
