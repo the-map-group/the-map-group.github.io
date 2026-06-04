76958b9f1f9b6b894b052f55d8dccb685def7f06
The Map Group <admin@the-map-group.top>
2026-06-04 03:14:00 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/last_total.py
+++ people/hugovonschreck/last_total.py
@@ -1,1 +1,1 @@
-number = 9339
+number = 9342
--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/hugovonschreck/',
   'location': 'Linsengericht, Germany',
   'countries': 11,
-  'markers': 1616,
-  'photos': 6702
+  'markers': 1617,
+  'photos': 6703
 }
