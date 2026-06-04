eac155b841d5f0c88c8192150df25e385f2e67d4
The Map Group <admin@the-map-group.top>
2026-06-04 03:05:57 -0300

    [auto] Updated map for member 'pascalvu1'

--- people/quanito/countries.py
+++ people/quanito/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'TH': ['Thailand', 11, 51],
-  'FR': ['France', 3324, 16290],
+  'FR': ['France', 3329, 16313],
   'LA': ['Laos', 5, 57],
   'KH': ['Cambodia', 1, 24],
   'IT': ['Italy', 391, 2046],
--- people/quanito/user.py
+++ people/quanito/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/quanito/',
   'location': 'Nogent sur Marne, FRANCE',
   'countries': 53,
-  'markers': 7900,
-  'photos': 37484
+  'markers': 7905,
+  'photos': 37507
 }
