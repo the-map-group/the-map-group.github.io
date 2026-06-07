75a22f089d43ebf52c17398a781ab09e58e898ac
The Map Group <admin@the-map-group.top>
2026-06-07 03:23:11 -0300

    [auto] Updated map for member 'enrico sprea'

--- people/enricosprea/user.py
+++ people/enricosprea/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/enricosprea/',
   'location': 'Brianza, Italia',
   'countries': 23,
-  'markers': 973,
-  'photos': 984
+  'markers': 974,
+  'photos': 985
 }
