58e43fdb14e870caeab0dedb852a65ef932fa9a8
The Map Group <admin@the-map-group.top>
2026-06-08 03:07:21 -0300

    [auto] Updated map for member 'Mani_H.'

--- people/manfred_h/countries.py
+++ people/manfred_h/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'DE': ['Germany', 692, 1407],
+  'DE': ['Germany', 693, 1411],
   'BE': ['Belgium', 5, 5],
   'IT': ['Italy', 1, 1],
   'DK': ['Denmark', 41, 55],
--- people/manfred_h/user.py
+++ people/manfred_h/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/manfred_h/',
   'location': 'Hamm-Bockum, Germany',
   'countries': 8,
-  'markers': 811,
-  'photos': 1568
+  'markers': 812,
+  'photos': 1572
 }
