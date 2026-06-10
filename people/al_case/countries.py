2327633f33c23d6a42f80b2a80082ac218daa91f
The Map Group <admin@the-map-group.top>
2026-06-10 03:45:01 -0300

    [auto] Updated map for member 'Al Case'

--- people/al_case/countries.py
+++ people/al_case/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'US': ['United States', 429, 485],
+  'US': ['United States', 430, 486],
   'DK': ['Denmark', 112, 150],
   'JP': ['Japan', 44, 44],
   'SE': ['Sweden', 23, 36],
--- people/al_case/user.py
+++ people/al_case/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/al_case/',
   'location': 'Ashland, Oregon, USA',
   'countries': 19,
-  'markers': 772,
-  'photos': 889
+  'markers': 773,
+  'photos': 890
 }
