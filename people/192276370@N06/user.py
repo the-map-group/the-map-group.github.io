6f95e186f19371cbc878c20358d3b808776b2ccd
The Map Group <admin@the-map-group.top>
2026-06-15 03:22:00 -0300

    [auto] Updated map for member 'Patrice Dardoize'

--- people/192276370@N06/user.py
+++ people/192276370@N06/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/192276370@N06/',
   'location': '',
   'countries': 3,
-  'markers': 775,
-  'photos': 978
+  'markers': 777,
+  'photos': 980
 }
