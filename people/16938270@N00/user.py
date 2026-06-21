9eec7b72de9f3e767266ddacd674a07e905428ab
The Map Group <admin@the-map-group.top>
2026-06-21 03:34:06 -0300

    [auto] Updated map for member 'Ian & Marg'

--- people/16938270@N00/user.py
+++ people/16938270@N00/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/16938270@N00/',
   'location': 'Ormiston, Australia',
   'countries': 35,
-  'markers': 1202,
-  'photos': 1296
+  'markers': 1204,
+  'photos': 1298
 }
