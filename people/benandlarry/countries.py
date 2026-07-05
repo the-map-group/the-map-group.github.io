cc50a352b2cf5116d20d988989ada5c7f6503282
The Map Group <admin@the-map-group.top>
2026-07-05 03:28:49 -0300

    [auto] Updated map for member 'benlarhome'

--- people/benandlarry/countries.py
+++ people/benandlarry/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'CA': ['Canada', 9238, 44555],
+  'CA': ['Canada', 9241, 44558],
   'CN': ['China', 16, 16],
   'AU': ['Australia', 3, 3],
   'US': ['United States', 1275, 5229],
--- people/benandlarry/user.py
+++ people/benandlarry/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/benandlarry/',
   'location': 'Canada',
   'countries': 33,
-  'markers': 10855,
-  'photos': 51255
+  'markers': 10858,
+  'photos': 51258
 }
