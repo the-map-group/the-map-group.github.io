5e41557860945b34c69f94263fb0171674a81e1f
The Map Group <admin@the-map-group.top>
2026-07-14 03:45:40 -0300

    [auto] Updated map for member 'I saw_that'

--- people/saw_that/countries.py
+++ people/saw_that/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'US': ['United States', 347, 717],
   'IT': ['Italy', 39, 48],
-  'CA': ['Canada', 947, 1945],
+  'CA': ['Canada', 949, 1948],
   'GB': ['United Kingdom', 19, 23],
   'FR': ['France', 14, 19],
   'DE': ['Germany', 3, 4],
--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/saw_that/',
   'location': 'Iqaluit, Nunavut, Canada',
   'countries': 19,
-  'markers': 1529,
-  'photos': 3098
+  'markers': 1531,
+  'photos': 3101
 }
