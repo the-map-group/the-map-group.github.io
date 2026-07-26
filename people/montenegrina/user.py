0b51251ee44952c786a1943a7a0fa2775f418924
The Map Group <admin@the-map-group.top>
2026-07-26 03:18:51 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/user.py
+++ people/montenegrina/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/montenegrina/',
   'location': 'Podgorica, Montenegro',
   'countries': 13,
-  'markers': 1425,
-  'photos': 1612
+  'markers': 1426,
+  'photos': 1613
 }
