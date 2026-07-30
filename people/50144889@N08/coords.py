132725f05343557700921cb070b114340dc5a447
The Map Group <admin@the-map-group.top>
2026-07-30 03:28:17 -0300

    [auto] Updated map for member 'Robert.Pittman'

--- people/50144889@N08/coords.py
+++ people/50144889@N08/coords.py
@@ -2225,5 +2225,6 @@ coords_dict = {
   '16.325173,-86.489648': ['HN', 'Honduras'],
   '52.669995,1.502778': ['GB', 'United Kingdom'],
   '50.628137,-2.168114': ['GB', 'United Kingdom'],
-  '50.52699,-5.021843': ['GB', 'United Kingdom']
+  '50.52699,-5.021843': ['GB', 'United Kingdom'],
+  '52.464641,1.564222': ['GB', 'United Kingdom']
 }
