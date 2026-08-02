82fe5e130fb59a7c833296e87c64ccd41dc846f4
The Map Group <admin@the-map-group.top>
2026-08-02 03:24:50 -0300

    [auto] Updated map for member 'D-Stanley'

--- people/davidstanleytravel/countries.py
+++ people/davidstanleytravel/countries.py
@@ -2,7 +2,7 @@ countries_dict = {
   'IS': ['Iceland', 102, 121],
   'AW': ['Aruba', 20, 30],
   'GI': ['Gibraltar', 18, 23],
-  'ES': ['Spain', 278, 358],
+  'ES': ['Spain', 279, 363],
   'GB': ['United Kingdom', 264, 297],
   'FO': ['Faroe Islands', 5, 10],
   'CN': ['China', 259, 300],
--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
   'location': 'Nanaimo, Canada',
   'countries': 129,
-  'markers': 9075,
-  'photos': 10660
+  'markers': 9076,
+  'photos': 10665
 }
