670c828ac6b9b2e34c19998136289fc0aa75fc75
The Map Group <admin@the-map-group.top>
2026-08-04 03:32:55 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'US': ['United States', 3838, 6874],
+  'US': ['United States', 3838, 6875],
   'GB': ['United Kingdom', 52, 95],
   'CA': ['Canada', 1, 1],
   'MX': ['Mexico', 8, 10],
