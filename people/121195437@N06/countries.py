d03e2eb2cd232eed6a00ed7f2f09eae7ea917217
The Map Group <admin@the-map-group.top>
2026-08-13 03:10:48 -0300

    [auto] Updated map for member 'Harald Schulz'

--- people/121195437@N06/countries.py
+++ people/121195437@N06/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'CA': ['Canada', 19, 42],
   'US': ['United States', 124, 591],
-  'FR': ['France', 604, 767],
+  'FR': ['France', 607, 770],
   'CH': ['Switzerland', 385, 506],
   'LV': ['Latvia', 2, 2],
   'EE': ['Estonia', 1, 1],
@@ -22,7 +22,7 @@ countries_dict = {
   'HR': ['Croatia', 4, 11],
   'GR': ['Greece', 9, 13],
   'SE': ['Sweden', 379, 482],
-  'NL': ['Netherlands', 393, 467],
+  'NL': ['Netherlands', 393, 468],
   'DK': ['Denmark', 67, 70],
   'GB': ['United Kingdom', 135, 170],
   'FI': ['Finland', 249, 313],
--- people/121195437@N06/user.py
+++ people/121195437@N06/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/121195437@N06/',
   'location': '',
   'countries': 28,
-  'markers': 5458,
-  'photos': 7860
+  'markers': 5461,
+  'photos': 7864
 }
