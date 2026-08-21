7e20b07c862c8f6ee6d655e72e62ac84f4ca9d7c
The Map Group <admin@the-map-group.top>
2026-08-21 03:20:14 -0300

    [auto] Updated map for member 'Brit 70013 fan'

--- people/93936890@N02/countries.py
+++ people/93936890@N02/countries.py
@@ -7,7 +7,7 @@ countries_dict = {
   'MY': ['Malaysia', 5, 5],
   'ID': ['Indonesia', 9, 10],
   'US': ['United States', 279, 397],
-  'CA': ['Canada', 110, 128],
+  'CA': ['Canada', 111, 129],
   'SG': ['Singapore', 5, 6],
   'IM': ['Isle of Man', 1, 2],
   'TH': ['Thailand', 4, 4],
--- people/93936890@N02/user.py
+++ people/93936890@N02/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/93936890@N02/',
   'location': 'Victoria, B.C., Canada',
   'countries': 45,
-  'markers': 917,
-  'photos': 1336
+  'markers': 918,
+  'photos': 1337
 }
