b564e11c1c343804d9939d733ccc6cdad0a72505
The Map Group <admin@the-map-group.top>
2026-09-06 03:25:28 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -6,6 +6,6 @@ countries_dict = {
   'HR': ['Croatia', 184, 243],
   'IT': ['Italy', 77, 126],
   'VA': ['Vatican City', 22, 31],
-  'CL': ['Chile', 97, 230],
+  'CL': ['Chile', 102, 241],
   'UY': ['Uruguay', 47, 80]
 }
--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/hpfilho/',
   'location': 'Campinas, Brazil',
   'countries': 9,
-  'markers': 753,
-  'photos': 1418
+  'markers': 758,
+  'photos': 1429
 }
