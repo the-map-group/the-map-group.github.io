b343d52b05a781936f60f86339c6ab1e9302e853
The Map Group <admin@the-map-group.top>
2026-09-09 03:18:20 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'IT': ['Italy', 4157, 5774],
+  'IT': ['Italy', 4165, 5782],
   'DE': ['Germany', 3301, 30589],
   'HR': ['Croatia', 214, 244],
   'GE': ['Georgia', 155, 176],
--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
   'location': 'Munich, Germany',
   'countries': 15,
-  'markers': 9819,
-  'photos': 39112
+  'markers': 9827,
+  'photos': 39120
 }
