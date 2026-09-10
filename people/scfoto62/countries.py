4f42e91b20f6b9221e55793044c6ec42f05668cd
The Map Group <admin@the-map-group.top>
2026-09-10 03:18:17 -0300

    [auto] Updated map for member 'CrËOS Photographie'

--- people/scfoto62/countries.py
+++ people/scfoto62/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'FR': ['France', 2085, 3180],
+  'FR': ['France', 2086, 3181],
   'BE': ['Belgium', 125, 205],
   'DE': ['Germany', 2, 2],
   'GB': ['United Kingdom', 62, 63]
--- people/scfoto62/user.py
+++ people/scfoto62/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/scfoto62/',
   'location': 'Métropole lilloise, FRANCE',
   'countries': 4,
-  'markers': 2274,
-  'photos': 3450
+  'markers': 2275,
+  'photos': 3451
 }
