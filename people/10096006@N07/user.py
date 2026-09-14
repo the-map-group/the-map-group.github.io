d9175ebd29461ed97e868581ff30c69e5fcc6a50
The Map Group <admin@the-map-group.top>
2026-09-14 03:28:49 -0300

    [auto] Updated map for member 'ceri1965'

--- people/10096006@N07/user.py
+++ people/10096006@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/10096006@N07/',
   'location': '',
   'countries': 1,
-  'markers': 181,
-  'photos': 229
+  'markers': 183,
+  'photos': 231
 }
