5ebb755093fb0710e37857f07daef970f481e7d7
The Map Group <admin@the-map-group.top>
2026-09-14 03:31:32 -0300

    [auto] Updated map for member 'Donatella Basaglia'

--- people/142678369@N04/countries.py
+++ people/142678369@N04/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'IT': ['Italy', 6908, 13181],
+  'IT': ['Italy', 6912, 13185],
   'FR': ['France', 641, 1035],
   'CH': ['Switzerland', 9, 14]
 }
--- people/142678369@N04/user.py
+++ people/142678369@N04/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/142678369@N04/',
   'location': 'Torino, Italia',
   'countries': 3,
-  'markers': 7558,
-  'photos': 14230
+  'markers': 7562,
+  'photos': 14234
 }
