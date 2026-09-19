afa4ee175e5b430dd3c78e51efc6307ec973ea28
The Map Group <admin@the-map-group.top>
2026-09-19 03:18:48 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/countries.py
+++ people/153072396@N07/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'GB': ['United Kingdom', 52, 86],
-  'FR': ['France', 98, 373],
+  'FR': ['France', 99, 374],
   'DK': ['Denmark', 58, 95],
   'IE': ['Ireland', 22, 29],
   'BE': ['Belgium', 1, 1],
--- people/153072396@N07/user.py
+++ people/153072396@N07/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/153072396@N07/',
   'location': 'France',
   'countries': 11,
-  'markers': 293,
-  'photos': 682
+  'markers': 294,
+  'photos': 683
 }
