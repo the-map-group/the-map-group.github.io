46067826152ad13374fe5adcea64f79e57f65dbd
The Map Group <admin@the-map-group.top>
2026-09-23 03:27:32 -0300

    [auto] Updated map for member 'mouzhik'

--- people/zemzem/user.py
+++ people/zemzem/user.py
@@ -6,6 +6,6 @@ user_info = {
   'url': 'https://www.flickr.com/photos/zemzem/',
   'location': 'France',
   'countries': 10,
-  'markers': 331,
-  'photos': 465
+  'markers': 332,
+  'photos': 466
 }
